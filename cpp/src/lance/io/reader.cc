b45533b711c4c67ca2713a0eefa1559da1b0ecad
Lei Xu <lei@eto.ai>
2022-07-08 12:05:11 -0700

    use std::optional to return offset

--- cpp/src/lance/io/reader.cc
+++ cpp/src/lance/io/reader.cc
@@ -1,3 +1,17 @@
+//  Copyright 2022 Lance Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 #include "lance/io/reader.h"
 
 #include <arrow/array/concatenate.h>
@@ -256,7 +270,7 @@ const lance::format::Metadata& FileReader::metadata() const { return *metadata_;
 }
 
 int64_t FileReader::GetChunkPosition(int64_t field_id, int64_t chunk_id) const {
-  return lookup_table_->GetOffset(field_id, chunk_id);
+  return lookup_table_->GetOffset(field_id, chunk_id).value_or(-1);
 }
 
 ::arrow::Result<std::shared_ptr<::arrow::Array>> FileReader::GetArray(
