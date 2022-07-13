5d99ec99b67c73c77ba5d3f77760b531ab255f82
Lei Xu <lei@eto.ai>
2022-07-13 11:23:08 -0700

    write dictionary column

--- python/lance/tests/duckdb_test.py
+++ python/lance/tests/duckdb_test.py
@@ -15,17 +15,24 @@
 
 """DuckDB Integration Tests"""
 
+from pathlib import Path
+
 import duckdb
 import lance
-import pyarrow as pa
 import pandas as pd
-from pathlib import Path
+import pyarrow as pa
 
 
 def test_dictionary_type_query(tmp_path: Path):
     df = pd.DataFrame({"class": ["foo", "bar", "foo", "zoo"], "grade": ["A", "B", "B", "A"]})
-    df["class"] = df["class"].astype("category")
-    df["grade"] = df["grade"].astype("category")
+    # df["class"] = df["class"].astype("category")
+    # df["grade"] = df["grade"].astype("category")
 
+    uri = tmp_path / "dict.lance"
+    print(pa.Table.from_pandas(df, preserve_index=False).schema)
     lance.write_table(pa.Table.from_pandas(df), tmp_path / "dict.lance", "class")
-    print(df)
+    # print(df)
+
+    ds = lance.dataset(uri)
+    print(duckdb.query("SELECT * FROM ds"))
+    print(duckdb.query("SELECT count(class), class FROM ds GROUP BY 2").to_df())
