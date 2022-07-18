d8f9edfb2748aaf5d8deace9244f954375bcf249
Lei Xu <eddyxu@gmail.com>
2022-07-18 13:32:02 -0700

    add comments and remove NotImplemented function for PR

--- cpp/src/lance/io/filter.h
+++ cpp/src/lance/io/filter.h
@@ -47,11 +47,6 @@ class Filter {
       std::tuple<std::shared_ptr<::arrow::UInt64Array>, std::shared_ptr<::arrow::RecordBatch>>>
       Exec(std::shared_ptr<::arrow::RecordBatch>) const;
 
-  /// Execute the filter on a batch.
-  ::arrow::Result<
-      std::tuple<std::shared_ptr<::arrow::BooleanArray>, std::shared_ptr<::arrow::Array>>>
-  Exec(std::shared_ptr<FileReader> reader, int32_t chunk_id) const;
-
   std::string ToString() const;
 
  private:
