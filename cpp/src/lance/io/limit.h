200a2da44e696fa41d69a9ea00ead00bf6fdf50c
Lei Xu <lei@eto.ai>
2022-07-24 13:02:55 -0700

    update comments

--- cpp/src/lance/io/limit.h
+++ cpp/src/lance/io/limit.h
@@ -35,11 +35,26 @@ class Limit {
   /// Construct a Limit Clause with limit, and optionally, offset.
   explicit Limit(int64_t limit, int64_t offset = 0) noexcept;
 
-  /// Apply limit to the size of the length.
+  /// Apply limit when reading a chunk.
   ///
-  /// \return a tuple of [offset, length]. Return [0, 0] to skip a chunk.
-  /// Returns std::nullopt to indicate the end of the iteration.
-  std::optional<std::tuple<int64_t, int64_t>> Apply(int64_t length);
+  /// \param chunk_length the length of a chunk to be read.
+  ///
+  /// \return a tuple of `[offset, length]` that should be physically loaded
+  /// into memory. Return `[0, 0]` to skip this chunk.
+  /// Return `std::nullopt` to indicate the end of the iteration.
+  ///
+  /// \code{.cpp}
+  /// auto chunk_length = GetChunkLength(chunk_id);
+  /// auto limit = Limit(20, 30);
+  /// auto offset_and_length = limit.Apply(chunk_length);
+  /// if (!offset_and_length) {
+  ///    // stop
+  ///    return;
+  /// }
+  /// auto [offset, length] = offset_and_length;
+  /// return infile->ReadAt(offset, length);
+  /// \endcode
+  std::optional<std::tuple<int64_t, int64_t>> Apply(int64_t chunk_length);
 
   /// Debug String
   std::string ToString() const;
