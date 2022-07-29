7ece80f923f5111090ed778f25c63b86fe9f1ba7
Lei Xu <lei@eto.ai>
2022-07-28 17:11:28 -0700

    Add duckdb integration tests (#29)

--- python/setup.py
+++ python/setup.py
@@ -13,7 +13,6 @@ lance_cpp = Path(__file__).resolve().parent.parent / 'cpp'
 lance_includes = str(lance_cpp / 'include')
 lance_libs = str(lance_cpp / 'build')
 
-
 extensions = [Extension(
     "lance.lib",
     ["lance/_lib.pyx"],
@@ -24,7 +23,6 @@ extensions = [Extension(
     extra_compile_args=["-Wall", "-std=c++20", "-O3"],
     extra_link_args=["-Wl,-rpath", lance_libs]
 )]
-
 
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
@@ -38,7 +36,7 @@ setup(
     ext_modules=cythonize(extensions, language_level="3"),
     zip_safe=False,
     install_requires=["pyarrow"],
-    extras_require={"test": ["pytest>=6.0", "pandas"]},
+    extras_require={"test": ["pytest>=6.0", "pandas", "duckdb"]},
     python_requires=">=3.8",
     packages=find_packages()
 )
