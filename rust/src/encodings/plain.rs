94b1906dfb389dc60e592b24332e04a936bbcd50
Lei Xu <lei@eto.ai>
2022-08-21 20:08:55 -0700

    Data Inspection CLI (#119)

---
+++ rust/src/encodings/plain.rs
@@ -0,0 +1,88 @@
+//  Copyright 2022 Lance Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+//! Plain encoding
+
+use arrow::array::{make_array, new_empty_array, ArrayDataBuilder, ArrayRef, Int32Array};
+use arrow::buffer::MutableBuffer;
+use arrow::compute::{subtract_scalar_dyn, take};
+use arrow::datatypes::{ArrowPrimitiveType, Int32Type};
+use std::io::{Error, ErrorKind, Read, Result, Seek, SeekFrom};
+
+use crate::encodings::Decoder;
+
+/// Plain Decoder
+pub struct PlainDecoder<'a, R: Read + Seek> {
+    file: &'a mut R,
+    position: u64,
+    page_length: i64,
+}
+
+impl<'a, R: Read + Seek> PlainDecoder<'a, R> {
+    pub fn new(file: &'a mut R, position: u64, page_length: i64) -> Self {
+        PlainDecoder {
+            file,
+            position,
+            page_length,
+        }
+    }
+}
+
+impl<'a, R: Read + Seek, T: ArrowPrimitiveType> Decoder<T> for PlainDecoder<'a, R> {
+    type ArrowType = T;
+
+    fn decode(&mut self, offset: i32, length: &Option<i32>) -> Result<ArrayRef> {
+        let read_len = length.unwrap_or((self.page_length - (offset as i64)) as i32) as usize;
+        (*self.file).seek(SeekFrom::Start(self.position + offset as u64))?;
+        let mut mutable_buf = MutableBuffer::new(read_len * T::get_byte_width());
+        (*self.file).read_exact(mutable_buf.as_slice_mut())?;
+        let builder = ArrayDataBuilder::new(T::DATA_TYPE).buffers(vec![mutable_buf.into()]);
+        let data = match builder.build() {
+            Ok(d) => d,
+            Err(e) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    format!("Invalid builder: {}", e),
+                ))
+            }
+        };
+        Ok(make_array(data))
+    }
+
+    fn take(&mut self, indices: &Int32Array) -> Result<ArrayRef> {
+        if indices.is_empty() {
+            return Ok(new_empty_array(&T::DATA_TYPE));
+        }
+        let start = indices.value(0);
+        let length = indices.values().last().map(|i| i - start);
+        // Not sure why it needs cast.
+        let values = <PlainDecoder<'a, R> as Decoder<T>>::decode(self, start, &length)?;
+        let reset_indices = match subtract_scalar_dyn::<Int32Type>(&values, start) {
+            Ok(arr) => arr,
+            Err(e) => return Err(Error::new(ErrorKind::InvalidData, e.to_string())),
+        };
+        match take(
+            &values,
+            reset_indices.as_any().downcast_ref::<Int32Array>().unwrap(),
+            None,
+        ) {
+            Ok(arr) => Ok(arr),
+            Err(e) => Err(Error::new(ErrorKind::InvalidData, format!("Error take indices: {}", e)))
+        }
+    }
+
+    fn value(&self, i: usize) -> Result<T::Native> {
+        todo!()
+    }
+}
---
+++ rust/src/io.rs
@@ -0,0 +1,99 @@
+//  Copyright 2022 Lance Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+use std::io::{Cursor, Error, ErrorKind, Read, Result, Seek, SeekFrom};
+
+use byteorder::{LittleEndian, ReadBytesExt};
+
+use crate::format::pb;
+use crate::schema::Schema;
+
+static MAGIC_NUMBER: &str = "LANC";
+
+/// Lance File Reader.
+pub struct FileReader<R: Read + Seek> {
+    file: R,
+    schema: Schema,
+    // TODO: impl a Metadata
+    metadata: pb::Metadata,
+}
+
+trait ProtoReader<P: prost::Message + Default> {
+    fn read<R: Read + Seek>(file: &mut R, pos: i64) -> Result<P>;
+}
+
+struct ProtoParser;
+
+impl<P: prost::Message + Default> ProtoReader<P> for ProtoParser {
+    fn read<R: Read + Seek>(file: &mut R, pos: i64) -> Result<P> {
+        let mut size_buf = [0; 4];
+        file.seek(SeekFrom::Start(pos as u64))?;
+        file.read_exact(&mut size_buf)?;
+        let pb_size = Cursor::new(size_buf).read_i32::<LittleEndian>()?;
+        let mut buf = vec![0; pb_size as usize];
+        file.read_exact(&mut buf)?;
+        match P::decode(&buf[..]) {
+            Ok(m) => Ok(m),
+            Err(e) => Err(Error::new(
+                ErrorKind::InvalidData,
+                "Invalid metadata: ".to_owned() + &e.to_string(),
+            )),
+        }
+    }
+}
+
+fn read_footer<R: Read + Seek>(file: &mut R) -> Result<i64> {
+    file.seek(SeekFrom::End(-16))?;
+    let mut buf = [0; 16];
+    let nbytes = file.read(&mut buf)?;
+    if nbytes < 16 {
+        return Err(Error::new(
+            ErrorKind::InvalidData,
+            "Not a lance file: size < 16 bytes",
+        ));
+    }
+    let s = match std::str::from_utf8(&buf[12..16]) {
+        Ok(s) => s,
+        Err(_) => return Err(Error::new(ErrorKind::InvalidData, "Not a lance file")),
+    };
+    if !s.eq(MAGIC_NUMBER) {
+        return Err(Error::new(ErrorKind::InvalidData, "Not a lance file"));
+    }
+    // TODO: check version
+
+    Cursor::new(&buf[0..8]).read_i64::<LittleEndian>()
+}
+
+impl<R: Read + Seek> FileReader<R> {
+    pub fn new(file: R) -> Result<Self> {
+        let mut f = file;
+        let metadata_pos = read_footer(&mut f)?;
+        let metadata: crate::format::pb::Metadata = ProtoParser::read(&mut f, metadata_pos)?;
+        let manifest: crate::format::pb::Manifest =
+            ProtoParser::read(&mut f, metadata.manifest_position as i64)?;
+        Ok(FileReader {
+            file: f,
+            schema: Schema::from_proto(&manifest.fields),
+            metadata,
+        })
+    }
+
+    pub fn schema(&self) -> &Schema {
+        &self.schema
+    }
+
+    pub fn num_chunks(&self) -> i32 {
+        self.metadata.batch_offsets.len() as i32 - 1
+    }
+}
