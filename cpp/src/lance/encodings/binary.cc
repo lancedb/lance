9305d35af5d2da72c285e0705aeb6e86b66961ea
Lei Xu <lei@eto.ai>
2022-08-24 15:13:16 -0700

    change var names

--- cpp/src/lance/encodings/binary.cc
+++ cpp/src/lance/encodings/binary.cc
@@ -33,12 +33,12 @@ VarBinaryEncoder::VarBinaryEncoder(std::shared_ptr<::arrow::io::OutputStream> ou
     : Encoder(out) {}
 
 Result<int64_t> VarBinaryEncoder::Write(const std::shared_ptr<::arrow::Array>& data) {
-  ARROW_ASSIGN_OR_RAISE(auto start_offset, out_->Tell());
+  ARROW_ASSIGN_OR_RAISE(auto values_position, out_->Tell());
   auto arr = std::static_pointer_cast<::arrow::BinaryArray>(data);
   typename ::arrow::BinaryArray::offset_type len;
   auto pos = arr->GetValue(0, &len);
-  auto first_offset = arr->value_offset(0);
-  auto bytes = arr->value_offset(arr->length()) - first_offset;
+  auto start_offset = arr->value_offset(0);
+  auto bytes = arr->value_offset(arr->length()) - start_offset;
   ARROW_RETURN_NOT_OK(out_->Write(pos, bytes));
 
   ARROW_ASSIGN_OR_RAISE(auto offsets_position, out_->Tell());
@@ -47,10 +47,10 @@ Result<int64_t> VarBinaryEncoder::Write(const std::shared_ptr<::arrow::Array>& d
   /// Reset the slice's first offset to zero.
   for (int64_t i = 0; i <= arr->length(); ++i) {
     ARROW_RETURN_NOT_OK(
-        offsets_builder_.Append(start_offset + arr->value_offset(i) - first_offset));
+        offsets_builder_.Append(values_position + arr->value_offset(i) - start_offset));
   }
   ARROW_RETURN_NOT_OK(offsets_builder_.Finish(&offsets_));
-  assert(offsets_->Value(0) == start_offset);
+  assert(offsets_->Value(0) == values_position);
 
   ARROW_RETURN_NOT_OK(
       out_->Write(offsets_->raw_values(),
