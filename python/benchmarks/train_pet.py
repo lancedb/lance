861b61c022c812cd604ed5dfba1d7cf9533687ab
Lei Xu <eddyxu@gmail.com>
2022-09-05 17:53:21 -0700

    add train loop

--- python/benchmarks/train_pet.py
+++ python/benchmarks/train_pet.py
@@ -4,7 +4,6 @@
 
 """
 
-from pydoc import cli
 import click
 import pytorch_lightning as pl
 import torch
@@ -15,7 +14,7 @@ import lance
 import lance.pytorch.data
 
 
-class EfficientNet(pl.LightningModule):
+class Classification(pl.LightningModule):
     def __init__(self) -> None:
         super().__init__()
         self.model = torchvision.models.efficientnet_b1()
@@ -34,9 +33,11 @@ class EfficientNet(pl.LightningModule):
 @click.option("-b", "--batch_size", default=64, help="batch size", show_default=True)
 @click.argument("dataset")
 def train(dataset: str, batch_size: int):
-    dataset = lance.pytorch.data.LanceDataset(dataset, columns=["class"], batch_size=batch_size)
+    dataset = lance.pytorch.data.LanceDataset(
+        dataset, columns=["class", "image"], batch_size=batch_size
+    )
     train_loader = torch.utils.data.DataLoader(dataset, num_workers=0, batch_size=None)
-    model = EfficientNet()
+    model = Classification()
     trainer = pl.Trainer(limit_train_batches=100, max_epochs=1)
     trainer.fit(model=model, train_dataloaders=train_loader)
 
