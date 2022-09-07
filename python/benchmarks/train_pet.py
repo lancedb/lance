1697a38bf9b8ec96dd7e4bff1d503c535e6e1e98
Lei Xu <lei@eto.ai>
2022-09-07 02:21:19 +0000

    read raw dataset

--- python/benchmarks/train_pet.py
+++ python/benchmarks/train_pet.py
@@ -6,9 +6,10 @@
 
 import io
 import os
-from typing import Optional
+from typing import Callable, Optional
 
 import click
+import pyarrow.fs
 import pytorch_lightning as pl
 import torch
 import torchvision
@@ -20,15 +21,74 @@ from torchvision.models.efficientnet import EfficientNet_B0_Weights
 import lance
 import lance.pytorch.data
 
+transform = T.Compose([EfficientNet_B0_Weights.DEFAULT.transforms()])
+
+
+def raw_collate_fn(batch):
+    images = []
+    labels = []
+    for img, label in batch:
+        images.append(img)
+        labels.append(label)
+    return torch.stack(images), torch.Tensor(labels)
+
+
+class RawOxfordPetDataset(torch.utils.data.Dataset):
+    """Build OxfordPet dataset from raw metadata.
+
+    The main difference between this and the one from torchvision is that
+    this one can directly read from cloud storage.
+    """
+
+    def __init__(
+        self, root: str, split: str = "trainval", transform: Optional[Callable] = None
+    ):
+        """Constructor.
+
+        Parameters
+        ----------
+        root : str
+            Root URI of the dataset, i.e., `s3://eto-public/datasets/oxford_pet/`.
+            We expect two sub-directories under the root: "annotations" and "images".
+        split : str
+            The split to load, either "trailval" or "test"
+        """
+        self.root = root
+        self.split = split
+        self.transform = transform
+
+        image_list_file = os.path.join(root, "annotations", f"{split}.txt")
+        fs, path = pyarrow.fs.FileSystem.from_uri(image_list_file)
+        self.fs = fs
+        self.images = []
+        self.labels = []
+        with fs.open_input_stream(path) as input:
+            content = input.readall().decode("utf-8")
+            for line in content.split("\n"):
+                comps = line.split()
+                if comps:
+                    self.images.append(comps[0])
+                    self.labels.append(int(comps[1]))
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image_uri = os.path.join(self.root, "images", f"{self.images[idx]}.jpg")
+        fs, path = pyarrow.fs.FileSystem.from_uri(image_uri)
+        with fs.open_input_stream(path) as fobj:
+            img = Image.open(io.BytesIO(fobj.readall()))
+            if self.transform:
+                img = self.transform(img)
+            return img, self.labels[idx]
+
 
 def collate_fn(batch):
     # TODO: Labels should be converted via torch.LanceDataset
     labels = torch.randint(0, 31, size=(len(batch[1]),))
     # TODO: Image conversion should in torch.LanceDataset
     preprocessing = EfficientNet_B0_Weights.DEFAULT.transforms()
-    images = [
-        preprocessing(T.ToTensor()(Image.open(io.BytesIO(data)))) for data in batch[0]
-    ]
+    images = [transform(Image.open(io.BytesIO(data))) for data in batch[0]]
     return torch.stack(images), labels
 
 
@@ -74,21 +134,47 @@ class Classification(pl.LightningModule):
     help="set pytorch DataLoader number of workers",
     show_default=True,
 )
+@click.option(
+    "--format",
+    "-F",
+    "data_format",
+    type=click.Choice(["lance", "raw", "parquet"]),
+    default="lance",
+)
 @click.option("--benchmark", type=click.Choice(["io", "train"]), default="train")
 @click.argument("dataset")
-def train(dataset: str, batch_size: int, epochs: int, benchmark: str, num_workers: int):
+def train(
+    dataset: str,
+    batch_size: int,
+    epochs: int,
+    benchmark: str,
+    num_workers: int,
+    data_format,
+):
     print(f"Running benchmark: {benchmark}")
-    dataset = lance.pytorch.data.LanceDataset(
-        dataset,
-        columns=["image", "class"],
-        batch_size=batch_size,
-    )
-    train_loader = torch.utils.data.DataLoader(
-        dataset,
-        num_workers=num_workers,
-        batch_size=None,
-        collate_fn=collate_fn,
-    )
+    if data_format == "lance":
+        dataset = lance.pytorch.data.LanceDataset(
+            dataset,
+            columns=["image", "class"],
+            batch_size=batch_size,
+        )
+        train_loader = torch.utils.data.DataLoader(
+            dataset,
+            num_workers=num_workers,
+            batch_size=None,
+            collate_fn=collate_fn,
+        )
+    elif data_format == "raw":
+        dataset = RawOxfordPetDataset(dataset, transform=transform)
+        train_loader = torch.utils.data.DataLoader(
+            dataset,
+            num_workers=num_workers,
+            batch_size=batch_size,
+            collate_fn=raw_collate_fn,
+        )
+    else:
+        raise ValueError("Unsupported data format")
+
     model = Classification(benchmark=benchmark)
     trainer = pl.Trainer(
         limit_train_batches=100, max_epochs=epochs, accelerator="gpu", devices=-1
