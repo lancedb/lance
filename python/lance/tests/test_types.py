297891189fca82b8699efb2d309b6671e414472a
Chang She <759245+changhiskhan@users.noreply.github.com>
2022-09-16 09:55:32 -0700

    test ImageArray

--- python/lance/tests/test_types.py
+++ python/lance/tests/test_types.py
@@ -176,3 +176,7 @@ def test_pickle(tmp_path):
         pickle.dump(img, fh)
     with (tmp_path / "image").open("rb") as fh:
         assert img == pickle.load(fh)
+
+
+def test_image_array(tmp_path):
+    pass
