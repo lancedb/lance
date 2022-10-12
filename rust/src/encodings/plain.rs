88ba333b97cbc6f2f16a0afd7fc03bc922f2bf6a
Renkai Ge <renkaige@tubi.tv>
2022-10-12 20:31:54 +0800

    arc to box

--- rust/src/encodings/plain.rs
+++ rust/src/encodings/plain.rs
@@ -15,7 +15,6 @@
 //! Plain encoding
 
 use std::io::{Error, ErrorKind, Read, Result, Seek, SeekFrom};
-use std::sync::Arc;
 
 use arrow2::array::{Array, MutableArray, MutablePrimitiveArray};
 use arrow2::array::new_empty_array;
