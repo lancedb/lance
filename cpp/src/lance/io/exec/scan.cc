1b8cc414829302de9f71a9c0fa7c559c1ec91c91
Lei Xu <lei@eto.ai>
2022-11-07 22:37:49 -0800

    ref

--- cpp/src/lance/io/exec/scan.cc
+++ cpp/src/lance/io/exec/scan.cc
@@ -84,7 +84,7 @@ Scan::Scan(const std::vector<FileReaderWithSchema>& readers, int64_t batch_size)
 
   std::vector<std::shared_ptr<::arrow::RecordBatch>> batches;
   for (auto& fut : futs) {
-    ARROW_ASSIGN_OR_RAISE(auto b, fut.result());
+    ARROW_ASSIGN_OR_RAISE(auto& b, fut.result());
     batches.emplace_back(std::move(b));
   }
 
