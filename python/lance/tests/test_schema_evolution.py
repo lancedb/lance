b2c64a1b6a0ddcaf4b5f909afe785efd86256fda
Lei Xu <lei@eto.ai>
2022-11-21 16:56:21 -0800

    add tests

--- python/lance/tests/test_schema_evolution.py
+++ python/lance/tests/test_schema_evolution.py
@@ -14,9 +14,11 @@
 
 from pathlib import Path
 
+import pandas as pd
+
 import lance
 import pyarrow as pa
-import pandas as pd
+import pyarrow.compute as pc
 
 
 def test_write_versioned_dataset(tmp_path: Path):
@@ -30,3 +32,34 @@ def test_write_versioned_dataset(tmp_path: Path):
     actual_df = new_dataset.to_table().to_pandas()
     expected_df = pd.DataFrame({"a": [1, 10], "b": [2, 20], "c": ["a0", "a1"]})
     pd.testing.assert_frame_equal(expected_df, actual_df)
+
+
+def test_add_column_with_literal(tmp_path: Path):
+    table = pa.Table.from_pylist([{"a": i} for i in range(10)])
+
+    base_dir = tmp_path / "test"
+    lance.write_dataset(table, base_dir)
+    dataset = lance.dataset(base_dir)
+    new_dataset = dataset.append_column(pa.field("b", pa.float64()), pc.scalar(0.5))
+
+    assert new_dataset.version["version"] == 2
+    actual_df = new_dataset.to_table().to_pandas()
+    expected_df = table.to_pandas()
+    expected_df["b"] = 0.5
+    pd.testing.assert_frame_equal(actual_df, expected_df)
+
+
+def test_add_column_with_compute(tmp_path: Path):
+    table = pa.Table.from_pylist([{"a": i} for i in range(10)])
+
+    base_dir = tmp_path / "test"
+    lance.write_dataset(table, base_dir)
+    dataset = lance.dataset(base_dir)
+    new_dataset = dataset.append_column(pa.field("b", pa.int64()),
+                                        pc.Expression._call("power", [pc.field("a"), pc.scalar(2)]))
+
+    assert new_dataset.version["version"] == 2
+    actual_df = new_dataset.to_table().to_pandas()
+    expected_df = table.to_pandas()
+    expected_df["b"] = expected_df["a"] * expected_df["a"]
+    pd.testing.assert_frame_equal(actual_df, expected_df)
