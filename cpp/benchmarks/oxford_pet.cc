ba4835546d69f3c077a25fa8fd32b87bae47bb58
Lei Xu <lei@eto.ai>
2022-11-24 14:24:34 -0800

    better parameters

--- cpp/benchmarks/oxford_pet.cc
+++ cpp/benchmarks/oxford_pet.cc
@@ -28,23 +28,22 @@
 #include "lance/arrow/scanner.h"
 
 // we don't use catch2 Benchmark, as it adds heavy noise during profiling.
-void BenchmarkFilterWithLimit(const std::string& uri, int samples = 5) {
+void BenchmarkFilterWithLimit(const std::string& uri, const std::string& cls, int samples = 5) {
   auto dataset = OpenDataset(uri);
   std::vector<std::chrono::milliseconds> durations;
-
   for (int i = 0; i < samples; i++) {
     auto start = std::chrono::system_clock::now();
     auto builder = lance::arrow::ScannerBuilder(dataset);
     assert(builder.Project({"image", "class"}).ok());
     assert(builder
                .Filter(::arrow::compute::equal(::arrow::compute::field_ref("class"),
-                                               ::arrow::compute::literal("pug")))
+                                               ::arrow::compute::literal(cls)))
                .ok());
     assert(builder.Limit(20, 50).ok());
     auto scanner = builder.Finish().ValueOrDie();
     auto table = scanner->ToTable().ValueOrDie();
-    durations.emplace_back(
-        std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start));
+    durations.emplace_back(std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now() - start));
   }
   fmt::print("Times: {}\n", durations);
 }
@@ -52,6 +51,11 @@ void BenchmarkFilterWithLimit(const std::string& uri, int samples = 5) {
 int main(int argc, char** argv) {
   argparse::ArgumentParser parser(argv[0]);
   parser.add_argument("uri").help("Dataset URI");
+  parser.add_argument("-c", "--class").default_value(std::string("pug")).help("filter by class");
+  parser.add_argument("-i", "--iteration")
+      .default_value(int(5))
+      .help("set the number of iterations")
+      .scan<'i', int>();
 
   try {
     parser.parse_args(argc, argv);
@@ -61,6 +65,7 @@ int main(int argc, char** argv) {
     std::exit(1);
   }
 
-  BenchmarkFilterWithLimit(parser.get("uri"));
+  BenchmarkFilterWithLimit(parser.get("uri"), parser.get("class"),
+                           parser.get<int>("iteration"));
   return 0;
 }
