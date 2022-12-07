4fdbc51133eb03cb5defa901827937cb88a0b818
Lei Xu <lei@eto.ai>
2022-12-06 16:34:03 -0800

    partitioning

---
+++ cpp/src/lance/format/partitioning.h
@@ -0,0 +1,42 @@
+//  Copyright 2022 Lance Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#pragma once
+
+#include <arrow/dataset/partition.h>
+#include <arrow/result.h>
+
+#include <memory>
+
+namespace lance::format {
+
+namespace pb {
+class Partitioning;
+}
+
+class Schema;
+
+class Partitioning {
+ public:
+  Partitioning(const Schema& dataset_schema, const Partitioning& proto);
+
+  std::shared_ptr<::arrow::dataset::Partitioning> ToArrow();
+
+  /// Partition schema.
+  const std::shared_ptr<Schema>& schema() const;
+
+ private:
+};
+
+}  // namespace lance::format
