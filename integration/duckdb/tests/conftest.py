835bf83c3e0183ae5f3cab55f6a47571268f6d00
Lei Xu <lei@eto.ai>
2022-12-08 10:30:34 -0800

    build static

--- integration/duckdb/tests/conftest.py
+++ integration/duckdb/tests/conftest.py
@@ -12,7 +12,8 @@ def db() -> duckdb.DuckDBPyConnection:
     """Initialize duckdb with lance extension"""
     db = duckdb.connect(config={"allow_unsigned_extensions": True})
 
-    build_dir = "cuda-build" if torch.cuda.is_available() else "manylinux-build"
+    #build_dir = "cuda-build" if torch.cuda.is_available() else "manylinux-build"
+    build_dir = "manylinux-build"
 
     cur_path = Path(__file__).parent
     db.install_extension(str(cur_path.parent / build_dir / "lance.duckdb_extension"), force_install=True)
