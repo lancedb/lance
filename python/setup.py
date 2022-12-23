71a963eb11748de494b0b5922e17e5cb76895ffe
Chang She <759245+changhiskhan@users.noreply.github.com>
2022-12-23 10:05:18 -0800

    support python 3.11 in GHA

--- python/setup.py
+++ python/setup.py
@@ -86,6 +86,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: POSIX :: Linux",
         "Intended Audience :: Developers",
     ],
