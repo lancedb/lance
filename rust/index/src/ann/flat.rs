d9c1fed99d2ed1b3d81f33eb69949d7e95362f0a
Lei Xu <lei@eto.ai>
2023-01-04 08:41:01 -0800

    extend async writer

--- rust/index/src/ann/flat.rs
+++ rust/index/src/ann/flat.rs
@@ -7,13 +7,13 @@ use arrow_array::RecordBatch;
 use async_trait::async_trait;
 use object_store::path::Path as ObjectPath;
 use object_store::ObjectStore;
-use prost::Message;
-use tokio::io::{AsyncWrite, AsyncWriteExt};
+use tokio::io::{AsyncWriteExt};
 
 use crate::ann::{AnnIndex, AnnIndexType};
 use crate::pb;
 use crate::pb::{AnnIndex as PbAnnIndex, Footer};
 use crate::Index;
+use crate::io::AsyncWriteProtoExt;
 
 /// Flat index.
 ///
@@ -29,7 +29,7 @@ impl<'a> FlatIndex<'a> {
     /// Create an empty index.
     pub fn new(object_store: &'a dyn ObjectStore, path: &str, columns: &[&str]) -> Result<Self> {
         Ok(Self {
-            columns: columns.into_iter().map(|c| c.to_string()).collect(),
+            columns: columns.iter().map(|c| c.to_string()).collect(),
             path: ObjectPath::from(path),
             object_store,
         })
@@ -39,34 +39,9 @@ impl<'a> FlatIndex<'a> {
     pub async fn open(object_store: &'a dyn ObjectStore, path: &str) -> Result<FlatIndex<'a>> {
         Ok(Self {
             columns: vec![],
-            path: ObjectPath::from(path).clone(),
+            path: ObjectPath::from(path),
             object_store,
         })
-    }
-}
-
-#[async_trait]
-trait AsyncWriteProtoExt {
-    async fn write_pb(&mut self, msg: impl Message) -> Result<u64>;
-
-    /// Write footer with the offset to the root metadata block.
-    async fn write_footer(&mut self, offset: u64) -> Result<()>;
-}
-
-#[async_trait]
-impl<T: AsyncWrite + Unpin + std::marker::Send> AsyncWriteProtoExt for T {
-    async fn write_pb(&mut self, msg: impl Message) -> Result<u64> {
-        let len = msg.encoded_len();
-
-        self.write_u32_le(len as u32).await?;
-        self.write_all(&msg.encode_to_vec()).await?;
-        Ok(0)
-    }
-
-    async fn write_footer(&mut self, offset: u64) -> Result<()> {
-        self.write_u64_le(offset).await?;
-        self.write_all(b"LANCEIDX").await?;
-        Ok(())
     }
 }
 
@@ -94,7 +69,7 @@ impl Index for FlatIndex<'_> {
 #[async_trait]
 impl AnnIndex for FlatIndex<'_> {
     fn ann_index_type() -> AnnIndexType {
-        todo!()
+        AnnIndexType::Flat
     }
 
     fn dim(&self) -> usize {
--- rust/index/src/io.rs
+++ rust/index/src/io.rs
@@ -3,13 +3,30 @@
 use prost::Message;
 use std::io::Result;
 use tokio::io::{AsyncWrite, AsyncWriteExt};
+use async_trait::async_trait;
 
-/// Write protobuf to an open writer.
-pub async fn write_proto(msg: &impl Message, writer: &mut (impl AsyncWrite + Unpin)) -> Result<()> {
-    let len = msg.encoded_len();
 
-    writer.write_u32_le(len as u32).await?;
-    writer.write_all(msg.encode_to_vec().as_slice()).await?;
+#[async_trait]
+pub trait AsyncWriteProtoExt {
+    async fn write_pb(&mut self, msg: impl Message) -> Result<u64>;
 
-    Ok(())
+    /// Write footer with the offset to the root metadata block.
+    async fn write_footer(&mut self, offset: u64) -> Result<()>;
+}
+
+#[async_trait]
+impl<T: AsyncWrite + Unpin + std::marker::Send> AsyncWriteProtoExt for T {
+    async fn write_pb(&mut self, msg: impl Message) -> Result<u64> {
+        let len = msg.encoded_len();
+
+        self.write_u32_le(len as u32).await?;
+        self.write_all(&msg.encode_to_vec()).await?;
+        Ok(0)
+    }
+
+    async fn write_footer(&mut self, offset: u64) -> Result<()> {
+        self.write_u64_le(offset).await?;
+        self.write_all(b"LANCEIDX").await?;
+        Ok(())
+    }
 }
