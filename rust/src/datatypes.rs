e037ec9f91e7fcd5bb5d8fe99d657664b158f033
Lei Xu <lei@eto.ai>
2023-01-05 20:28:56 -0800

    add list of list test

--- rust/src/datatypes.rs
+++ rust/src/datatypes.rs
@@ -440,4 +440,18 @@ mod tests {
         assert_eq!(&field.data_type(), arrow_field.data_type());
         assert_eq!(ArrowField::try_from(&field).unwrap(), arrow_field);
     }
+
+    #[test]
+    fn test_list_of_list() {
+        let arrow_field = ArrowField::new(
+            "data",
+            DataType::List(Box::new(ArrowField::new("item", DataType::List(
+                Box::new(ArrowField::new("item", DataType::Float16, true))
+            ), true))),
+            true,
+        );
+        let field = Field::try_from(&arrow_field).unwrap();
+        assert_eq!(ArrowField::try_from(&field).unwrap(), arrow_field);
+    }
+
 }
