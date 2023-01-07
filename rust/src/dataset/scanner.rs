5f096a30c79c4a22ec3d0727053e9cf074cc2544
Lei Xu <lei@eto.ai>
2023-01-07 15:50:56 -0800

    add scanner

--- rust/src/dataset/scanner.rs
+++ rust/src/dataset/scanner.rs
@@ -1,23 +1,74 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use std::sync::Arc;
+
 use arrow_array::{RecordBatch, RecordBatchReader};
-use arrow_schema::{ArrowError, SchemaRef};
+use arrow_schema::{ArrowError, Schema as ArrowSchema, SchemaRef};
+
+use super::Dataset;
+use crate::datatypes::Schema;
+use crate::error::Result;
 
 /// Dataset Scanner
-pub struct Scanner {}
+#[derive(Debug)]
+pub struct Scanner<'a> {
+    dataset: &'a Dataset,
 
-impl Scanner {
-    pub fn new() -> Self {
-        Self {}
+    projections: Schema,
+
+    batch_size: usize,
+
+    // filter: how to present filter
+    limit: Option<i64>,
+    offset: Option<i64>,
+}
+
+impl<'a> Scanner<'a> {
+    pub fn new(dataset: &'a Dataset) -> Self {
+        Self {
+            dataset,
+            projections: dataset.schema().clone(),
+            batch_size: 1024,
+            limit: None,
+            offset: None,
+        }
+    }
+
+    pub fn project(&mut self, columns: &[&str]) -> Result<&mut Self> {
+        self.projections = self.dataset.schema().project(columns)?;
+        Ok(self)
+    }
+
+    pub fn limit(&mut self, limit: i64, offset: Option<i64>) -> &mut Self {
+        self.limit = Some(limit);
+        self.offset = offset;
+        self
     }
 }
 
-impl RecordBatchReader for Scanner {
+impl<'a> RecordBatchReader for Scanner<'a> {
     fn schema(&self) -> SchemaRef {
-        todo!()
+        Arc::new(ArrowSchema::from(&self.projections))
     }
 }
 
-impl Iterator for Scanner {
-    type Item = Result<RecordBatch, ArrowError>;
+impl<'a> Iterator for Scanner<'a> {
+    type Item = std::result::Result<RecordBatch, ArrowError>;
 
     fn next(&mut self) -> Option<Self::Item> {
         todo!()
