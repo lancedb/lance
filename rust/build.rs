330b38cbaf04c78d57119c5b616af32153afc837
Lei Xu <lei@eto.ai>
2023-01-07 11:30:49 -0800

    fix cargo publish

--- rust/build.rs
+++ rust/build.rs
@@ -2,8 +2,7 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=protos");
-    println!("cargo:rerun-if-changed=../protos");
 
-    prost_build::compile_protos(&["../protos/format.proto"], &["../protos"])?;
+    prost_build::compile_protos(&["./protos/format.proto"], &["./protos"])?;
     Ok(())
 }
