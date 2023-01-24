6cc813d1460a2f25382d89e0808394ff1797ba5e
Lei Xu <lei@eto.ai>
2023-01-24 14:47:44 -0800

    compute word2vec

--- python/benchmarks/spotify/datagen.py
+++ python/benchmarks/spotify/datagen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #
 
+import os
 
 import lance
 import pyarrow as pa
@@ -9,22 +10,16 @@ from gensim.models.word2vec import Word2Vec
 
 
 def generate_embeddings(col: pd.Series, **hyper_params) -> pd.Series:
-    unique_tracks = col.unique()
-    print(type(unique_tracks), unique_tracks.dtype)
     # unique_tracks
     sentences = []
-    for s in unique_tracks:
-        if isinstance(s, float):
+    for s in col:
+        if not isinstance(s, str):
             continue
         # We could prob do more cleaning here
-        sentences.append(s.split())
-    # sentences = [s.split() for s in unique_tracks]
-    # print(sentences)
-    w2v = Word2Vec(sentences, **hyper_params)
-
-    print(w2v)
-    print(w2v.wv.index_to_key)
-    w2v.wv[sentences[0]]
+        sentences.append(s)
+    model = Word2Vec(sentences, **hyper_params)
+    print(f"Vector space size: {len(model.wv.index_to_key)}")
+    print(model.wv[sentences[0]])
 
 
 def main():
@@ -37,13 +32,19 @@ def main():
 
     # Hyper parameters from https://outerbounds.com/docs/recsys-tutorial-L4/
     embeddings = generate_embeddings(
-        df["track"], min_count=3, epochs=30, vector_size=48, window=10, ns_exponent=0.75
+        df["track"],
+        min_count=3,
+        epochs=30,
+        vector_size=48,
+        window=10,
+        ns_exponent=0.75,
+        workers=os.cpu_count(),
     )
     df["embeddings"] = embeddings
     print(embeddings)
     table = pa.Table.from_pandas(df)
 
-    #lance.write_dataset(table, "spotify.lance")
+    # lance.write_dataset(table, "spotify.lance")
 
 
 if __name__ == "__main__":
