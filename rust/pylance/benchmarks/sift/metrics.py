d2b43cd9becf7eb46d2eb33fefb48de5532aebfc
Chang She <759245+changhiskhan@users.noreply.github.com>
2023-01-28 19:43:42 -0800

    add a test_dataset function to compute the recall for lance (#479)

--- rust/pylance/benchmarks/sift/metrics.py
+++ rust/pylance/benchmarks/sift/metrics.py
@@ -1,3 +1,5 @@
+import duckdb
+import lance
 import numpy as np
 
 
@@ -58,3 +60,27 @@ def test(nsamples=100):
         results.append(l2_part(mat, q, 10))
     rs = recall(np.array(actual_sorted), np.array(results))
     assert np.abs(rs.mean() - 1.0) < 1e-3
+
+
+def test_dataset(uri, nsamples=100, k=10):
+    dataset = lance.dataset(uri)
+    tbl = dataset.to_table()
+    v = tbl["vector"].combine_chunks()
+    all_vectors = v.values.to_numpy().reshape(len(tbl), v.type.list_size)
+
+    query_vectors = duckdb.query(f"SELECT vector FROM tbl USING SAMPLE {nsamples}").to_df()
+    query_vectors = np.array([np.array(x) for x in query_vectors.vector.values])
+
+    actual_sorted = []
+    results = []
+
+    for i in range(nsamples):
+        q = query_vectors[i, :]
+        actual_sorted.append(l2_sort(all_vectors, q))
+        results.append(
+            dataset.to_table(
+                nearest={"column": "vector", "q": q, "k": k}
+            )["score"].combine_chunks().to_numpy()
+        )
+    return recall(np.array(actual_sorted), np.array(results))
+
