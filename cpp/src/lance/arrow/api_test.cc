52aaeae433e0eb5828362153f7845d0dce800ba6
Lei Xu <eddyxu@gmail.com>
2023-01-30 22:32:15 -0800

    Sayonara C++

--- cpp/src/lance/arrow/api_test.cc
+++
@@ -1,7 +0,0 @@
-/// Test the public header does not leak internal implementations.
-///
-/// We should only include headers under `/cpp/include`.
-///
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("Read full table") {}
