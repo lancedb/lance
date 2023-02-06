c3e7bce5eb90a45662be196c981a7b487e1b57f2
Lei Xu <lei@eto.ai>
2023-02-06 09:37:29 -0800

    Remove duckdb extension based on c++ codebase. (#530)
    Remove duckdb extension

--- integration/duckdb/cuda/build_lance.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=/usr/local/lib
-export PATH=/usr/local/cmake/bin:/opt/rh/devtoolset-11/root/usr/bin:/opt/python/cp310-cp310/bin:$PATH
-gcc --version
-
-pushd /code
-cmake -B cuda-build -DLANCE_BUILD_CUDA=TRUE
-make -C cuda-build -j
