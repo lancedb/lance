9f0bc8b0c9bca3dac429f6de71fe744100aaaf26
Lei Xu <eddyxu@gmail.com>
2023-02-06 09:37:56 -0800

    add error

--- integration/duckdb/src/lib.rs
+++ integration/duckdb/src/lib.rs
@@ -1,20 +1,43 @@
+// Copyright 2023 Lance Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+use std::ffi::{c_char, c_void};
+
+use duckdb_extension_framework::duckly::duckdb_library_version;
+use duckdb_extension_framework::Database;
+
+pub mod error;
+
+use error::Result;
 
 #[no_mangle]
 pub extern "C" fn lance_version_rust() -> *const c_char {
-    unsafe {  }
+    unsafe { duckdb_library_version() }
 }
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+#[no_mangle]
+pub unsafe extern "C" fn lance_init_rust(db: *mut c_void) {
+    init(db).expect("init failed");
+}
+
+unsafe fn init(db: *mut c_void) -> Result<()> {
+    let db = Database::from_cpp_duckdb(db);
+    let table_function = build_table_function_def();
+    let connection = db.connect()?;
+    connection.register_table_function(table_function)?;
+    Ok(())
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
