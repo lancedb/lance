94f26f2d0e324ac50b141e929213de439f7a5e37
Chang She <759245+changhiskhan@users.noreply.github.com>
2023-02-09 20:00:52 -0800

    update metrics (#547)

--- python/benchmarks/sift/metrics.py
+++ python/benchmarks/sift/metrics.py
@@ -29,15 +29,13 @@ def recall(actual_sorted: np.ndarray, results: np.ndarray):
     Recall-at-k
     """
     len = results.shape[1]
-    t = actual_sorted[:, len - 1] + 1e-3
-    recall_at_k = (results <= t[:, None]).sum(axis=1) * 1.0 / len
+    recall_at_k = np.array([np.sum([1 if id in results[i, :] else 0 for id in row]) * 1.0 / len
+                            for i, row in enumerate(actual_sorted)])
     return (recall_at_k.mean(), recall_at_k.std(), recall_at_k)
 
 
-def l2_sort(mat, q):
+def l2_argsort(mat, q):
     """
-    Compute the actual euclidean squared
-
     Parameters
     ----------
     mat: ndarray
@@ -45,40 +43,7 @@ def l2_sort(mat, q):
     q: ndarray
         shape is d, this is the query vector
     """
-    return np.sort(((mat - q) ** 2).sum(axis=1))
-
-
-def l2_part(mat, q, k):
-    """
-    Compute topk by partition
-
-    Parameters
-    ----------
-    mat: ndarray
-        shape is (n, d) where n is number of vectors and d is number of dims
-    q: ndarray
-        shape is d, this is the query vector
-    k: int
-        topk
-    """
-    return np.partition(((mat - q) ** 2).sum(axis=1), k)[:k]
-
-
-def test(nsamples=100):
-    """
-    make sure the recall computation is correct.
-    if we just use np.partition, we should have perfect recall
-    """
-    mat = np.random.randn(1000000, 128)
-    mat = mat / np.sqrt((mat**2).sum(axis=1))[:, None]  # to unit vectors
-    actual_sorted = []
-    results = []
-    for _ in range(nsamples):
-        q = mat[np.random.randint(mat.shape[0]), :]
-        actual_sorted.append(l2_sort(mat, q))
-        results.append(l2_part(mat, q, 10))
-    rs = recall(np.array(actual_sorted), np.array(results))
-    assert np.abs(rs.mean() - 1.0) < 1e-3
+    return np.argsort(((mat - q) ** 2).sum(axis=1))
 
 
 def test_dataset(
@@ -88,31 +53,21 @@ def test_dataset(
     tbl = dataset.to_table()
     v = tbl["vector"].combine_chunks()
     all_vectors = v.values.to_numpy().reshape(len(tbl), v.type.list_size)
+    # all_vectors = all_vectors / np.linalg.norm(all_vectors, axis=1)[:, None]
 
     query_vectors = duckdb.query(
         f"SELECT vector FROM tbl USING SAMPLE {nsamples}"
     ).to_df()
     query_vectors = np.array([np.array(x) for x in query_vectors.vector.values])
+    # query_vectors = query_vectors / np.linalg.norm(query_vectors, axis=1)[:, None]
 
     actual_sorted = []
     results = []
 
     tot = 0
-
-    q = query_vectors[0, :]
-    dataset.to_table(
-        nearest={
-            "column": "vector",
-            "q": q,
-            "k": k,
-            "nprobes": nprobes,
-            "refine_factor": refine_factor,
-        }
-    )
-
     for i in range(nsamples):
         q = query_vectors[i, :]
-        actual_sorted.append(l2_sort(all_vectors, q))
+        actual_sorted.append(l2_argsort(all_vectors, q)[:k])
         start = time.time()
         rs = dataset.to_table(
             nearest={
@@ -125,7 +80,7 @@ def test_dataset(
         )
         end = time.time()
         tot += end - start
-        results.append(rs["score"].combine_chunks().to_numpy())
+        results.append(rs["id"].combine_chunks().to_numpy())
     avg_latency = tot / nsamples
     return recall(np.array(actual_sorted), np.array(results)), avg_latency
 
