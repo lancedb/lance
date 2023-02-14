1f14f840b40d80eb978493abaffbb80c1a716a7f
Chang She <759245+changhiskhan@users.noreply.github.com>
2023-02-13 17:19:16 -0800

    docstring

--- python/python/lance/vector.py
+++ python/python/lance/vector.py
@@ -1,5 +1,3 @@
-"""Embedding vector utilities"""
-
 #  Copyright 2023 Lance Developers
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +10,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+"""Embedding vector utilities"""
 
 from typing import Optional, Union
 
