6283eb0eff949d99c27bfa2a3755df6e372f7262
Chang She <759245+changhiskhan@users.noreply.github.com>
2023-02-13 16:13:41 -0800

    [python] Add __version__ to python package. Closes #561 (#572)
    Pass the version from the pylance Cargo.toml and make it
    accessible as `lance.__version__` in python.

--- python/python/lance/__init__.py
+++ python/python/lance/__init__.py
@@ -14,7 +14,7 @@
 
 from typing import Optional
 
-from .dataset import LanceDataset, write_dataset
+from .dataset import LanceDataset, write_dataset, __version__
 
 
 def dataset(uri: str, version: Optional[int] = None) -> LanceDataset:
