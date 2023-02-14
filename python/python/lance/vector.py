6514345a55b587981aea875816f1242fd1105662
Chang She <759245+changhiskhan@users.noreply.github.com>
2023-02-13 16:08:11 -0800

    utility to convert to pyarrow table with vectors

---
+++ python/python/lance/vector.py
@@ -0,0 +1,84 @@
+"""Embedding vector utilities"""
+from typing import Union, Optional
+
+import numpy as np
+import pyarrow as pa
+
+#  Copyright 2023 Lance Developers
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+def _normalize_vectors(vectors, ndim):
+    if ndim is None:
+        ndim = len(next(iter(vectors)))
+    values = np.array(vectors, dtype="float32").ravel()
+    return pa.FixedSizeListArray.from_arrays(values, list_size=ndim)
+
+
+def _validate_ndim(values, ndim):
+    for v in values:
+        if ndim is None:
+            ndim = len(v)
+        else:
+            if ndim != len(v):
+                raise ValueError(f"Expected {ndim} dimensions but got {len(v)} for {v}")
+    return ndim
+
+
+def to_vectors(data: Union[dict, list, np.ndarray],
+               names: Optional[Union[str, list[str]]] = None,
+               ndim: Optional[int] = None,
+               check_ndim: bool = True) -> pa.Table:
+    """
+    Create a pyarrow Table containing vectors.
+    Vectors are created as FixedSizeListArray's in pyarrow with Float32 values.
+
+    Parameters
+    ----------
+    data: dict, list, or np.ndarray
+        If dict, the keys are added as "id" column
+        If list, then each element is assumed to be a vector
+        If ndarray, then each row is assumed to be a vector
+    names: str or list, optional
+        If data is dict, then names should be a list of 2 str; default ["id", "vector"]
+        If data is list or ndarray, then names should be str; default "vector"
+    ndim: int, optional
+        Number of dimensions of the vectors. Inferred if omitted.
+    check_ndim: bool, default True
+        Whether to verify that all vectors have the same length
+    """
+    if isinstance(data, dict):
+        if names is None:
+            names = ["id", "vector"]
+        elif not isinstance(names, (list, tuple)) and len(names) == 2:
+            raise ValueError("If data is a dict, names must be a list or tuple of 2 strings")
+        values = list(data.values())
+        if check_ndim:
+            ndim = _validate_ndim(values, ndim)
+        vectors = _normalize_vectors(values, ndim)
+        ids = pa.array(data.keys())
+        arrays = [ids, vectors]
+    elif isinstance(data, (list, np.ndarray)):
+        if names is None:
+            names = ["vector"]
+        elif isinstance(names, str):
+            names = [names]
+        elif not isinstance(names, (list, tuple)) and len(names) == 1:
+            raise ValueError(f"names cannot be more than 1 got {len(names)}")
+        if check_ndim:
+            ndim = _validate_ndim(data, ndim)
+        vectors = _normalize_vectors(data, ndim)
+        arrays = [vectors]
+    else:
+        raise NotImplementedError(f"data must be dict, list, or ndarray, got {type(data)} instead")
+    return pa.Table.from_arrays(arrays, names=names)
---
+++ python/python/tests/test_vector.py
@@ -0,0 +1,101 @@
+
+import numpy as np
+import pyarrow as pa
+import pytest
+
+from lance.vector import to_vectors
+
+#  Copyright 2023 Lance Developers
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+def test_dict():
+    ids, vectors = _create_data()
+    dd = dict(zip(ids, vectors))
+    tbl = to_vectors(dd)
+    expected = [pa.array(ids), _to_vec(vectors)]
+    assert_table(tbl, expected)
+
+    new_tbl = to_vectors(dd, names=["foo", "bar"])
+    assert new_tbl.column_names == ["foo", "bar"]
+
+    with pytest.raises(ValueError):
+        ids, vectors = _create_bad_dims()
+        dd = dict(zip(ids, vectors))
+        to_vectors(dd)
+
+
+def test_list():
+    _, vectors = _create_data()
+    tbl = to_vectors(vectors)
+    expected = [_to_vec(vectors)]
+    assert_table(tbl, expected)
+
+    with pytest.raises(ValueError):
+        _, vectors = _create_bad_dims()
+        to_vectors(vectors)
+
+
+def test_ndarray():
+    _, vectors = _create_data()
+    tbl = to_vectors(np.array(vectors))
+    expected = [_to_vec(vectors)]
+    assert_table(tbl, expected)
+
+    with pytest.raises(ValueError):
+        _, vectors = _create_bad_dims()
+        to_vectors(np.array(vectors))
+
+
+def assert_table(tbl, expected_arrays, names=None):
+    if names is None:
+        if len(expected_arrays) == 1:
+            names = ["vector"]
+        else:
+            names = ["id", "vector"]
+
+    for i, n in enumerate(names):
+        assert_array_eq(tbl[n], expected_arrays[i])
+
+
+def assert_array_eq(left: pa.Array, right: pa.Array):
+    if isinstance(left, pa.ChunkedArray):
+        left = left.combine_chunks()
+    if isinstance(right, pa.ChunkedArray):
+        right = right.combine_chunks()
+    if pa.types.is_float32(left.type):
+        assert np.all(np.abs(left.to_numpy(zero_copy_only=False) -
+                             right.to_numpy(zero_copy_only=False)) < 1e-6)
+    if pa.types.is_fixed_size_list(left.type):
+        assert_array_eq(left.values, right.values)
+    else:
+        assert np.all(left.to_numpy(False) == right.to_numpy(False))
+
+
+def _create_data():
+    ids = list(range(10))
+    vectors = np.random.randn(10, 8)
+    return ids, vectors
+
+
+def _create_bad_dims():
+    ids = list(range(10))
+    vectors = [np.random.randn(8) for _ in ids]
+    vectors[5] = np.random.randn(5)
+    return ids, vectors
+
+
+def _to_vec(lst):
+    return pa.FixedSizeListArray.from_arrays(
+        pa.array(np.array(lst).ravel(), type=pa.float32()),
+        list_size=8)
