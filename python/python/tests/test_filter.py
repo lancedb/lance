652dc48772f787eeb691c97f6c67acf59bc8237e
Chang She <759245+changhiskhan@users.noreply.github.com>
2023-02-24 22:34:47 -0800

    add duckdb as test dependency and add duckdb unit test

--- python/python/tests/test_filter.py
+++ python/python/tests/test_filter.py
@@ -30,9 +30,13 @@ import string
 
 import lance
 import numpy as np
+import pandas as pd
+import pandas.testing as tm
 import pyarrow as pa
 import pyarrow.compute as pc
 import pytest
+
+from lance.vector import vec_to_table
 
 
 def create_table(nrows=100):
@@ -88,3 +92,46 @@ def test_compound(dataset):
             assert dataset.to_table(filter=compound) == dataset.to_table().filter(compound)
             compound = expr | other_expr
             assert dataset.to_table(filter=compound) == dataset.to_table().filter(compound)
+
+
+def create_table_for_duckdb(nvec=10000, ndim=768):
+    mat = np.random.randn(nvec, ndim)
+    price = (np.random.rand(nvec) + 1) * 100
+
+    def gen_str(n):
+        return "".join(random.choices("abc"))
+
+    meta = np.array([gen_str(1) for _ in range(nvec)])
+    tbl = (
+        vec_to_table(data=mat)
+        .append_column("price", pa.array(price))
+        .append_column("meta", pa.array(meta))
+        .append_column("id", pa.array(range(nvec)))
+    )
+    return tbl
+
+
+def test_duckdb(tmp_path):
+    duckdb = pytest.importorskip("duckdb")
+    tbl = create_table_for_duckdb()
+    ds = lance.write_dataset(tbl, str(tmp_path))
+
+    actual = duckdb.query("SELECT id, meta, price FROM ds WHERE id==1000").to_df()
+    expected = duckdb.query("SELECT id, meta, price FROM ds").to_df()
+    expected = expected[expected.id == 1000].reset_index()
+    assert tm.assert_frame_equal(actual, expected)
+
+    actual = duckdb.query("SELECT id, meta, price FROM ds WHERE id=1000").to_df()
+    expected = duckdb.query("SELECT id, meta, price FROM ds").to_df()
+    expected = expected[expected.id == 1000].reset_index()
+    assert tm.assert_frame_equal(actual, expected)
+
+    actual = duckdb.query("SELECT id, meta, price FROM ds WHERE price>20.0 and price<=90").to_df()
+    expected = duckdb.query("SELECT id, meta, price FROM ds").to_df()
+    expected = expected[(expected.price > 20.0) & (expected.price <= 90)].reset_index()
+    assert tm.assert_frame_equal(actual, expected)
+
+    actual = duckdb.query("SELECT id, meta, price FROM ds WHERE meta=='aa'").to_df()
+    expected = duckdb.query("SELECT id, meta, price FROM ds").to_df()
+    expected = expected[expected.meta == "aa"].reset_index()
+    assert tm.assert_frame_equal(actual, expected)
