a46889a304dfadc224a0e7e81d0bab30c77c6c78
Renkai Ge <renkaige@tubi.tv>
2023-03-05 09:41:26 +0800

    JVM passsed

--- scala/src/lib.rs
+++ scala/src/lib.rs
@@ -1,3 +1,4 @@
+use std::mem::ManuallyDrop;
 use arrow::array::{ArrayData, Int32Array};
 use arrow::ffi::{ArrowArray, FFI_ArrowArray, FFI_ArrowSchema};
 use jni::JNIEnv;
@@ -19,6 +20,9 @@ pub extern "system" fn Java_lance_JNI_saveToLance<'local>(
     path: JString<'local>,
     vec: JObject,
     allocator: JObject) {
+    // references:
+    // https://github.com/apache/arrow-rs/blob/231ae9b31769b62da368b9f1eb355a840540cb06/arrow/src/ffi.rs#L567
+    // https://arrow.apache.org/docs/java/cdata.html#java-to-c
     let path: String = env
         .get_string(&path)
         .expect("Couldn't get java string!")
@@ -26,11 +30,11 @@ pub extern "system" fn Java_lance_JNI_saveToLance<'local>(
     let schema = FFI_ArrowSchema::empty();
     let array = FFI_ArrowArray::empty();
 
-    let schema = Box::new(schema);
-    let array = Box::new(array);
+    let schema = Box::new(ManuallyDrop::new(schema));
+    let array = Box::new(ManuallyDrop::new(array));
 
-    let schema_ptr = &*schema as *const FFI_ArrowSchema;
-    let array_ptr = &*array as *const FFI_ArrowArray;
+    let schema_ptr = &**schema as *const FFI_ArrowSchema;
+    let array_ptr = &**array as *const FFI_ArrowArray;
 
     let schema_jlong = schema_ptr as i64;
     let array_jlong = array_ptr as i64;
