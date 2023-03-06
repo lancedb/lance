bb82fb909ad6ea3e8799d0a54f64c9b4f03b94a5
Lei Xu <eddyxu@gmail.com>
2023-03-06 12:56:17 -0800

    add test data from numpy

--- rust/src/arrow/linalg.rs
+++ rust/src/arrow/linalg.rs
@@ -44,9 +44,11 @@ impl SingularValueDecomposition for FixedSizeListArray {
     type Sigma = Float32Array;
 
     fn svd(&self) -> Result<(Self::Matrix, Self::Sigma, Self::Matrix)> {
-        use lapacke::{sgesvd, Layout};
+        #[cfg(target_os = "macos")]
+        use accelerate_src;
+        use lapack::sgesvd;
 
-        if !matches!(self.data_type(), DataType::Float32) {
+        if !matches!(self.value_type(), DataType::Float32) {
             return Err(Error::Arrow(format!(
                 "SVD only supports f32 type, got {}",
                 self.data_type()
@@ -57,41 +59,50 @@ impl SingularValueDecomposition for FixedSizeListArray {
         let n = self.value_length();
         let values = self.values();
         let flatten_values: &Float32Array = as_primitive_array(values.as_ref());
+
         // Solving: A = U * Sigma * Vt
         //
         // TODO: Lapacke requires a mutable reference of `A`.
         // How can we get mutable reference without copying data?
         let mut a = flatten_values.values().to_vec();
         // f32 array builder for matrix U.
-        let mut u_builder = Float32Builder::with_capacity((m * m) as usize);
+        let mut u = vec![0.0; (m * m) as usize];
         // f32 array builder for matrix V_T
-        let mut vt_builder = Float32Builder::with_capacity((n * n) as usize);
-        let mut sigma_builder = Float32Builder::with_capacity(n as usize);
-        let mut superb = vec![0_f32; min(m, n) as usize - 1];
+        let mut vt = vec![0.0; (n*n) as usize];
+        let mut sigma = vec![0.0;n as usize];
+
+        let mut work = vec![0_f32; 1];
+        let lwork: i32 = -1;
+        let mut info: i32 = -1;
 
         unsafe {
             sgesvd(
-                Layout::RowMajor,
                 b'A',
                 b'A',
                 m,
                 n,
                 a.as_mut_slice(),
-                m as i32,
-                sigma_builder.values_slice_mut(),
-                u_builder.values_slice_mut(),
+                m,
+                sigma.as_mut_slice(),
+                u.as_mut_slice(),
+                m,
+                vt.as_mut_slice(),
                 n,
-                vt_builder.values_slice_mut(),
-                n,
-                superb.as_mut_slice(),
+                work.as_mut_slice(),
+                lwork,
+                &mut info,
             );
         }
+        println!("Info value: {info} work={:?}", work);
+        if info > 0 {
+            println!("Failed to compute sgesvd");
+        }
 
-        let u_values = u_builder.finish();
+        let u_values = Float32Array::from_iter_values(u);
         let u = FixedSizeListArray::try_new(&u_values, m)?;
-        let vt_values = vt_builder.finish();
+        let vt_values = Float32Array::from_iter_values(vt);
         let vt = FixedSizeListArray::try_new(&vt_values, n)?;
-        let sigma = sigma_builder.finish();
+        let sigma = Float32Array::from_iter_values(sigma);
         Ok((u, sigma, vt))
     }
 }
@@ -101,5 +112,53 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_svd() {}
+    fn test_svd() {
+        let values = Float32Array::from_iter_values([
+            8.79, 9.93, 9.83, 5.45, 3.16, 6.11, 6.91, 5.04, -0.27, 7.98, -9.15, -7.93, 4.86, 4.85,
+            3.01, 9.57, 1.64, 8.83, 0.74, 5.80, -3.49, 4.02, 9.80, 10.00, 4.27, 9.84, 0.15, -8.99,
+            -6.02, -5.31,
+        ]);
+        let a = FixedSizeListArray::try_new(&values, 5).unwrap();
+
+        let (u, sigma, vt) = a.svd().unwrap();
+        let expected_u = Float32Array::from_iter_values([
+            -0.59114238,
+            0.26316781,
+            0.35543017,
+            0.31426436,
+            0.22993832,
+            0.55075318,
+            -0.39756679,
+            0.24379903,
+            -0.22239,
+            -0.75346615,
+            -0.36358969,
+            0.18203479,
+            -0.03347897,
+            -0.60027258,
+            -0.45083927,
+            0.23344966,
+            -0.30547573,
+            0.53617327,
+            -0.4297069,
+            0.23616681,
+            -0.68586286,
+            0.33186002,
+            0.16492763,
+            -0.38966287,
+            -0.46974792,
+            -0.3508914,
+            0.3874446,
+            0.15873556,
+            -0.51825744,
+            -0.46077223,
+            0.29335876,
+            0.57626212,
+            -0.02085292,
+            0.37907767,
+            -0.6525516,
+            0.10910681,
+        ]);
+        assert_eq!(as_primitive_array(u.values().as_ref()), &expected_u);
+    }
 }
