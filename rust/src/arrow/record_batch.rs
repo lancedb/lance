09e91bd12110f99d1f8f5d5b5e05e90f00db5e8d
Lei Xu <eddyxu@gmail.com>
2023-04-13 18:02:03 -0700

    handle null

--- rust/src/arrow/record_batch.rs
+++ rust/src/arrow/record_batch.rs
@@ -15,9 +15,8 @@
 //! Additional utility for [`RecordBatch`]
 //!
 
-use arrow_array::{cast::as_struct_array, Array, RecordBatch, RecordBatchReader, StructArray};
+use arrow_array::{RecordBatch, RecordBatchReader};
 use arrow_schema::{ArrowError, SchemaRef};
-use arrow_select::interleave::interleave;
 
 use crate::Result;
 
@@ -49,39 +48,6 @@ impl RecordBatchBuffer {
     pub fn finish(&self) -> Result<Vec<RecordBatch>> {
         Ok(self.batches.clone())
     }
-
-    /// Make interleaving indices to be used with [`arrow_select::interleave::interleave`].
-    ///
-    fn make_interleaving_indices(&self, indices: &[usize]) -> Vec<(usize, usize)> {
-        let mut lengths = vec![0_usize];
-        for batch in self.batches.iter() {
-            lengths.push(lengths.last().unwrap() + batch.num_rows());
-        }
-
-        let mut idx = vec![];
-        for i in indices {
-            let batch_id = match lengths.binary_search(&i) {
-                Ok(i) => i,
-                Err(i) => i - 1,
-            };
-            idx.push((batch_id, i - lengths[batch_id]));
-        }
-        idx
-    }
-
-    /// Take rows by indices.
-    pub fn take_rows(&self, indices: &[usize]) -> Result<RecordBatch> {
-        let arrays = self
-            .batches
-            .iter()
-            .map(|batch| StructArray::from(batch.clone()))
-            .collect::<Vec<_>>();
-        let refs = arrays.iter().map(|a| a as &dyn Array).collect::<Vec<_>>();
-
-        let interleaving_indices = self.make_interleaving_indices(indices);
-        let array = interleave(&refs, &interleaving_indices)?;
-        Ok(as_struct_array(&array).into())
-    }
 }
 
 impl RecordBatchReader for RecordBatchBuffer {
@@ -108,47 +74,5 @@ impl FromIterator<RecordBatch> for RecordBatchBuffer {
     fn from_iter<T: IntoIterator<Item = RecordBatch>>(iter: T) -> Self {
         let batches = iter.into_iter().collect::<Vec<_>>();
         Self::new(batches)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    use std::sync::Arc;
-
-    use arrow_array::{Float64Array, Int32Array, StringArray};
-    use arrow_schema::{DataType, Field, Schema};
-
-    #[test]
-    fn test_take() {
-        let schema = Arc::new(Schema::new(vec![
-            Field::new("i", DataType::Int32, false),
-            Field::new("f", DataType::Float64, false),
-            Field::new("s", DataType::Utf8, false),
-        ]));
-
-        let batch_buffer: RecordBatchBuffer = (0..5)
-            .map(|v| {
-                let values = (v * 10..v * 10 + 10).collect::<Vec<_>>();
-                RecordBatch::try_new(
-                    schema.clone(),
-                    vec![
-                        Arc::new(Int32Array::from_iter(values.iter().copied())),
-                        Arc::new(Float64Array::from_iter(values.iter().map(|v| *v as f64))),
-                        Arc::new(StringArray::from_iter_values(
-                            values.iter().map(|v| format!("str_{}", v)),
-                        )),
-                    ],
-                )
-                .unwrap()
-            })
-            .collect();
-        let batch = batch_buffer.take_rows(&[10, 14, 30, 49, 0, 22]).unwrap();
-        assert_eq!(batch.num_rows(), 6);
-        assert_eq!(
-            batch.column_by_name("i").unwrap().as_ref(),
-            &Int32Array::from(vec![10, 14, 30, 49, 0, 22])
-        );
     }
 }
--- rust/src/dataset/hash_joiner.rs
+++ rust/src/dataset/hash_joiner.rs
@@ -16,9 +16,13 @@
 
 use std::collections::HashMap;
 
-use arrow_array::{Array, RecordBatch, RecordBatchReader};
+use arrow_array::StructArray;
+use arrow_array::{
+    builder::UInt64Builder, cast::as_struct_array, Array, RecordBatch, RecordBatchReader,
+};
+use arrow_select::{concat::concat_batches, take::take};
 
-use crate::arrow::{hash, RecordBatchBuffer};
+use crate::arrow::hash;
 use crate::{Error, Result};
 
 /// `HashJoiner` does hash join on two datasets.
@@ -26,7 +30,7 @@ pub(super) struct HashJoiner {
     /// Hash value to row index map.
     index_map: HashMap<u64, usize>,
 
-    data: RecordBatchBuffer,
+    batch: RecordBatch,
 
     on_column: String,
 }
@@ -37,57 +41,127 @@ impl HashJoiner {
         // Check column exist
         reader.schema().field_with_name(on)?;
 
+        // Hold all data in memory for simple implementation. Can do external sort later.
+        let batches = reader.collect::<std::result::Result<Vec<RecordBatch>, _>>()?;
+        if batches.is_empty() {
+            return Err(Error::IO("HashJoiner: No data".to_string()));
+        };
+        let batch = concat_batches(&batches[0].schema(), &batches)?;
+
         Ok(Self {
             index_map: HashMap::new(),
-
-            // Hold all data in memory for simple implementation. Can do external sort later.
-            data: reader.collect::<std::result::Result<RecordBatchBuffer, _>>()?,
-
+            batch,
             on_column: on.to_string(),
         })
     }
 
     /// Build the hash index.
     pub(super) fn build(&mut self) -> Result<()> {
-        let mut start_idx = 0;
+        let key_column = self
+            .batch
+            .column_by_name(&self.on_column)
+            .ok_or_else(|| Error::IO(format!("HashJoiner: Column {} not found", self.on_column)))?;
 
-        for batch in &self.data.batches {
-            let key_column = batch.column_by_name(&self.on_column).ok_or_else(|| {
-                Error::IO(format!("HashJoiner: Column {} not found", self.on_column))
-            })?;
-
-            let hashes = hash(key_column.as_ref())?;
-            for (i, hash_value) in hashes.iter().enumerate() {
-                let idx = start_idx + i;
-                let Some(key) = hash_value else {
+        let hashes = hash(key_column.as_ref())?;
+        for (i, hash_value) in hashes.iter().enumerate() {
+            let Some(key) = hash_value else {
                     continue;
                 };
 
-                if self.index_map.contains_key(&key) {
-                    return Err(Error::IO(format!("HashJoiner: Duplicate key {}", key)));
-                }
-                // TODO: use [`HashMap::try_insert`] when it's stable.
-                self.index_map.insert(key, idx);
+            if self.index_map.contains_key(&key) {
+                return Err(Error::IO(format!("HashJoiner: Duplicate key {}", key)));
             }
-            start_idx += batch.num_rows();
+            // TODO: use [`HashMap::try_insert`] when it's stable.
+            self.index_map.insert(key, i);
         }
+
         Ok(())
     }
 
     /// Collecting the data using the index column from left table.
     pub(super) fn collect(&self, index_column: &dyn Array) -> Result<RecordBatch> {
         let hashes = hash(index_column)?;
-        let mut indices: Vec<usize> = Vec::with_capacity(index_column.len());
+        let mut builder = UInt64Builder::with_capacity(index_column.len());
         for hash_value in hashes.iter() {
             let Some(key) = hash_value else {
+                builder.append_null();
                 continue;
             };
 
             if let Some(idx) = self.index_map.get(&key) {
-                indices.push(*idx);
+                builder.append_value(*idx as u64);
+            } else {
+                builder.append_null();
             }
         }
+        let indices = builder.finish();
 
-        todo!()
+        let struct_arr = StructArray::from(self.batch.clone());
+        let results = take(&struct_arr, &indices, None)?;
+        Ok(as_struct_array(&results).into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    use arrow_array::{Int32Array, StringArray, UInt32Array};
+    use arrow_schema::{DataType, Field, Schema};
+    use std::sync::Arc;
+
+    use crate::arrow::RecordBatchBuffer;
+
+    #[test]
+    fn test_joiner_collect() {
+        let schema = Arc::new(Schema::new(vec![
+            Field::new("i", DataType::Int32, false),
+            Field::new("s", DataType::Utf8, false),
+        ]));
+
+        let mut batch_buffer: RecordBatchBuffer = (0..5)
+            .map(|v| {
+                let values = (v * 10..v * 10 + 10).collect::<Vec<_>>();
+                RecordBatch::try_new(
+                    schema.clone(),
+                    vec![
+                        Arc::new(Int32Array::from_iter(values.iter().copied())),
+                        Arc::new(StringArray::from_iter_values(
+                            values.iter().map(|v| format!("str_{}", v)),
+                        )),
+                    ],
+                )
+                .unwrap()
+            })
+            .collect();
+        let mut joiner = HashJoiner::try_new(&mut batch_buffer, "i").unwrap();
+        joiner.build().unwrap();
+
+        let indices = UInt32Array::from_iter(&[
+            Some(15),
+            None,
+            Some(10),
+            Some(0),
+            None,
+            None,
+            Some(22),
+            Some(11111), // not found
+        ]);
+        let results = joiner.collect(&indices).unwrap();
+
+        assert_eq!(
+            results.column_by_name("s").unwrap().as_ref(),
+            &StringArray::from(vec![
+                Some("str_15"),
+                None,
+                Some("str_10"),
+                Some("str_0"),
+                None,
+                None,
+                Some("str_22"),
+                None  // 11111 not found
+            ])
+        );
     }
 }
