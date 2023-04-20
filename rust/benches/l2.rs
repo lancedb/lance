38ae6b3d618b1931681e81ae8dbb1518dab37cc2
Lei Xu <eddyxu@gmail.com>
2023-04-20 13:30:11 -0700

    fix l2 bench

--- rust/benches/l2.rs
+++ rust/benches/l2.rs
@@ -1,49 +1,52 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
+// Copyright 2023 Lance Developers.
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use arrow_arith::aggregate::sum;
 use arrow_arith::arithmetic::{multiply_dyn, subtract_dyn};
+use arrow_arith::arity::binary;
 use arrow_array::cast::as_primitive_array;
 use arrow_array::types::Float32Type;
 use arrow_array::{Array, Float32Array};
 use criterion::{criterion_group, criterion_main, Criterion};
+
+#[cfg(target_os = "linux")]
 use pprof::criterion::{Output, PProfProfiler};
 
-use lance::utils::distance::{l2::l2_distance_arrow, CosineDistance, Distance, L2Distance};
+use lance::linalg::l2::l2_distance_batch;
 use lance::utils::testing::generate_random_array;
+
+#[inline]
+fn l2_arrow(x: &Float32Array, y: &Float32Array) -> f32 {
+    0.0
+}
+
+#[inline]
+fn l2_arrow_arity(x: &Float32Array, y: &Float32Array) -> f32 {
+    0.0
+}
 
 fn bench_distance(c: &mut Criterion) {
     const DIMENSION: usize = 1024;
+    const TOTAL: usize = 1024 * 1024; // 1M vectors
 
-    let key = generate_random_array(DIMENSION as usize);
+    let key = generate_random_array(DIMENSION);
     // 1M of 1024 D vectors. 4GB in memory.
-    let target = generate_random_array(1024 * 1024 * DIMENSION as usize);
-    let dist = L2Distance::new();
+    let target = generate_random_array(TOTAL * DIMENSION);
 
     c.bench_function("L2 distance", |b| {
         b.iter(|| {
-            dist.distance(&key, &target, DIMENSION).unwrap();
-        })
-    });
-
-    c.bench_function("Cosine Distance", |b| {
-        let dist = CosineDistance::default();
-        b.iter(|| {
-            dist.distance(&key, &target, DIMENSION).unwrap();
+            l2_distance_batch(key.values(), target.values(), DIMENSION);
         })
     });
 
@@ -53,7 +56,7 @@ fn bench_distance(c: &mut Criterion) {
                 (0..target.len() / DIMENSION)
                     .map(|idx| {
                         let arr = target.slice(idx * DIMENSION, DIMENSION);
-                        l2_distance_arrow(&key, as_primitive_array(arr.as_ref()))
+                        l2_arrow(&key, as_primitive_array(arr.as_ref()))
                     })
                     .map(|d| Some(d)),
             )
@@ -76,9 +79,16 @@ fn bench_distance(c: &mut Criterion) {
     });
 }
 
+#[cfg(target_os = "linux")]
 criterion_group!(
     name=benches;
     config = Criterion::default().significance_level(0.1).sample_size(10)
         .with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    targets = bench_distance);
+
+#[cfg(not(target_os = "linux"))]
+criterion_group!(
+    name=benches;
+    config = Criterion::default().significance_level(0.1).sample_size(10);
     targets = bench_distance);
 criterion_main!(benches);
