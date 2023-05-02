33d8d55d7de9dbf786a183d22e016e7ccc48bf8b
Lei Xu <eddyxu@gmail.com>
2023-05-02 09:45:17 -0700

    add

--- python/src/updater.rs
+++ python/src/updater.rs
@@ -15,6 +15,8 @@
 use std::sync::Arc;
 
 use arrow::pyarrow::PyArrowConvert;
+use arrow_array::RecordBatch;
+use futures::TryFutureExt;
 use pyo3::{exceptions::*, prelude::*};
 
 use lance::dataset::updater::Updater as LanceUpdater;
@@ -22,11 +24,15 @@ use lance::dataset::updater::Updater as LanceUpdater;
 #[pyclass(name = "_Updater", module = "_lib")]
 pub struct Updater {
     inner: LanceUpdater,
+    rt: tokio::runtime::Runtime,
 }
 
 impl Updater {
     pub(super) fn new(updater: LanceUpdater) -> Self {
-        Self { inner: updater }
+        Self {
+            inner: updater,
+            rt: tokio::runtime::Runtime::new().unwrap(),
+        }
     }
 }
 
@@ -35,9 +41,9 @@ impl Updater {
     /// Return the next batch as input data.
     #[pyo3(signature=())]
     fn next(&mut self, py: Python<'_>) -> PyResult<Option<PyObject>> {
-        let rt = Arc::new(tokio::runtime::Runtime::new()?);
         let batch = {
-            rt.block_on(async { self.inner.next().await })
+            self.rt
+                .block_on(async { self.inner.next().await })
                 .map_err(|err| PyIOError::new_err(err.to_string()))?
         };
         if let Some(batch) = batch {
@@ -48,7 +54,13 @@ impl Updater {
     }
 
     /// Update one batch
-    fn update(&self, batch: &PyAny) -> PyResult<()> {
-        Ok(())
+    fn update(&mut self, batch: &PyAny) -> PyResult<()> {
+        let batch = RecordBatch::from_pyarrow(batch)?;
+        self.rt.block_on(async {
+            self.inner
+                .update(&batch)
+                .await
+                .map_err(|e| PyIOError::new_err(e.to_string()))
+        })
     }
 }
