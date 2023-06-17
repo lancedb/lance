231fe339224bbcd0063dfd84a771730d5b7e13b8
Lei Xu <lei@lancedb.com>
2023-06-17 08:58:14 -0700

    Use log and allow users to customize log via LANCE_LOG env (#987)

--- rust/src/index/vector/ivf.rs
+++ rust/src/index/vector/ivf.rs
@@ -32,6 +32,7 @@ use futures::{
     stream::{self, StreamExt},
     TryStreamExt,
 };
+use log::info;
 use rand::{rngs::SmallRng, SeedableRng};
 
 use super::{
@@ -540,7 +541,7 @@ pub async fn build_ivf_pq_index(
     ivf_params: &IvfBuildParams,
     pq_params: &PQBuildParams,
 ) -> Result<()> {
-    println!(
+    info!(
         "Building vector index: IVF{},{}PQ{}, metric={}",
         ivf_params.num_partitions,
         if pq_params.use_opq { "O" } else { "" },
