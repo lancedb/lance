e6064068bc86c2c555bc1b98b8573b3bb73a8db0
Lei Xu <lei@lancedb.com>
2023-10-01 18:11:10 -0700

    datagen for dbpedia

--- benchmarks/dbpedia-openai/datagen.py
+++ benchmarks/dbpedia-openai/datagen.py
@@ -3,12 +3,13 @@
 #
 # Data generation for KShivendu/dbpedia-entities-openai-1M dataset on huggingface.
 
+import shutil
 from argparse import ArgumentParser
+from pathlib import Path
 
-from datasets import load_dataset, DownloadConfig
-import pyarrow as pa
 import lance
-
+import pyarrow as pa
+from datasets import DownloadConfig, load_dataset
 
 schema = pa.schema(
     [
@@ -20,30 +21,58 @@ schema = pa.schema(
 )
 
 
+def to_fixed_size_array(array, dim):
+    return pa.FixedSizeListArray.from_arrays(array.values, dim)
+
+
 def convert_dataset():
     for batch in load_dataset(
         "KShivendu/dbpedia-entities-openai-1M",
         download_config=DownloadConfig(num_proc=8, resume_download=True),
+        split="train",
     ).data.to_batches():
-        print(batch)
-        break
-
-
-def datagen(args):
-    lance.write_dataset(
-        convert_dataset(),
-        args.output,
-        max_rows_per_group=10240,
-        max_rows_per_file=204800,
-    )
+        yield pa.RecordBatch.from_arrays(
+            [
+                batch["_id"],
+                batch["title"],
+                batch["text"],
+                to_fixed_size_array(batch["openai"], 1536),
+            ],
+            schema=schema,
+        )
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("-o", "--output", type=str, default="dbpedia.lance")
+    parser.add_argument(
+        "-g",
+        "--max_rows_per_group",
+        type=int,
+        default=10240,
+        metavar="ROWS",
+        help="set max rows per group",
+    )
+    parser.add_argument(
+        "-f",
+        "--max_rows_per_file",
+        type=int,
+        default=2048 * 100,
+        metavar="ROWS",
+        help="set max rows per file",
+    )
     args = parser.parse_args()
 
-    datagen(args)
+    if Path(args.output).exists():
+        shutil.rmtree(args.output)
+
+    lance.write_dataset(
+        convert_dataset(),
+        args.output,
+        schema=schema,
+        max_rows_per_group=args.max_rows_per_group,
+        max_rows_per_file=args.max_rows_per_file,
+    )
 
 
 if __name__ == "__main__":
