1ef478fbe9893e519c4b4ab66a1adb478fa09145
Lei Xu <lei@lancedb.com>
2023-11-05 05:52:50 +0000

    dot

--- rust/lance-linalg/src/distance/dot.rs
+++ rust/lance-linalg/src/distance/dot.rs
@@ -107,10 +107,10 @@ impl Dot for Float32Type {
                 let y7 = f32x16::load_unaligned(y.as_ptr().add(96));
                 let y8 = f32x16::load_unaligned(y.as_ptr().add(112));
 
-                sum16 += (x1 * y1 + x2 * y2)
-                    + (x3 * y3 + x4 * y4)
-                    + (x5 * y5 + x6 * y6)
-                    + (x7 * y7 + x8 * y8);
+                sum16 += ((x1 * y1 + x2 * y2)
+                    + (x3 * y3 + x4 * y4))
+                    + ((x5 * y5 + x6 * y6)
+                    + (x7 * y7 + x8 * y8));
             });
         sum16.reduce_sum()
 
