f339b7c61af1888b4224919f82e579cf057fb8e6
Lei Xu <lei@lancedb.com>
2023-11-05 20:53:30 -0800

    measure perf to run ivf partition search

---
+++ rust/lance-index/benches/find_partitions.rs
@@ -0,0 +1,72 @@
+// Copyright 2023 Lance Developers.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::sync::Arc;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use lance_index::vector::ivf::Ivf;
+use lance_linalg::distance::MetricType;
+use lance_linalg::MatrixView;
+use lance_testing::datagen::generate_random_array_with_seed;
+
+fn bench_partitions(c: &mut Criterion) {
+    const DIMENSION: usize = 1536;
+    const SEED: [u8; 32] = [42; 32];
+
+    let query = generate_random_array_with_seed(DIMENSION, SEED);
+
+    for num_centroids in &[10240, 65536] {
+        let centroids = Arc::new(generate_random_array_with_seed(
+            num_centroids * DIMENSION,
+            SEED,
+        ));
+        let matrix = MatrixView::new(centroids.clone(), DIMENSION);
+
+        for k in &[1, 10, 50] {
+            let ivf = Ivf::new(matrix.clone(), MetricType::L2, vec![]);
+
+            c.bench_function(format!("IVF{},k={},L2", num_centroids, k).as_str(), |b| {
+                b.iter(|| {
+                    let _ = ivf.find_partitions(&query, *k);
+                })
+            });
+
+            let ivf = Ivf::new(matrix.clone(), MetricType::Cosine, vec![]);
+            c.bench_function(
+                format!("IVF{},k={},Cosine", num_centroids, k).as_str(),
+                |b| {
+                    b.iter(|| {
+                        let _ = ivf.find_partitions(&query, *k);
+                    })
+                },
+            );
+        }
+    }
+}
+
+#[cfg(target_os = "linux")]
+criterion_group!(
+    name=benches;
+    config = Criterion::default().significance_level(0.1).sample_size(10)
+        .with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    targets =bench_partitions);
+
+#[cfg(not(target_os = "linux"))]
+criterion_group!(
+    name=benches;
+    config = Criterion::default().significance_level(0.1).sample_size(10);
+    targets = bench_partitions);
+
+criterion_main!(benches);
