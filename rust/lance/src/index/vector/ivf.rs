843f799dad0e810aa3ed1aeeffb1e5a3a2b93f9e
Rok Mihevc <rok@mihevc.org>
2023-11-08 15:45:46 +0100

    Another one

--- rust/lance/src/index/vector/ivf.rs
+++ rust/lance/src/index/vector/ivf.rs
@@ -1567,7 +1567,7 @@ mod tests {
                     .as_primitive::<Float32Type>()
                     .values()
                     .iter()
-                    .all(|v| (-100.0 * DIM as f32..100.0).contains(v)));
+                    .all(|v| (-100.0 * DIM as f32..0.0).contains(v)));
             }
         }
     }
