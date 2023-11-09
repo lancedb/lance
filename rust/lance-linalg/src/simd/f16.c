3d418961512e19faa96edc1e1b3d20b4bafa1fa0
Lei Xu <lei@lancedb.com>
2023-11-09 18:27:30 +0000

    faster faster

--- rust/lance-linalg/src/simd/f16.c
+++ rust/lance-linalg/src/simd/f16.c
@@ -15,65 +15,16 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#ifdef __ARM_NEON
-#define LANES 4
-#define UNROLL_COUNT 2
-#endif  // __ARM_NEON
-
-#ifdef __x86_64__
-#define LANES 64
-#define UNROLL_COUNT 4
-#endif  // X86_64
-
 /// Works on NEON + FP16 or AVX512FP16
-//
-// Please make sure run "cargo bench --bench norm_l2" on both Apple Silicon and
-// X86_64, before you change this function.
-
-#ifdef __ARM_NEON
-
-// The NEON version is 40% faster than the x86_64 version on Apple M2 Max.
-_Float16 norm_l2_f16(const _Float16* data, uint32_t dimension) {
-  _Float16 vsum[LANES] = {0};
-
-#pragma clang loop unroll_count(UNROLL_COUNT) interleave(enable)
-  for (uint32_t i = 0; i < dimension / LANES * LANES; i += LANES) {
-#pragma clang loop vectorize(enable) interleave(enable)
-    for (uint32_t j = 0; j < LANES; j++) {
-      _Float16 v = data[i + j];
-      vsum[j] += v * v;
-    }
-  }
-
-  _Float16 sum = 0;
-#pragma clang loop vectorize(enable) interleave(enable)
-  for (size_t i = 0; i < LANES; i++) sum += vsum[i];
-
-// Remaining
-#pragma clang loop unroll(enable) interleave(enable)
-  for (uint32_t i = dimension / LANES * LANES; i < dimension; i++) {
-    _Float16 v = data[i];
-    sum += v * v;
-  }
-  return sum;
-}
-
-#endif
-
-#ifdef __x86_64__
-
 _Float16 norm_l2_f16(const _Float16* data, uint32_t dimension) {
   _Float16 sum = 0;
 
-#pragma clang loop unroll_count(UNROLL_COUNT) vectorize_width(LANES) interleave(enable)
+#pragma clang loop unroll(enable) vectorize(enable) interleave(enable)
   for (uint32_t i = 0; i < dimension; i++) {
-    _Float16 v = data[i];
-    sum += v * v;
+    sum += data[i] * data[i];
   }
   return sum;
 }
-
-#endif  // X86_64
 
 /// @brief Dot product of two f16 vectors.
 /// @param x A f16 vector
@@ -83,7 +34,7 @@ _Float16 norm_l2_f16(const _Float16* data, uint32_t dimension) {
 _Float16 dot_f16(const _Float16* x, const _Float16* y, uint32_t dimension) {
   _Float16 sum = 0;
 
-#pragma clang loop unroll_count(UNROLL_COUNT) interleave(enable) vectorize_width(LANES)
+#pragma clang loop unroll(enable) interleave(enable) vectorize(enable)
   for (uint32_t i = 0; i < dimension; i++) {
     sum += x[i] * y[i];
   }
