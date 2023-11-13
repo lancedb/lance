36efee6864364e1fe423788fc57326a6e35178e0
Lei Xu <lei@eto.ai>
2023-11-12 19:50:20 -0800

    fsl to tensor

--- python/python/lance/torch/data.py
+++ python/python/lance/torch/data.py
@@ -18,17 +18,46 @@
 # PEP-585. Can be removed after deprecating python 3.8 support.
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
+import numpy as np
+import torch
 from torchdata.datapipes.iter import IterDataPipe
+import pyarrow as pa
+
+from ..sampler import maybe_sample
 
 if TYPE_CHECKING:
     from .. import LanceDataset
 
+__all__ = ["LanceDataLoader"]
+
+
+def _to_tensor(batch: pa.RecordBatch) -> dict[str, torch.Tensor]:
+    ret = {}
+    for col in batch.column_names:
+        arr: pa.Array = batch[col]
+        if pa.types.is_fixed_size_list(arr.type):
+            tensor = torch.tensor(np.stack(arr.to_numpy(zero_copy_only=False)))
+        else:
+            tensor = torch.from_numpy(arr.to_numpy(zero_copy_only=False))
+        ret[col] = tensor
+    return ret
+
 
 class LanceDataLoader(IterDataPipe):
-    def __init__(self, dataset: LanceDataset, batch_size: int, *args, **kwargs):
+    def __init__(self, dataset: LanceDataset, batch_size: int, *args, columns: list[str] = None,
+                 samples: Optional[int] = 0, **kwargs):
         super().__init__(**kwargs)
         self.dataset = dataset
+        self.columns = columns
         self.batch_size = batch_size
+        self.samples: Optional[int] = samples
 
+    def __iter__(self):
+        if self.samples:
+            for batch in maybe_sample(self.dataset, n=self.samples, columns=self.columns):
+                yield _to_tensor(batch)
+        else:
+            for batch in self.dataset.to_batches(columns=self.columns, batch_size=self.batch_size):
+                yield _to_tensor(batch)
---
+++ python/python/tests/torch_tests/test_data.py
@@ -0,0 +1,37 @@
+#  Copyright (c) 2023. Lance Developers
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+import torch
+import torchdata.datapipes.iter.util
+import numpy as np
+import pyarrow as pa
+import lance
+from lance.torch.data import LanceDataLoader
+
+
+def test_iter_over_dataset(tmp_path):
+    # 10240 of 32-d vectors.
+    data = np.random.random(10240 * 32).astype("f")
+
+    fsl = pa.FixedSizeListArray.from_arrays(data, 32)
+    ids = pa.array(range(0, 10240))
+    tbl = pa.Table.from_arrays([ids, fsl], ["ids", "vec"])
+
+    ds = lance.write_dataset(tbl, tmp_path / "data.lance")
+
+    loader = LanceDataLoader(ds, batch_size=256, samples=2048, columns=["ids", "vec"])
+    print(list(loader.__iter__()))
+
+
