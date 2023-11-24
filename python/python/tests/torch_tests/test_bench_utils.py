9e1b686c3530a174b0ba21e31ed966a98a98ff09
Lei Xu <lei@lancedb.com>
2023-11-24 09:31:31 +0900

    add test for find ground truth

--- python/python/tests/torch_tests/test_bench_utils.py
+++ python/python/tests/torch_tests/test_bench_utils.py
@@ -12,15 +12,19 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from pathlib import Path
+
+import numpy as np
+import pyarrow as pa
 import torch
-from lance.torch.bench_utils import sort_tensors
+from lance.torch.bench_utils import sort_multiple_tensors
 
 
 def test_sort_tensor():
     ids = torch.tensor([[5, 7, 3, 9, 1], [10, 2, 4, 6, 8]], dtype=torch.float32)
     values = ids.clone()
 
-    sorted_vals, sorted_ids = sort_tensors(values, ids, 3)
+    sorted_vals, sorted_ids = sort_multiple_tensors(values, ids, 3)
 
     assert torch.allclose(
         sorted_vals, torch.tensor([[1, 3, 5], [2, 4, 6]], dtype=torch.float32)
@@ -28,3 +32,18 @@ def test_sort_tensor():
     assert torch.allclose(
         sorted_ids, torch.tensor([[1, 3, 5], [2, 4, 6]], dtype=torch.float32)
     )
+
+
+def test_ground_truth(tmp_path: Path):
+    N = 1000
+    K = 50
+    DIM = 128
+
+    data = np.random.rand(N, DIM).astype("float")
+    print(data)
+    fsl = pa.FixedSizeListArray()
+
+    schema = pa.schema([pa.field("vec", pa.list_(pa.float32(), DIM))])
+    tbl = pa.Table.from_arrays([data])
+
+    pass
