172f207b2b35ef03caa195eb0f60eb87fe789cb9
Rob Meng <rob.xu.meng@gmail.com>
2023-12-05 16:31:50 -0500

    feat: add batch buffering and async loading to torch.LanceDataset (#1687)
    closes #1686 
    
    buffer batches together so GPU can train more efficiently

--- python/python/tests/torch_tests/test_data.py
+++ python/python/tests/torch_tests/test_data.py
@@ -29,21 +29,29 @@ def test_iter_over_dataset(tmp_path):
     ids = pa.array(range(0, 10240), type=pa.int32())
     tbl = pa.Table.from_arrays([ids, fsl], ["ids", "vec"])
 
-    ds = lance.write_dataset(tbl, tmp_path / "data.lance")
+    ds = lance.write_dataset(tbl, tmp_path / "data.lance", max_rows_per_group=32)
 
     torch_ds = LanceDataset(
         ds, batch_size=256, samples=2048, columns=["ids", "vec"], cache=True
     )
 
+    total_rows = 0
     for batch in torch_ds:
         assert set(batch.keys()) == {"ids", "vec"}
+        # row groups of 32 can be batched into 256 exactly.
+        assert batch["vec"].shape[0] == 256
+        total_rows += batch["vec"].shape[0]
         assert batch["ids"].dtype == torch.int32
         assert batch["vec"].shape[1] == 32
+    assert total_rows == 2048
 
     shutil.rmtree(tmp_path / "data.lance")
 
+    total_rows = 0
     # it should read from cache this time.
     for batch in torch_ds:
         assert set(batch.keys()) == {"ids", "vec"}
         assert batch["ids"].dtype == torch.int32
+        total_rows += batch["vec"].shape[0]
         assert batch["vec"].shape[1] == 32
+    assert total_rows == 2048
