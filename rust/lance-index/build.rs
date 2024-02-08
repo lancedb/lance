b5ec6a0b5190dbfc4bc4a621f482966ba10ebb1a
Will Jones <willjones127@gmail.com>
2024-02-07 16:00:16 -0800

    ci: remove stdsimd reference (#1925)
    Our build was broken because the `stdsimd` feature went away:
    
    https://docs.rs/crate/lance/0.9.14/builds/1117911
    
    But it doesn't seem like we actually use any SIMD in the index crate
    anyways, so I'm just removing that line. (It seems all the SIMD was
    moved to the `linalg` crate.

--- rust/lance-index/build.rs
+++ rust/lance-index/build.rs
@@ -1,12 +1,6 @@
 use std::io::Result;
 
-use rustc_version::{version_meta, Channel};
-
 fn main() -> Result<()> {
-    if version_meta().unwrap().channel == Channel::Nightly {
-        println!("cargo:rustc-cfg=nightly");
-    }
-
     println!("cargo:rerun-if-changed=protos");
 
     let mut prost_build = prost_build::Config::new();
