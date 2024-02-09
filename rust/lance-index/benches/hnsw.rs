fb8332851c5b08e714b7c55293435e1df2ddb44d
Lei Xu <lei@lancedb.com>
2024-02-08 20:43:43 -0800

    add benchmark

--- rust/lance-index/benches/hnsw.rs
+++ rust/lance-index/benches/hnsw.rs
@@ -12,7 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 //! Benchmark of HNSW graph.
 //!
-//! 
+//!
+
+use std::time::Duration;
+
+use arrow_array::types::Float32Type;
+use criterion::{criterion_group, criterion_main, Criterion};
+use lance_linalg::MatrixView;
+use lance_testing::datagen::generate_random_array_with_seed;
+
+use lance_index::vector::hnsw::builder::HNSWBuilder;
+
+fn bench_hnsw(c: &mut Criterion) {
+    const DIMENSION: usize = 1024;
+    const TOTAL: usize = 65536;
+    const SEED: [u8; 32] = [42; 32];
+
+    let data = generate_random_array_with_seed::<Float32Type>(TOTAL * DIMENSION, SEED);
+    let mat = MatrixView::<Float32Type>::new(data.into(), DIMENSION);
+
+    c.bench_function("create_hnsw(65535x1024)", |b| {
+        b.iter(|| {
+            let hnsw = HNSWBuilder::new(mat.clone()).build();
+        })
+    });
+}
+
+#[cfg(target_os = "linux")]
+criterion_group!(
+    name=benches;
+    config = Criterion::default()
+        .measurement_time(Duration::from_secs(10))
+        .sample_size(32)
+        .with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    targets = bench_argmin);
+
+// Non-linux version does not support pprof.
+#[cfg(not(target_os = "linux"))]
+criterion_group!(
+    name=benches;
+    config = Criterion::default()
+        .measurement_time(Duration::from_secs(10))
+        .sample_size(32);
+    targets = bench_hnsw);
+
+criterion_main!(benches);
