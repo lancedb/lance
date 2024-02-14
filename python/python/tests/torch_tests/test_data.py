ca53b7c41f4668b80318879cf5eb77d64223f5a1
Weston Pace <weston.pace@gmail.com>
2024-02-14 13:58:43 -0800

    feat: add support for filtering to the ShardedBatchSampler (#1950)
    The algorithm is described in more detail in the comments. When there is
    a filter and randomness is requested then we will do our best to attempt
    randomness but the resulting sequence will not be perfectly randomized.

--- python/python/tests/torch_tests/test_data.py
+++ python/python/tests/torch_tests/test_data.py
@@ -196,3 +196,48 @@ def test_sample_batches(tmp_path: Path):
 
     all_ids = list(chain.from_iterable([batch.cpu().numpy() for batch in ds]))
     assert all_ids == [i for i in range(2000) if i // 25 % 2 == 1]
+
+
+def test_sample_batches_with_filter(tmp_path: Path):
+    NUM_ROWS = 10000
+    tbl = pa.Table.from_pydict({
+        "id": range(NUM_ROWS),
+        "filterme": [i % 2 for i in range(NUM_ROWS)],
+    })
+
+    lance.write_dataset(tbl, tmp_path, max_rows_per_file=2000)
+
+    ds = LanceDataset(
+        tmp_path,
+        batch_size=25,
+        columns=["id"],
+        with_row_id=True,
+        filter="filterme == 0",
+        sampler=ShardedBatchSampler(rank=3, world_size=5),
+    )
+
+    # The filtered sequence is 0, 2, 4, ...
+    #
+    # With rank 3 and world size 5 we should get
+    #
+    # - - - 6  -
+    # - - - 16 -
+    # - - - 26 -
+    # ...
+    all_ids = list(chain.from_iterable([batch.cpu().numpy() for batch in ds]))
+    assert all_ids == [6 + (10 * i) for i in range(len(all_ids))]
+
+    # Now test with random order
+    ds = LanceDataset(
+        tmp_path,
+        batch_size=25,
+        columns=["id"],
+        with_row_id=True,
+        filter="filterme == 0",
+        sampler=ShardedBatchSampler(rank=3, world_size=5, randomize=True),
+    )
+
+    randomized_ids = list(chain.from_iterable([batch.cpu().numpy() for batch in ds]))
+    assert randomized_ids != all_ids
+    randomized_ids.sort()
+    assert randomized_ids == all_ids
