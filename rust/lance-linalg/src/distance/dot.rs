20f8df7d2d8f3f8a2a66f57595419840e68a61d2
Will Jones <willjones127@gmail.com>
2024-03-05 07:57:15 -0800

    fix: stronger numeric guarantees for distance kernels (#2013)
    This adds tests to each of the distance kernels making sure they produce
    accurate output over a wide range of inputs.

--- rust/lance-linalg/src/distance/dot.rs
+++ rust/lance-linalg/src/distance/dot.rs
@@ -43,30 +43,34 @@ use crate::Result;
 // Please make sure run `cargo bench --bench dot` with and without AVX-512 before any change.
 // Tested `target-features`: avx512f,avx512vl,f16c
 #[inline]
-fn dot_scalar<T: Real + Sum + AddAssign + AsPrimitive<f32>, const LANES: usize>(
+fn dot_scalar<
+    T: AsPrimitive<Output>,
+    Output: Real + Sum + AddAssign + 'static,
+    const LANES: usize,
+>(
     from: &[T],
     to: &[T],
-) -> f32 {
+) -> Output {
     let x_chunks = to.chunks_exact(LANES);
     let y_chunks = from.chunks_exact(LANES);
     let sum = if x_chunks.remainder().is_empty() {
-        T::zero()
+        Output::zero()
     } else {
         x_chunks
             .remainder()
             .iter()
             .zip(y_chunks.remainder().iter())
-            .map(|(&x, &y)| x * y)
-            .sum::<T>()
+            .map(|(&x, &y)| x.as_() * y.as_())
+            .sum::<Output>()
     };
     // Use known size to allow LLVM to kick in auto-vectorization.
-    let mut sums = [T::zero(); LANES];
+    let mut sums = [Output::zero(); LANES];
     for (x, y) in x_chunks.zip(y_chunks) {
         for i in 0..LANES {
-            sums[i] += x[i] * y[i];
+            sums[i] += x[i].as_() * y[i].as_();
         }
     }
-    (sum + sums.iter().copied().sum::<T>()).as_()
+    sum + sums.iter().copied().sum::<Output>()
 }
 
 /// Dot product.
@@ -96,7 +100,7 @@ pub trait Dot: ArrowFloatType {
 impl Dot for BFloat16Type {
     #[inline]
     fn dot(x: &[bf16], y: &[bf16]) -> f32 {
-        dot_scalar::<bf16, 32>(x, y)
+        dot_scalar::<bf16, f32, 32>(x, y)
     }
 }
 
@@ -136,7 +140,7 @@ impl Dot for Float16Type {
             SimdSupport::Avx2 => unsafe {
                 kernel::dot_f16_avx2(x.as_ptr(), y.as_ptr(), x.len() as u32)
             },
-            _ => dot_scalar::<f16, 16>(x, y),
+            _ => dot_scalar::<f16, f32, 16>(x, y),
         }
     }
 }
@@ -200,7 +204,7 @@ impl Dot for Float32Type {
 impl Dot for Float64Type {
     #[inline]
     fn dot(x: &[f64], y: &[f64]) -> f32 {
-        dot_scalar::<f64, 8>(x, y)
+        dot_scalar::<f64, f64, 8>(x, y) as f32
     }
 }
 
@@ -281,7 +285,11 @@ pub fn dot_distance_arrow_batch(
 mod tests {
 
     use super::*;
-    use num_traits::FromPrimitive;
+    use crate::test_utils::{
+        arbitrary_bf16, arbitrary_f16, arbitrary_f32, arbitrary_f64, arbitrary_vector_pair,
+    };
+    use num_traits::{Float, FromPrimitive};
+    use proptest::prelude::*;
 
     #[test]
     fn test_dot() {
@@ -302,5 +310,67 @@ mod tests {
         let x: Vec<f64> = (20..40).map(|v| f64::from_i32(v).unwrap()).collect();
         let y: Vec<f64> = (120..140).map(|v| f64::from_i32(v).unwrap()).collect();
         assert_eq!(Float64Type::dot(&x, &y), dot(&x, &y));
+    }
+
+    /// Reference implementation of dot product.
+    fn dot_scalar_ref(x: &[f64], y: &[f64]) -> f32 {
+        x.iter().zip(y.iter()).map(|(&x, &y)| x * y).sum::<f64>() as f32
+    }
+
+    // Accuracy of dot product depends on the size of the components
+    // of the vector.
+    // Imagine that each `x_i` can vary by `є * |x_i|`. Similarly for `y_i`.
+    // (Basically, it's accurate to ±(1 + є) * |x_i|).
+    // Error for `sum(x, y)` is `є_x + є_y`. Error for multiple is `є_x * x + є_y * y`.
+    // See: https://www.geol.lsu.edu/jlorenzo/geophysics/uncertainties/Uncertaintiespart2.html
+    // The multiplication of `x_i` and `y_i` can vary by `(є * |x_i|) * |y_i| + (є * |y_i|) * |x_i|`.
+    // This simplifies to `2 * є * (|x_i| + |y_i|)`.
+    // So the error for the sum of all the multiplications is `є * sum(|x_i| + |y_i|)`.
+    fn max_error<T: Float + AsPrimitive<f64>>(x: &[f64], y: &[f64]) -> f32 {
+        let dot = x
+            .iter()
+            .cloned()
+            .zip(y.iter().cloned())
+            .map(|(x, y)| x.abs() * y.abs())
+            .sum::<f64>();
+        (2.0 * T::epsilon().as_() * dot) as f32
+    }
+
+    fn do_dot_test<T: FloatToArrayType>(x: &[T], y: &[T]) -> std::result::Result<(), TestCaseError>
+    where
+        T::ArrowType: Dot,
+    {
+        let f64_x = x.iter().map(|&v| v.as_()).collect::<Vec<f64>>();
+        let f64_y = y.iter().map(|&v| v.as_()).collect::<Vec<f64>>();
+
+        let expected = dot_scalar_ref(&f64_x, &f64_y);
+        let result = dot(x, y);
+
+        let max_error = max_error::<T>(&f64_x, &f64_y);
+
+        prop_assert!(approx::relative_eq!(expected, result, epsilon = max_error));
+        Ok(())
+    }
+
+    proptest::proptest! {
+        #[test]
+        fn test_dot_f16((x, y) in arbitrary_vector_pair(arbitrary_f16, 4..4048)) {
+            do_dot_test(&x, &y)?;
+        }
+
+        #[test]
+        fn test_dot_bf16((x, y) in arbitrary_vector_pair(arbitrary_bf16, 4..4048)){
+            do_dot_test(&x, &y)?;
+        }
+
+        #[test]
+        fn test_dot_f32((x, y) in arbitrary_vector_pair(arbitrary_f32, 4..4048)){
+            do_dot_test(&x, &y)?;
+        }
+
+        #[test]
+        fn test_dot_f64((x, y) in arbitrary_vector_pair(arbitrary_f64, 4..4048)){
+            do_dot_test(&x, &y)?;
+        }
     }
 }
--- rust/lance-linalg/src/distance/l2.rs
+++ rust/lance-linalg/src/distance/l2.rs
@@ -38,9 +38,11 @@ use crate::simd::{
 };
 use crate::{Error, Result};
 
+use super::Normalize;
+
 /// Calculate the L2 distance between two vectors.
 ///
-pub trait L2: ArrowFloatType {
+pub trait L2: ArrowFloatType + Normalize {
     /// Calculate the L2 distance between two vectors.
     fn l2(x: &[Self::Native], y: &[Self::Native]) -> f32;
 
@@ -67,10 +69,14 @@ where
 ///
 /// This is pub for test/benchmark only. use [l2] instead.
 #[inline]
-pub fn l2_scalar<T: Float + Sum + AddAssign + AsPrimitive<f32>, const LANES: usize>(
+pub fn l2_scalar<
+    T: AsPrimitive<Output>,
+    Output: Float + Sum + AddAssign + 'static,
+    const LANES: usize,
+>(
     from: &[T],
     to: &[T],
-) -> f32 {
+) -> Output {
     let x_chunks = from.chunks_exact(LANES);
     let y_chunks = to.chunks_exact(LANES);
 
@@ -80,30 +86,30 @@ pub fn l2_scalar<T: Float + Sum + AddAssign + AsPrimitive<f32>, const LANES: usi
             .iter()
             .zip(y_chunks.remainder())
             .map(|(&x, &y)| {
-                let diff = x - y;
+                let diff = x.as_() - y.as_();
                 diff * diff
             })
-            .sum()
+            .sum::<Output>()
     } else {
-        T::zero()
+        Output::zero()
     };
 
-    let mut sums = [T::zero(); LANES];
+    let mut sums = [Output::zero(); LANES];
     for (x, y) in x_chunks.zip(y_chunks) {
         for i in 0..LANES {
-            let diff = x[i] - y[i];
+            let diff = x[i].as_() - y[i].as_();
             sums[i] += diff * diff;
         }
     }
 
-    (s + sums.iter().copied().sum()).as_()
+    s + sums.iter().copied().sum()
 }
 
 impl L2 for BFloat16Type {
     #[inline]
     fn l2(x: &[bf16], y: &[bf16]) -> f32 {
         // TODO: add SIMD support
-        l2_scalar::<bf16, 16>(x, y)
+        l2_scalar::<bf16, f32, 16>(x, y)
     }
 }
 
@@ -143,7 +149,7 @@ impl L2 for Float16Type {
             SimdSupport::Avx2 => unsafe {
                 kernel::l2_f16_avx2(x.as_ptr(), y.as_ptr(), x.len() as u32)
             },
-            _ => l2_scalar::<f16, 16>(x, y),
+            _ => l2_scalar::<f16, f32, 16>(x, y),
         }
     }
 }
@@ -151,7 +157,7 @@ impl L2 for Float16Type {
 impl L2 for Float32Type {
     #[inline]
     fn l2(x: &[f32], y: &[f32]) -> f32 {
-        l2_scalar::<f32, 32>(x, y)
+        l2_scalar::<f32, f32, 32>(x, y)
     }
 
     fn l2_batch<'a>(
@@ -179,7 +185,8 @@ impl L2 for Float64Type {
     #[inline]
     fn l2(x: &[f64], y: &[f64]) -> f32 {
         // TODO: add SIMD support
-        l2_scalar::<f64, 8>(x, y)
+        // TODO: should we let this return f64 to avoid overflow?
+        l2_scalar::<f64, f64, 8>(x, y) as f32
     }
 }
 
@@ -287,6 +294,11 @@ mod tests {
 
     use approx::assert_relative_eq;
     use arrow_array::Float32Array;
+    use proptest::prelude::*;
+
+    use crate::test_utils::{
+        arbitrary_bf16, arbitrary_f16, arbitrary_f32, arbitrary_f64, arbitrary_vector_pair,
+    };
 
     #[test]
     fn test_euclidean_distance() {
@@ -383,5 +395,62 @@ mod tests {
 
         let d = l2_distance_batch(q.values(), values.values(), 32).collect::<Vec<_>>();
         assert_relative_eq!(0.319_357_84, d[0]);
+    }
+
+    /// Reference implementation of L2 distance.
+    ///
+    /// Note that we skip the final square root step for performance reasons.
+    fn l2_distance_reference(x: &[f64], y: &[f64]) -> f64 {
+        x.iter()
+            .zip(y.iter())
+            .map(|(x, y)| (*x - *y).powi(2))
+            .sum::<f64>()
+    }
+
+    fn do_l2_test<T: FloatToArrayType>(x: &[T], y: &[T]) -> std::result::Result<(), TestCaseError>
+    where
+        T::ArrowType: L2,
+    {
+        let x_f64 = x.iter().map(|v| v.to_f64().unwrap()).collect::<Vec<f64>>();
+        let y_f64 = y.iter().map(|v| v.to_f64().unwrap()).collect::<Vec<f64>>();
+
+        let result = l2(x, y);
+        let reference = l2_distance_reference(&x_f64, &y_f64) as f32;
+
+        prop_assert!(approx::relative_eq!(result, reference, max_relative = 1e-6));
+        Ok(())
+    }
+
+    #[test]
+    fn test_l2_distance_f16_max() {
+        let x = vec![f16::MAX; 4048];
+        let y = vec![-f16::MAX; 4048];
+        do_l2_test(&x, &y).unwrap();
+    }
+
+    // Test L2 distance over different types.
+    // * L2 is valid over the entire range of f16.
+    // * L2 is valid over f32 and bf16 in the range of +-1e12.
+    // * L2 for f64 should match the reference implementation.
+    proptest::proptest! {
+        #[test]
+        fn test_l2_distance_f16((x, y) in arbitrary_vector_pair(arbitrary_f16, 4..4048)) {
+            do_l2_test(&x, &y)?;
+        }
+
+        #[test]
+        fn test_l2_distance_bf16((x, y) in arbitrary_vector_pair(arbitrary_bf16, 4..4048)){
+            do_l2_test(&x, &y)?;
+        }
+
+        #[test]
+        fn test_l2_distance_f32((x, y) in arbitrary_vector_pair(arbitrary_f32, 4..4048)){
+            do_l2_test(&x, &y)?;
+        }
+
+        #[test]
+        fn test_l2_distance_f64((x, y) in arbitrary_vector_pair(arbitrary_f64, 4..4048)){
+            do_l2_test(&x, &y)?;
+        }
     }
 }
