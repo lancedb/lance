9e52ddad7d12cf7aeb8914f4c7056d2a1901be60
Lei Xu <lei@lancedb.com>
2024-03-04 17:41:22 -0800

    cmt

--- java/src/main/java/com/lancedb/lance/Dataset.java
+++ java/src/main/java/com/lancedb/lance/Dataset.java
@@ -18,13 +18,14 @@ import io.questdb.jar.jni.JarJniLoader;
 import java.io.Closeable;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.apache.arrow.c.ArrowArrayStream;
 
 /**
- * Class representing a Lance dataset, interfacing with the native lance library.
- * This class provides functionality to open and manage datasets with native code.
- * The native library is loaded statically and utilized through native methods.
- * It implements the {@link java.io.Closeable} interface to ensure proper resource management.
+ * Class representing a Lance dataset, interfacing with the native lance library. This class
+ * provides functionality to open and manage datasets with native code. The native library is loaded
+ * statically and utilized through native methods. It implements the {@link java.io.Closeable}
+ * interface to ensure proper resource management.
  */
 public class Dataset implements Closeable {
   static {
@@ -33,15 +34,14 @@ public class Dataset implements Closeable {
 
   private long nativeDatasetHandle;
 
-  private Dataset() {
-  }
+  private Dataset() {}
 
   public static Dataset write(ArrowArrayStream stream, String path, WriteParams params) {
     return writeWithFfiStream(stream.memoryAddress(), path, params.toMap());
   }
 
-  private static native Dataset writeWithFfiStream(long arrowStreamMemoryAddress, String path,
-      Map<String, Object> params);
+  private static native Dataset writeWithFfiStream(
+      long arrowStreamMemoryAddress, String path, Map<String, Object> params);
 
   /**
    * Opens a dataset from the specified path using the native library.
@@ -61,17 +61,24 @@ public class Dataset implements Closeable {
   /**
    * Get all fragments in this dataset.
    *
+   * We do not need to close fragments.
+   *
    * @return A list of {@link Fragment}.
    */
   public List<Fragment> getFragments() {
-    return  this.getFragmentsNative().stream().peek(f -> f.dataset = this).toList();
+    // Set a pointer in Fragment to dataset, to make it is easier to issue IOs later.
+    //
+    // We do not need to close Fragments.
+    return this.getFragmentsNative().stream()
+        .peek(f -> f.dataset = this)
+        .collect(Collectors.toList());
   }
 
   private native List<Fragment> getFragmentsNative();
 
   /**
-   * Closes this dataset and releases any system resources associated with it.
-   * If the dataset is already closed, then invoking this method has no effect.
+   * Closes this dataset and releases any system resources associated with it. If the dataset is
+   * already closed, then invoking this method has no effect.
    */
   @Override
   public void close() {
