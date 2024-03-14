lancedb/lance

commit 73b4b30233bbad573be8013bddf2955f4d11bf2a

feat: use a scalar index, if available, during a merge insert operation (#1987)
Closes #1895

rust/lance/src/dataset/write/merge_insert.rs
@@ -40,26 +40,44 @@ use datafusion::{
     execution::context::{SessionConfig, SessionContext},
     logical_expr::{Expr, JoinType},
     physical_plan::{
-        stream::RecordBatchStreamAdapter, ColumnarValue, PhysicalExpr, SendableRecordBatchStream,
+        joins::{HashJoinExec, PartitionMode},
+        repartition::RepartitionExec,
+        stream::RecordBatchStreamAdapter,
+        union::UnionExec,
+        ColumnarValue, ExecutionPlan, PhysicalExpr, SendableRecordBatchStream,
     },
     scalar::ScalarValue,
 };
 
-use futures::{stream, Stream, StreamExt, TryStreamExt};
+use datafusion_physical_expr::expressions::Column;
+use futures::{
+    stream::{self},
+    Stream, StreamExt, TryStreamExt,
+};
 use lance_core::{
     datatypes::SchemaCompareOptions,
     error::{box_error, InvalidInputSnafu},
+    utils::futures::Capacity,
     Error, Result,
 };
-use lance_datafusion::utils::reader_to_stream;
-use lance_table::format::Fragment;
+use lance_datafusion::{
+    exec::{execute_plan, LanceExecutionOptions, OneShotExec},
+    utils::reader_to_stream,
+};
+use lance_index::DatasetIndexExt;
+use lance_table::format::{Fragment, Index};
+use log::info;
 use roaring::RoaringTreemap;
 use snafu::{location, Location, ResultExt};
 
 use crate::{
     datafusion::dataframe::SessionContextExt,
     dataset::transaction::{Operation, Transaction},
-    io::{commit::commit_transaction, exec::Planner},
+    index::DatasetIndexInternalExt,
+    io::{
+        commit::commit_transaction,
+        exec::{scalar_index::MapIndexExec, utils::ReplayExec, Planner, ProjectionExec, TakeExec},
+    },
     Dataset,
 };
 
@@ -333,32 +351,157 @@ impl MergeInsertJob {
         )
     }
 
-    /// Executes the merge insert job
-    ///
-    /// This will take in the source, merge it with the existing target data, and insert new
-    /// rows, update existing rows, and delete existing rows
-    pub async fn execute(self, source: SendableRecordBatchStream) -> Result<Arc<Dataset>> {
+    async fn join_key_as_scalar_index(&self) -> Result<Option<Index>> {
+        if self.params.on.len() != 1 {
+            Ok(None)
+        } else {
+            let col = &self.params.on[0];
+            self.dataset.load_scalar_index_for_column(col).await
+        }
+    }
+
+    async fn create_indexed_scan_joined_stream(
+        &self,
+        source: SendableRecordBatchStream,
+        index: Index,
+    ) -> Result<SendableRecordBatchStream> {
+        // This relies on a few non-standard physical operators and so we cannot use the
+        // datafusion dataframe API and need to construct the plan manually :'(
+
+        // 1 - Input from user
+        let input = Arc::new(OneShotExec::new(source));
+
+        // 2 - Fork/Replay the input
+        // Regrettably, this needs to have unbounded capacity, and so we need to fully read
+        // the new data into memory.  In the future, we can do better
+        let shared_input = Arc::new(ReplayExec::new(Capacity::Unbounded, input));
+
+        // 3 - Use the index to map input to row addresses
+        let index_column = self.params.on[0].clone();
+        let index_mapper = Arc::new(MapIndexExec::new(
+            self.dataset.clone(),
+            index_column.clone(),
+            shared_input.clone(),
+        ));
+
+        // 4 - Take the mapped row addresses
+        let mut target = Arc::new(TakeExec::try_new(
+            self.dataset.clone(),
+            index_mapper,
+            Arc::new(self.dataset.schema().clone()),
+            num_cpus::get(),
+        )?) as Arc<dyn ExecutionPlan>;
+
+        // 3a - We also need to scan any new unindexed data
+        let unindexed_fragments = self.dataset.unindexed_fragments(&index.name).await?;
+        if !unindexed_fragments.is_empty() {
+            let unindexed_data = self
+                .dataset
+                .scan()
+                .with_row_id()
+                .with_fragments(unindexed_fragments)
+                .create_plan()
+                .await?;
+            let unioned = UnionExec::new(vec![target, unindexed_data]);
+            // Enforce only 1 partition.
+            target = Arc::new(RepartitionExec::try_new(
+                Arc::new(unioned),
+                datafusion::physical_plan::Partitioning::RoundRobinBatch(1),
+            )?);
+        }
+
+        // 5 - Take puts the row id at the beginning.  A full scan (used when there is no scalar
+        //     index) puts the row id at the end.  We need to match these up so we reorder the row
+        //     id to the end
+        let schema = target.schema();
+        let fields = schema.fields();
+        let mut columns = fields[1..].to_vec();
+        columns.push(fields[0].clone());
+        let projected_schema = lance_core::datatypes::Schema::try_from(&Schema::new(columns))?;
+        let target_projected =
+            Arc::new(ProjectionExec::try_new(target, Arc::new(projected_schema))?);
+
+        // 6 - Finally, join the input (source table) with the taken data (target table)
+        let source_key = Column::new_with_schema(&index_column, shared_input.schema().as_ref())?;
+        let target_key =
+            Column::new_with_schema(&index_column, target_projected.schema().as_ref())?;
+        let joined = Arc::new(
+            HashJoinExec::try_new(
+                target_projected,
+                shared_input,
+                vec![(target_key, source_key)],
+                None,
+                &JoinType::Full,
+                PartitionMode::CollectLeft,
+                true,
+            )
+            .unwrap(),
+        );
+        execute_plan(
+            joined,
+            LanceExecutionOptions {
+                use_spilling: true,
+                ..Default::default()
+            },
+        )
+    }
+
+    // If the join keys are not indexed then we need to do a full scan of the table
+    async fn create_full_table_joined_stream(
+        &self,
+        source: SendableRecordBatchStream,
+    ) -> Result<SendableRecordBatchStream> {
         let session_config = SessionConfig::default().with_target_partitions(1);
         let session_ctx = SessionContext::new_with_config(session_config);
         let schema = source.schema();
         self.check_compatible_schema(&schema)?;
         let existing = session_ctx.read_lance(self.dataset.clone(), true)?;
         let new_data = session_ctx.read_one_shot(source)?;
-        let merger = Merger::try_new(self.params, schema.clone())?;
-        let deleted_rows = merger.deleted_rows.clone();
-        let join_cols = merger
+        let join_cols = self
             .params
             .on
             .iter()
             .map(|c| c.as_str())
             .collect::<Vec<_>>();
         let joined = existing.join(new_data, JoinType::Full, &join_cols, &join_cols, None)?;
-        let joined = joined.execute_stream().await?;
+        Ok(joined.execute_stream().await?)
+    }
 
+    async fn create_joined_stream(
+        &self,
+        source: SendableRecordBatchStream,
+    ) -> Result<SendableRecordBatchStream> {
+        // We need to do a full index scan if we're deleting source data
+        let can_use_scalar_index = matches!(
+            self.params.delete_not_matched_by_source,
+            WhenNotMatchedBySource::Keep
+        );
+        if can_use_scalar_index {
+            if let Some(index) = self.join_key_as_scalar_index().await? {
+                self.create_indexed_scan_joined_stream(source, index).await
+            } else {
+                info!("There is no scalar index on the merge insert join key, falling back to a less efficient full table scan");
+                self.create_full_table_joined_stream(source).await
+            }
+        } else {
+            info!("The merge insert operation is configured to delete rows from the target table, this requires a potentially costly full table scan");
+            self.create_full_table_joined_stream(source).await
+        }
+    }
+
+    /// Executes the merge insert job
+    ///
+    /// This will take in the source, merge it with the existing target data, and insert new
+    /// rows, update existing rows, and delete existing rows
+    pub async fn execute(self, source: SendableRecordBatchStream) -> Result<Arc<Dataset>> {
+        let schema = source.schema();
+
+        let joined = self.create_joined_stream(source).await?;
+        let merger = Merger::try_new(self.params, schema.clone())?;
+        let deleted_rows = merger.deleted_rows.clone();
         let stream = joined
             .and_then(move |batch| merger.clone().execute_batch(batch))
             .try_flatten();
-
         let stream = RecordBatchStreamAdapter::new(schema, stream);
 
         let new_fragments = write_fragments_internal(
@@ -687,7 +830,14 @@ mod tests {
     use arrow_select::concat::concat_batches;
     use datafusion::common::Column;
     use lance_datafusion::utils::reader_to_stream;
+    use lance_datagen::{array, BatchCount, RowCount, Seed};
+    use lance_index::IndexType;
     use tempfile::tempdir;
+
+    use crate::{
+        dataset::{WriteMode, WriteParams},
+        index::scalar::ScalarIndexParams,
+    };
 
     use super::*;
 
@@ -927,5 +1077,130 @@ mod tests {
             .try_build()
             .unwrap();
         check(new_batch.clone(), job, &[1, 4, 5, 6], &[]).await;
+    }
+
+    #[tokio::test]
+    async fn test_indexed_merge_insert() {
+        let test_dir = tempdir().unwrap();
+        let test_uri = test_dir.path().to_str().unwrap();
+
+        let data = lance_datagen::gen()
+            .with_seed(Seed::from(1))
+            .col(Some("key".to_string()), array::rand_pseduo_uuid_hex())
+            .col(Some("value".to_string()), array::step::<UInt32Type>());
+        let data = data.into_reader_rows(RowCount::from(1024), BatchCount::from(32));
+        let schema = data.schema();
+
+        // Create an input dataset with a scalar index on key
+        let mut ds = Dataset::write(data, test_uri, None).await.unwrap();
+        let index_params = ScalarIndexParams::default();
+        ds.create_index(&["key"], IndexType::Scalar, None, &index_params, false)
+            .await
+            .unwrap();
+
+        // Create some new (unindexed) data
+        let data = lance_datagen::gen()
+            .with_seed(Seed::from(2))
+            .col(Some("key".to_string()), array::rand_pseduo_uuid_hex())
+            .col(Some("value".to_string()), array::step::<UInt32Type>());
+        let data = data.into_reader_rows(RowCount::from(1024), BatchCount::from(8));
+        let ds = Dataset::write(
+            data,
+            test_uri,
+            Some(WriteParams {
+                mode: WriteMode::Append,
+                ..Default::default()
+            }),
+        )
+        .await
+        .unwrap();
+
+        let ds = Arc::new(ds);
+
+        let just_index_col = Schema::new(vec![Field::new("key", DataType::Utf8, false)]);
+
+        // Sample 2048 random indices and then paste on a column of 9999999's
+        let some_indices = ds
+            .sample(2048, &(&just_index_col).try_into().unwrap())
+            .await
+            .unwrap();
+        let some_indices = some_indices.column(0).clone();
+        let some_vals = lance_datagen::gen()
+            .col(None, array::fill::<UInt32Type>(9999999))
+            .into_batch_rows(RowCount::from(2048))
+            .unwrap();
+        let some_vals = some_vals.column(0).clone();
+        let source_batch =
+            RecordBatch::try_new(schema.clone(), vec![some_indices, some_vals]).unwrap();
+        // To make things more interesting, lets make the input a stream of four batches
+        let source_batches = vec![
+            source_batch.slice(0, 512),
+            source_batch.slice(512, 512),
+            source_batch.slice(1024, 512),
+            source_batch.slice(1536, 512),
+        ];
+        let source = Box::new(RecordBatchIterator::new(
+            source_batches.clone().into_iter().map(Ok),
+            schema.clone(),
+        ));
+
+        // Run merge_insert
+        let ds = MergeInsertBuilder::try_new(ds.clone(), vec!["key".to_string()])
+            .unwrap()
+            .when_not_matched(WhenNotMatched::DoNothing)
+            .when_matched(WhenMatched::UpdateAll)
+            .try_build()
+            .unwrap()
+            .execute_reader(source)
+            .await
+            .unwrap();
+
+        // Check that the data is as expected
+        let updated = ds
+            .scan()
+            .filter("value = 9999999")
+            .unwrap()
+            .count_rows()
+            .await
+            .unwrap();
+        assert_eq!(updated, 2048);
+
+        // Make sure we don't use an indexed scan if there is a delete criteria
+        let source = Box::new(RecordBatchIterator::new(
+            source_batches.clone().into_iter().map(Ok),
+            schema.clone(),
+        ));
+        // Run merge_insert
+        let ds = MergeInsertBuilder::try_new(ds.clone(), vec!["key".to_string()])
+            .unwrap()
+            .when_not_matched(WhenNotMatched::DoNothing)
+            .when_matched(WhenMatched::UpdateAll)
+            .when_not_matched_by_source(WhenNotMatchedBySource::Delete)
+            .try_build()
+            .unwrap()
+            .execute_reader(source)
+            .await
+            .unwrap();
+
+        // Check that the data is as expected
+        assert_eq!(ds.count_rows().await.unwrap(), 2048);
+
+        let source = Box::new(RecordBatchIterator::new(
+            source_batches.clone().into_iter().map(Ok),
+            schema.clone(),
+        ));
+        // Run merge_insert one last time.  The index is now completely out of date.  Every
+        // row it points to is a deleted row.  Make sure that doesn't break.
+        let ds = MergeInsertBuilder::try_new(ds.clone(), vec!["key".to_string()])
+            .unwrap()
+            .when_not_matched(WhenNotMatched::DoNothing)
+            .when_matched(WhenMatched::UpdateAll)
+            .try_build()
+            .unwrap()
+            .execute_reader(source)
+            .await
+            .unwrap();
+
+        assert_eq!(ds.count_rows().await.unwrap(), 2048);
     }
 }

rust/lance/src/io/exec/utils.rs
@@ -1,0 +1,250 @@
+// Copyright 2024 Lance Developers.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use std::sync::{Arc, Mutex};
+
+use arrow_array::RecordBatch;
+use arrow_schema::SchemaRef;
+use datafusion::error::{DataFusionError, Result as DataFusionResult};
+use datafusion::physical_plan::{
+    DisplayAs, DisplayFormatType, ExecutionPlan, RecordBatchStream, SendableRecordBatchStream,
+};
+use futures::{Stream, StreamExt};
+use lance_core::error::{CloneableResult, Error};
+use lance_core::utils::futures::{Capacity, SharedStream, SharedStreamExt};
+
+struct InnerState {
+    cached: Option<SendableRecordBatchStream>,
+    taken: bool,
+}
+
+impl std::fmt::Debug for InnerState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("InnerState")
+            .field("cached", &self.cached.is_some())
+            .field("taken", &self.taken)
+            .finish()
+    }
+}
+
+/// An execution node that can be used as an input twice
+///
+/// This can be used to broadcast an input to multiple outputs.
+///
+/// Note: this is done by caching the results.  If one output is consumed
+/// more quickly than the other, this can lead to increased memory usage.
+/// The `capacity` parameter can bound this, by blocking the faster output
+/// when the cache is full.  Take care not to cause deadlock.
+///
+/// For example, if both outputs are fed to a HashJoinExec then one side
+/// of the join will be fully consumed before the other side is read.  In
+/// this case, you should probably use an unbounded capacity.
+#[derive(Debug)]
+pub struct ReplayExec {
+    capacity: Capacity,
+    input: Arc<dyn ExecutionPlan>,
+    inner_state: Arc<Mutex<InnerState>>,
+}
+
+impl ReplayExec {
+    pub fn new(capacity: Capacity, input: Arc<dyn ExecutionPlan>) -> Self {
+        Self {
+            capacity,
+            input,
+            inner_state: Arc::new(Mutex::new(InnerState {
+                cached: None,
+                taken: false,
+            })),
+        }
+    }
+}
+
+impl DisplayAs for ReplayExec {
+    fn fmt_as(&self, t: DisplayFormatType, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match t {
+            DisplayFormatType::Default | DisplayFormatType::Verbose => {
+                write!(f, "Replay: capacity={:?}", self.capacity)
+            }
+        }
+    }
+}
+
+// There's some annoying adapter-work that needs to happen here.  In order
+// to share a stream we need its items to be Clone and DataFusionError is
+// not Clone.  So we wrap the stream in a CloneableResult.  However, in order
+// for that shared stream to be a SendableRecordBatchStream, it needs to be
+// using DataFusionError.  So we need to adapt the stream back to a
+// SendableRecordBatchStream.
+struct ShareableRecordBatchStream(SendableRecordBatchStream);
+
+impl Stream for ShareableRecordBatchStream {
+    type Item = CloneableResult<RecordBatch>;
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        match self.0.poll_next_unpin(cx) {
+            std::task::Poll::Ready(None) => std::task::Poll::Ready(None),
+            std::task::Poll::Ready(Some(res)) => {
+                std::task::Poll::Ready(Some(CloneableResult::from(res.map_err(Error::from))))
+            }
+            std::task::Poll::Pending => std::task::Poll::Pending,
+        }
+    }
+}
+
+struct ShareableRecordBatchStreamAdapter {
+    schema: SchemaRef,
+    stream: SharedStream<'static, CloneableResult<RecordBatch>>,
+}
+
+impl Stream for ShareableRecordBatchStreamAdapter {
+    type Item = DataFusionResult<RecordBatch>;
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        match self.stream.poll_next_unpin(cx) {
+            std::task::Poll::Ready(None) => std::task::Poll::Ready(None),
+            std::task::Poll::Ready(Some(res)) => std::task::Poll::Ready(Some(
+                res.0
+                    .map_err(|e| DataFusionError::External(e.0.to_string().into())),
+            )),
+            std::task::Poll::Pending => std::task::Poll::Pending,
+        }
+    }
+}
+
+impl RecordBatchStream for ShareableRecordBatchStreamAdapter {
+    fn schema(&self) -> SchemaRef {
+        self.schema.clone()
+    }
+}
+
+impl ExecutionPlan for ReplayExec {
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    fn schema(&self) -> arrow_schema::SchemaRef {
+        self.input.schema()
+    }
+
+    fn output_partitioning(&self) -> datafusion_physical_expr::Partitioning {
+        self.input.output_partitioning()
+    }
+
+    fn output_ordering(&self) -> Option<&[datafusion_physical_expr::PhysicalSortExpr]> {
+        self.input.output_ordering()
+    }
+
+    fn children(&self) -> Vec<Arc<dyn ExecutionPlan>> {
+        vec![self.input.clone()]
+    }
+
+    fn with_new_children(
+        self: Arc<Self>,
+        _: Vec<Arc<dyn ExecutionPlan>>,
+    ) -> datafusion::error::Result<Arc<dyn ExecutionPlan>> {
+        unimplemented!()
+    }
+
+    fn execute(
+        &self,
+        partition: usize,
+        context: Arc<datafusion::execution::TaskContext>,
+    ) -> datafusion::error::Result<SendableRecordBatchStream> {
+        let mut inner_state = self.inner_state.lock().unwrap();
+        if let Some(cached) = inner_state.cached.take() {
+            if inner_state.taken {
+                panic!("ReplayExec can only be executed twice");
+            }
+            inner_state.taken = true;
+            Ok(cached)
+        } else {
+            let input = self.input.execute(partition, context)?;
+            let schema = input.schema().clone();
+            let input = ShareableRecordBatchStream(input);
+            let (to_return, to_cache) = input.boxed().share(self.capacity);
+            inner_state.cached = Some(Box::pin(ShareableRecordBatchStreamAdapter {
+                schema: schema.clone(),
+                stream: to_cache,
+            }));
+            Ok(Box::pin(ShareableRecordBatchStreamAdapter {
+                schema,
+                stream: to_return,
+            }))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::sync::Arc;
+
+    use arrow_array::{types::UInt32Type, RecordBatchReader};
+    use arrow_schema::SortOptions;
+    use datafusion::{
+        logical_expr::JoinType,
+        physical_expr::expressions::Column,
+        physical_plan::{
+            joins::SortMergeJoinExec, stream::RecordBatchStreamAdapter, ExecutionPlan,
+        },
+    };
+    use futures::{StreamExt, TryStreamExt};
+    use lance_core::utils::futures::Capacity;
+    use lance_datafusion::exec::OneShotExec;
+    use lance_datagen::{array, BatchCount, RowCount};
+
+    use super::ReplayExec;
+
+    #[tokio::test]
+    async fn test_replay() {
+        let data = lance_datagen::gen()
+            .col(Some("x".to_string()), array::step::<UInt32Type>())
+            .into_reader_rows(RowCount::from(1024), BatchCount::from(16));
+        let schema = data.schema().clone();
+        let data = Box::pin(RecordBatchStreamAdapter::new(
+            schema,
+            futures::stream::iter(data).map_err(datafusion::error::DataFusionError::from),
+        ));
+
+        let input = Arc::new(OneShotExec::new(data));
+        let shared = Arc::new(ReplayExec::new(Capacity::Bounded(4), input));
+
+        let joined = Arc::new(
+            SortMergeJoinExec::try_new(
+                shared.clone(),
+                shared,
+                vec![(Column::new("x", 0), Column::new("x", 0))],
+                JoinType::Inner,
+                vec![SortOptions::default()],
+                true,
+            )
+            .unwrap(),
+        );
+
+        let mut join_stream = joined
+            .execute(0, Arc::new(datafusion::execution::TaskContext::default()))
+            .unwrap();
+
+        while let Some(batch) = join_stream.next().await {
+            // We don't test much here but shouldn't really need to.  The join and stream sharing
+            // are tested on their own.  We just need to make sure they get hooked up correctly
+            assert_eq!(batch.unwrap().num_columns(), 2);
+        }
+    }
+}

rust/lance-core/src/utils.rs
@@ -15,6 +15,7 @@
 pub mod address;
 pub mod cpu;
 pub mod deletion;
+pub mod futures;
 pub mod mask;
 pub mod testing;
 pub mod tracing;

.gitignore
@@ -88,6 +88,7 @@ wheelhouse
 # Rust
 target
 Cargo.lock
+**/sccache.log
 
 # c++ lsp
 .ccls-cache/

Cargo.toml
@@ -125,6 +125,8 @@ tokio = { version = "1.23", features = [
     "fs",
     "sync",
 ] }
+tokio-stream = "0.1.14"
+tokio-util = { version = "0.7.10" }
 tracing = "0.1"
 url = "2.3"
 uuid = { version = "1.2", features = ["v4", "serde"] }

python/python/benchmarks/test_merge_insert.py
@@ -1,0 +1,114 @@
+# Copyright 2024 Lance Developers.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+import shutil
+import uuid
+from datetime import timedelta
+from pathlib import Path
+from typing import NamedTuple
+
+import lance
+import pyarrow as pa
+import pyarrow.compute as pc
+import pytest
+
+NUM_ROWS = 10_000_000
+NEW_ROWS = 10_000
+
+
+def create_table(num_rows: int) -> pa.Table:
+    keys = pa.array([uuid.uuid4().bytes.hex() for _ in range(num_rows)])
+    values = pa.array(range(num_rows))
+    return pa.table({"keys": keys, "keys_no_index": keys, "values": values})
+
+
+def create_base_dataset(data_dir: Path) -> lance.LanceDataset:
+    tmp_path = data_dir / "merge_insert_dataset"
+
+    if tmp_path.exists():
+        shutil.rmtree(tmp_path)
+
+    rows_remaining = NUM_ROWS
+    dataset = None
+    while rows_remaining > 0:
+        next_batch_length = min(rows_remaining, 1024 * 1024)
+        rows_remaining -= next_batch_length
+        table = create_table(next_batch_length)
+        dataset = lance.write_dataset(table, tmp_path, mode="append")
+
+    dataset.create_scalar_index("keys", "BTREE")
+
+    return dataset
+
+
+def create_delete_dataset(base_dataset):
+    dataset = lance.dataset(base_dataset.uri, version=base_dataset.version)
+    dataset.restore()
+    dataset.delete("values % 2 != 0")
+    return dataset
+
+
+def create_new_rows_dataset(base_dataset):
+    dataset = lance.dataset(base_dataset.uri, version=base_dataset.version)
+    dataset.restore()
+    table = create_table(NEW_ROWS)
+    return lance.write_dataset(table, base_dataset.uri, mode="append")
+
+
+class Datasets(NamedTuple):
+    # A clean dataset
+    clean: lance.LanceDataset
+    # A dataset where every fragment has a deletion vector
+    with_delete_files: lance.LanceDataset
+    # A dataset that has new, unindexed rows
+    with_new_rows: lance.LanceDataset
+
+
+@pytest.fixture(scope="module")
+def datasets(data_dir: Path) -> Datasets:
+    base_dataset = create_base_dataset(data_dir)
+    delete_dataset = create_delete_dataset(base_dataset)
+    new_rows_dataset = create_new_rows_dataset(base_dataset)
+    return Datasets(
+        clean=base_dataset,
+        with_delete_files=delete_dataset,
+        with_new_rows=new_rows_dataset,
+    )
+
+
+@pytest.fixture(scope="module", params=Datasets._fields)
+def test_dataset(datasets: Datasets, request) -> lance.LanceDataset:
+    return datasets.__getattribute__(request.param)
+
+
+@pytest.mark.benchmark(group="merge_insert")
+@pytest.mark.parametrize("use_scalar_index", (False, True))
+def test_bulk_update(test_dataset, benchmark, use_scalar_index):
+    test_dataset.cleanup_old_versions(older_than=timedelta(seconds=1))
+    NUM_NEW_ROWS = 1000
+    version = test_dataset.version
+    indices = random.sample(range(test_dataset.count_rows()), NUM_NEW_ROWS)
+    new_table = test_dataset.take(indices)
+    values_idx = new_table.schema.get_field_index("values")
+    new_col = pc.add(new_table.column(values_idx), 1)
+    new_table.set_column(values_idx, new_table.schema.field(values_idx), new_col)
+    key = "keys" if use_scalar_index else "keys_no_index"
+
+    def bench_fn():
+        bench_ds = test_dataset.checkout_version(version)
+        bench_ds.restore()
+        bench_ds.merge_insert(key).when_matched_update_all().execute(new_table)
+
+    benchmark(bench_fn)

rust/lance-core/Cargo.toml
@@ -34,6 +34,8 @@ roaring.workspace = true
 serde_json.workspace = true
 snafu.workspace = true
 tokio.workspace = true
+tokio-stream.workspace = true
+tokio-util.workspace = true
 tracing.workspace = true
 url.workspace = true
 

commit 89a4e3e3dfe75693de1895f3f5e9ad7ffdb332d6

fix: update merge_insert code to use latest df version (#2071)
https://github.com/lancedb/lance/commit/73b4b30233bbad573be8013bddf2955f4d11bf2a
broke the build because it was written against an older DF version and
we had updated between the last time CI ran on the PR and the actual
merge.

This updates the merge insert code to use the newer DF APIs.

rust/lance/src/dataset/write/merge_insert.rs
@@ -429,7 +429,7 @@ impl MergeInsertJob {
             HashJoinExec::try_new(
                 target_projected,
                 shared_input,
-                vec![(target_key, source_key)],
+                vec![(Arc::new(target_key), Arc::new(source_key))],
                 None,
                 &JoinType::Full,
                 PartitionMode::CollectLeft,

rust/lance/src/io/exec/utils.rs
@@ -229,7 +229,8 @@ mod tests {
             SortMergeJoinExec::try_new(
                 shared.clone(),
                 shared,
-                vec![(Column::new("x", 0), Column::new("x", 0))],
+                vec![(Arc::new(Column::new("x", 0)), Arc::new(Column::new("x", 0)))],
+                None,
                 JoinType::Inner,
                 vec![SortOptions::default()],
                 true,