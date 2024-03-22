767fff797f61a28e12c22cc5a3d92f00a9967d0a
BubbleCal <bubble-cal@outlook.com>
2024-03-22 17:26:18 +0800

    fix
    Signed-off-by: BubbleCal <bubble-cal@outlook.com>

--- rust/lance/src/index/vector/ivf.rs
+++ rust/lance/src/index/vector/ivf.rs
@@ -2173,7 +2173,7 @@ mod tests {
             .with_row_id()
             .nearest("vector", query, k)
             .unwrap()
-            .nprobs(2)
+            .nprobs(nlist)
             .try_into_stream()
             .await
             .unwrap()
@@ -2196,7 +2196,7 @@ mod tests {
         let gt = ground_truth(&mat, query.values(), k);
         let recall = results.intersection(&gt).count() as f32 / k as f32;
         assert!(
-            recall >= 0.7,
+            recall >= 0.8,
             "recall: {}\n results: {:?}\n\ngt: {:?}",
             recall,
             results.iter().sorted().collect_vec(),
