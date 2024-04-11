946651076b8b419976c3f965e040db51fa7121a8
BubbleCal <bubble-cal@outlook.com>
2024-04-12 00:22:08 +0800

    fix lint
    Signed-off-by: BubbleCal <bubble-cal@outlook.com>

--- python/python/lance/util.py
+++ python/python/lance/util.py
@@ -4,7 +4,7 @@
 from __future__ import annotations
 
 from datetime import datetime, timedelta
-from typing import TYPE_CHECKING, Literal, Optional, Union
+from typing import TYPE_CHECKING, Literal, Optional, Union, Iterator
 
 import pyarrow as pa
 
