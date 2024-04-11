c7e2dc2b8684af4ecb580a608ce4456bae7b665d
Lei Xu <lei@lancedb.com>
2024-04-10 22:10:56 -0700

    support schema infer

--- python/python/lance/ray/sink.py
+++ python/python/lance/ray/sink.py
@@ -2,7 +2,8 @@
 # SPDX-FileCopyrightText: Copyright The Lance Authors
 
 import logging
-from typing import TYPE_CHECKING, Iterable, List, Optional, Union
+from typing import TYPE_CHECKING, Iterable, List, Optional, Union, Literal, Tuple
+from itertools import chain
 
 import pyarrow as pa
 
@@ -29,7 +30,8 @@ class LanceDatasink(ray.data.Datasink):
     schema : pyarrow.Schema
         The schema of the dataset.
     mode : str, optional
-        The write mode. Default is 'append'. Choices are 'append' and 'overwrite'.
+        The write mode. Default is 'append'.
+        Choices are 'append', 'create', 'overwrite'.
     max_rows_per_file : int, optional
         The maximum number of rows per file. Default is 1024 * 1024.
 
@@ -62,7 +64,7 @@ class LanceDatasink(ray.data.Datasink):
         self,
         uri: str,
         schema: Optional[pa.Schema] = None,
-        mode: str = "create",
+        mode: Literal["create", "append", "overwrite"] = "create",
         max_rows_per_file: int = 1024 * 1024,
         *args,
         **kwargs,
@@ -73,10 +75,8 @@ class LanceDatasink(ray.data.Datasink):
         self.schema = schema
         self.mode = mode
 
-        if self.mode in set(["create", "overwrite"]) and self.schema is None:
-            raise ValueError("schema is required for create and overwrite modes")
-
         self.max_rows_per_file = max_rows_per_file
+        # if mode is append, read_version is read from existing dataset.
         self.read_version: int | None = None
 
     @property
@@ -93,27 +93,34 @@ class LanceDatasink(ray.data.Datasink):
     def write(
         self,
         blocks: Iterable[Union[pa.Table, "pd.DataFrame"]],
-        _ctx,
+        ctx,
     ):
         from ..dependencies import _PANDAS_AVAILABLE
         from ..dependencies import pandas as pd
+
+        if self.schema is None:
+            first = next(blocks)
+            if _PANDAS_AVAILABLE and isinstance(first, pd.DataFrame):
+                schema = pa.Schema.from_pandas(first)
+            else:
+                schema = first.schema
+            blocks = chain([first], blocks)
+        else:
+            schema = self.schema
 
         # TODO: use v2 writer later.
         def record_batch_converter():
             for block in blocks:
                 if _PANDAS_AVAILABLE and isinstance(block, pd.DataFrame):
-                    tbl = pa.Table.from_pandas(block, schema=self.schema)
+                    tbl = pa.Table.from_pandas(block, schema=schema)
                 else:
                     tbl = block
                 yield from tbl.to_batches()
 
-        reader = pa.RecordBatchReader.from_batches(
-            self.schema, record_batch_converter()
-        )
-        fragment = LanceFragment.create(
-            self.uri, reader, fragment_id=0, schema=self.schema
-        )
-        return fragment
+        reader = pa.RecordBatchReader.from_batches(schema, record_batch_converter())
+        fragment = LanceFragment.create(self.uri, reader, fragment_id=0, schema=schema)
+        ret_schema = None if ctx.task_idx != 0 else schema
+        return (fragment, ret_schema)
 
     def on_write_start(self):
         if self.mode == "append":
@@ -122,12 +129,25 @@ class LanceDatasink(ray.data.Datasink):
             if self.schema is None:
                 self.schema = ds.schema
 
-    def on_write_complete(self, fragments: List[FragmentMetadata]):
+    def on_write_complete(
+        self, write_results: List[Tuple[FragmentMetadata, Optional[pa.Schema]]]
+    ):
         logging.info(
-            "%s.on_write_complete: committing %s fragments", self, len(fragments)
+            "%s.on_write_complete: committing %s fragments", self, len(write_results)
         )
+
+        schema = self.schema
+        if schema is None:
+            for rst in write_results:
+                if rst[1] is not None:
+                    schema = rst[1]
+                    break
+        if schema is None:
+            raise ValueError("schema is not provided or inferred from data.")
+
+        fragments = [rst[0] for rst in write_results]
         if self.mode in set(["create", "overwrite"]):
-            op = lance.LanceOperation.Overwrite(self.schema, fragments)
+            op = lance.LanceOperation.Overwrite(schema, fragments)
         elif self.mode == "append":
             op = lance.LanceOperation.Append(fragments)
         lance.LanceDataset.commit(self.uri, op, read_version=self.read_version)
--- python/python/tests/test_ray.py
+++ python/python/tests/test_ray.py
@@ -17,13 +17,14 @@ def test_ray_sink(tmp_path: Path):
     ray.init()
     schema = pa.schema([pa.field("id", pa.int64()), pa.field("str", pa.string())])
 
-    sink = LanceDatasink(tmp_path, schema=schema)
+    sink = LanceDatasink(tmp_path)
     ray.data.range(10).map(
         lambda x: {"id": x["id"], "str": f"str-{x['id']}"}
     ).write_datasink(sink)
 
     ds = lance.dataset(tmp_path)
     ds.count_rows() == 10
+    assert ds.schema == schema
     tbl = ds.to_table()
     assert sorted(tbl["id"].to_pylist()) == list(range(10))
     assert set(tbl["str"].to_pylist()) == set([f"str-{i}" for i in range(10)])
