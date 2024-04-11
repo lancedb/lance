b46a9e42093ed72c860703be7f8c0b8d0b779a08
BubbleCal <bubble-cal@outlook.com>
2024-04-11 19:04:08 +0800

    fix path
    Signed-off-by: BubbleCal <bubble-cal@outlook.com>

--- python/python/tests/test_hnsw.py
+++ python/python/tests/test_hnsw.py
@@ -5,8 +5,9 @@ import lance
 import pyarrow as pa
 
 
-def test_train_hnsw():
+def test_train_hnsw(tmp_path):
+    idx_path = tmp_path / "hnsw.idx"
     arrays = pa.array([1, 2, 3, 4], type=pa.float32())
     vectors = pa.FixedSizeListArray.from_arrays(arrays, 2)
     hnsw = lance.util.HNSW.build([vectors])
-    hnsw.to_lance_file("/tmp/hnsw.idx")
+    hnsw.to_lance_file(str(idx_path))
