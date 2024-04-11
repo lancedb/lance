edc499ab03c4b20e2ab404705d3f25fcda246332
Weston Pace <weston.pace@gmail.com>
2024-04-10 19:22:54 -0700

    Hoisted by my own petard

--- rust/lance-encoding/src/encodings/physical/buffers.rs
+++ rust/lance-encoding/src/encodings/physical/buffers.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright The Lance Authors
+
 use arrow_array::{cast::AsArray, ArrayRef};
 
 use arrow_buffer::BooleanBufferBuilder;
