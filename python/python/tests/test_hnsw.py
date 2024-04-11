397c115d15657d41c2149caa6ccb9612964a2f31
BubbleCal <bubble-cal@outlook.com>
2024-04-11 18:18:07 +0800

    fix
    Signed-off-by: BubbleCal <bubble-cal@outlook.com>

--- python/python/tests/test_hnsw.py
+++ python/python/tests/test_hnsw.py
@@ -10,12 +10,3 @@ def test_train_hnsw():
     vectors = pa.FixedSizeListArray.from_arrays(arrays, 2)
     hnsw = lance.util.HNSW.build([vectors])
     hnsw.to_lance_file("/tmp/hnsw.idx")
-
-
-def test_build_sq_storage():
-    arrays = pa.array([1, 2, 3, 4], type=pa.float32())
-    vectors = pa.FixedSizeListArray.from_arrays(arrays, 2)
-    hnsw = lance.util.HNSW.build([vectors])
-    hnsw.to_lance_file("/tmp/hnsw.idx")
-
-    lance.util.build_sq_storage()
