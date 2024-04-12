a99b4bacc94269a07d2419a312939d54651c5619
Lei Xu <eddyxu@gmail.com>
2024-04-12 00:52:03 +0000

    stream ray write

--- python/python/lance/ray/sink.py
+++ python/python/lance/ray/sink.py
@@ -1,10 +1,22 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright The Lance Authors
 
-import logging
 from itertools import chain
-from typing import TYPE_CHECKING, Iterable, List, Literal, Optional, Tuple, Union
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Dict,
+    Generator,
+    Iterable,
+    List,
+    Literal,
+    Optional,
+    Tuple,
+    Union,
+)
 
+import cloudpickle
 import pyarrow as pa
 
 import lance
@@ -15,10 +27,93 @@ from ..dependencies import ray
 if TYPE_CHECKING:
     import pandas as pd
 
-__all__ = ["LanceDatasink"]
+__all__ = ["LanceDatasink", "LanceFragmentWriter", "LanceCommitter"]
 
 
-class LanceDatasink(ray.data.Datasink):
+def _pd_to_arrow(
+    df: Union[pa.Table, "pd.DataFrame", Dict], schema: Optional[pa.Schema]
+) -> pa.Table:
+    """Convert a pandas DataFrame to pyarrow Table."""
+    from ..dependencies import _PANDAS_AVAILABLE
+    from ..dependencies import pandas as pd
+
+    if isinstance(df, dict):
+        return pa.Table.from_pydict(df, schema=schema)
+    if _PANDAS_AVAILABLE and isinstance(df, pd.DataFrame):
+        return pa.Table.from_pandas(df, schema=schema)
+    return df
+
+
+def _write_fragment(
+    stream: Iterable[pa.Table], uri: str, schema: Optional[pa.Schema] = None
+) -> Tuple[FragmentMetadata, pa.Schema]:
+    from ..dependencies import _PANDAS_AVAILABLE
+    from ..dependencies import pandas as pd
+
+    if schema is None:
+        first = next(stream)
+        if _PANDAS_AVAILABLE and isinstance(first, pd.DataFrame):
+            schema = pa.Schema.from_pandas(first)
+        else:
+            schema = first.schema
+        stream = chain([first], stream)
+
+    def record_batch_converter():
+        for block in stream:
+            tbl = _pd_to_arrow(block, schema)
+            yield from tbl.to_batches()
+
+    # TODO: use format v2.
+    reader = pa.RecordBatchReader.from_batches(schema, record_batch_converter())
+    fragment = LanceFragment.create(uri, reader, fragment_id=0, schema=schema)
+    return (fragment, schema)
+
+
+class _BaseLanceDatasink(ray.data.Datasink):
+    """Base Lance Ray Datasink."""
+
+    def __init__(
+        self,
+        uri: str,
+        schema: Optional[pa.Schema] = None,
+        mode: Literal["create", "append", "overwrite"] = "create",
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        self.uri = uri
+        self.schema = schema
+        self.mode = mode
+
+        self.read_version: int | None = None
+
+    def on_write_start(self):
+        if self.mode == "append":
+            ds = lance.LanceDataset(self.uri)
+            self.read_version = ds.version
+            if self.schema is None:
+                self.schema = ds.schema
+
+    def on_write_complete(
+        self,
+        write_results: List[List[Tuple[str, str]]],
+    ):
+        fragments = []
+        schema = None
+        for batch in write_results:
+            for fragment_str, schema_str in batch:
+                fragment = cloudpickle.loads(fragment_str)
+                fragments.append(fragment)
+                schema = cloudpickle.loads(schema_str)
+        if self.mode in set(["create", "overwrite"]):
+            op = lance.LanceOperation.Overwrite(schema, fragments)
+        elif self.mode == "append":
+            op = lance.LanceOperation.Append(fragments)
+        lance.LanceDataset.commit(self.uri, op, read_version=self.read_version)
+
+
+class LanceDatasink(_BaseLanceDatasink):
     """Lance Ray Datasink.
 
     Write a Ray dataset to lance.
@@ -48,11 +143,7 @@ class LanceDatasink(ray.data.Datasink):
         *args,
         **kwargs,
     ):
-        super().__init__(*args, **kwargs)
-
-        self.uri = uri
-        self.schema = schema
-        self.mode = mode
+        super().__init__(uri, schema=schema, mode=mode, *args, **kwargs)
 
         self.max_rows_per_file = max_rows_per_file
         # if mode is append, read_version is read from existing dataset.
@@ -74,59 +165,66 @@ class LanceDatasink(ray.data.Datasink):
         blocks: Iterable[Union[pa.Table, "pd.DataFrame"]],
         ctx,
     ):
-        from ..dependencies import _PANDAS_AVAILABLE
-        from ..dependencies import pandas as pd
+        fragment, schema = _write_fragment(blocks, self.uri, self.schema)
+        return [(cloudpickle.dumps(fragment), cloudpickle.dumps(schema))]
 
-        if self.schema is None:
-            first = next(blocks)
-            if _PANDAS_AVAILABLE and isinstance(first, pd.DataFrame):
-                schema = pa.Schema.from_pandas(first)
-            else:
-                schema = first.schema
-            blocks = chain([first], blocks)
-        else:
-            schema = self.schema
 
-        # TODO: use v2 writer later.
-        def record_batch_converter():
-            for block in blocks:
-                if _PANDAS_AVAILABLE and isinstance(block, pd.DataFrame):
-                    tbl = pa.Table.from_pandas(block, schema=schema)
-                else:
-                    tbl = block
-                yield from tbl.to_batches()
+class LanceFragmentWriter:
+    """Write a fragment to one of Lance fragment.
 
-        reader = pa.RecordBatchReader.from_batches(schema, record_batch_converter())
-        fragment = LanceFragment.create(self.uri, reader, fragment_id=0, schema=schema)
-        ret_schema = None if ctx.task_idx != 0 else schema
-        return (fragment, ret_schema)
+    This Writer can be used in case to write large-than-memory data to lance,
+    in distributed fashion.
+    """
 
-    def on_write_start(self):
-        if self.mode == "append":
-            ds = lance.LanceDataset(self.uri)
-            self.read_version = ds.version
-            if self.schema is None:
-                self.schema = ds.schema
-
-    def on_write_complete(
-        self, write_results: List[Tuple[FragmentMetadata, Optional[pa.Schema]]]
+    def __init__(
+        self,
+        base_uri: str,
+        transform: Callable[[pa.Table], Union[pa.Table, Generator]] = lambda x: x,
+        schema: Optional[pa.Schema] = None,
     ):
-        logging.info(
-            "%s.on_write_complete: committing %s fragments", self, len(write_results)
+        self.base_uri = base_uri
+        self.schema = schema
+        self.transform = transform
+
+    def __call__(self, batch: Union[pa.Table, "pd.DataFrame"]) -> Dict[str, Any]:
+        """Write a Batch to the Lance fragment."""
+
+        transformed = self.transform(batch)
+        if not isinstance(transformed, Generator):
+            transformed = (t for t in [transformed])
+
+        fragment, schema = _write_fragment(transformed, self.base_uri, self.schema)
+        return pa.Table.from_pydict(
+            {
+                "fragment": [cloudpickle.dumps(fragment)],
+                "schema": [cloudpickle.dumps(schema)],
+            }
         )
 
-        schema = self.schema
-        if schema is None:
-            for rst in write_results:
-                if rst[1] is not None:
-                    schema = rst[1]
-                    break
-        if schema is None:
-            raise ValueError("schema is not provided or inferred from data.")
 
-        fragments = [rst[0] for rst in write_results]
-        if self.mode in set(["create", "overwrite"]):
-            op = lance.LanceOperation.Overwrite(schema, fragments)
-        elif self.mode == "append":
-            op = lance.LanceOperation.Append(fragments)
-        lance.LanceDataset.commit(self.uri, op, read_version=self.read_version)
+class LanceCommitter(_BaseLanceDatasink):
+    """Lance Commiter as Ray Datasink.
+
+    This is used with `LanceFragmentWriter` to write large-than-memory data to lance file.
+    """
+
+    @property
+    def num_rows_per_write(self) -> int:
+        return 1
+
+    def get_name(self) -> str:
+        return f"LanceCommiter({self.mode})"
+
+    def write(
+        self,
+        blocks: Iterable[Union[pa.Table, "pd.DataFrame"]],
+        _ctx,
+    ):
+        """Passthrough the fragments to commit phase"""
+        v = []
+        for block in blocks:
+            for fragment, schema in zip(
+                block["fragment"].to_pylist(), block["schema"].to_pylist()
+            ):
+                v.append((fragment, schema))
+        return v
