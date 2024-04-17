3c3d834e8305bac1ec6d961c07d6592486032ba9
Will Jones <willjones127@gmail.com>
2024-04-17 14:46:01 -0700

    pr feedback

--- python/src/fragment.rs
+++ python/src/fragment.rs
@@ -392,11 +392,21 @@ impl FragmentMetadata {
         )
     }
 
+    /// Get the physical rows statistic.
+    ///
+    /// This represents the original number of rows in the fragment
+    /// before any deletions.
+    ///
+    /// If this is None, it is unavailable.
     #[getter]
     fn physical_rows(&self) -> Option<usize> {
         self.inner.physical_rows
     }
 
+    /// Get the number of tombstoned rows in the fragment.
+    ///
+    /// If this is None, this statistic is unavailable. It does not necessarily
+    /// mean there are no deletions.
     #[getter]
     fn num_deletions(&self) -> Option<usize> {
         self.inner
@@ -405,13 +415,18 @@ impl FragmentMetadata {
             .and_then(|d| d.num_deleted_rows)
     }
 
+    /// Get the number of rows in the fragment.
+    ///
+    /// This is equivalent to physical_rows minus num_deletions.
+    ///
+    /// If this is None, this statistic is unavailable.
     #[getter]
     fn num_rows(&self) -> Option<usize> {
         self.inner.num_rows()
     }
 
     #[getter]
-    fn id(&self) -> PyResult<u64> {
+    fn id(&self) -> u64 {
         Ok(self.inner.id)
     }
 }
