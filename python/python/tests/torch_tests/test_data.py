119ba171ec1a4ec1eb1c6e1ae1ab6c299fd80411
Lei Xu <lei@lancedb.com>
2024-05-03 11:17:12 -0700

    store int tensors

--- python/python/tests/torch_tests/test_data.py
+++ python/python/tests/torch_tests/test_data.py
@@ -235,3 +235,20 @@ def test_sample_batches_with_filter(tmp_path: Path):
     assert randomized_ids != all_ids
     randomized_ids.sort()
     assert randomized_ids == all_ids
+
+
+def test_convert_int_tensors(tmp_path: Path):
+    data = np.random.randint(0, 256, size=128 * 32, dtype=np.uint8)
+    fsl = pa.FixedSizeListArray.from_arrays(data, 32)
+    ids = pa.array(range(0, 128), type=pa.int32())
+    tbl = pa.Table.from_arrays([ids, fsl], ["ids", "vec"])
+
+    ds = lance.write_dataset(tbl, tmp_path / "data.lance", max_rows_per_group=32)
+
+    torch_ds = LanceDataset(
+        ds,
+        batch_size=4,
+    )
+    first = next(iter(torch_ds))
+    assert first["vec"].dtype == torch.uint8
+    assert first["vec"].shape == (4, 32)
