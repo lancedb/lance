701aa45a9be4b7c8e5796e0ac95098d70959e028
Lei Xu <lei@lancedb.com>
2024-05-05 10:59:35 -0700

    simiplify hamming

--- rust/lance-linalg/src/distance/hamming.rs
+++ rust/lance-linalg/src/distance/hamming.rs
@@ -4,11 +4,11 @@
 //! Hamming distance.
 
 /// Hamming distance between two vectors.
-pub fn hamming(x: &[u8], y: &[u8]) -> u32 {
+pub fn hamming(x: &[u8], y: &[u8]) -> f32 {
     x.iter()
         .zip(y.iter())
         .map(|(xi, yi)| (xi ^ yi).count_ones())
-        .sum()
+        .sum::<u32>() as f32
 }
 
 #[cfg(test)]
@@ -19,12 +19,12 @@ mod tests {
     fn test_hamming() {
         let x = vec![0b1101_1010, 0b1010_1010, 0b1010_1010];
         let y = vec![0b1101_1010, 0b1010_1010, 0b1010_1010];
-        assert_eq!(hamming(&x, &y), 0);
+        assert_eq!(hamming(&x, &y), 0.0);
 
         let y = vec![0b1101_1010, 0b1010_1010, 0b1010_1000];
-        assert_eq!(hamming(&x, &y), 1);
+        assert_eq!(hamming(&x, &y), 1.0);
 
         let y = vec![0b1101_1010, 0b1010_1010, 0b1010_1001];
-        assert_eq!(hamming(&x, &y), 2);
+        assert_eq!(hamming(&x, &y), 2.0);
     }
 }
