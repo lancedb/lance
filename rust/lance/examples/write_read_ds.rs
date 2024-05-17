f75e87fb32b65d902543b8dae22b768e1d2bdd84
raunaks13 <raunak@eto.ai>
2024-05-17 21:19:22 +0000

    fix: small fixes, reformatting

--- rust/lance/examples/write_read_ds.rs
+++ rust/lance/examples/write_read_ds.rs
@@ -1,68 +1,53 @@
-use std::sync::Arc;
-use arrow::datatypes::{DataType, Field, Schema};
 use arrow::array::UInt32Array;
+use arrow::datatypes::{DataType, Field, Schema};
 use arrow::record_batch::{RecordBatch, RecordBatchIterator};
-use lance::dataset::{WriteParams, WriteMode};
-use lance::Dataset;
 use futures::StreamExt;
+use lance::dataset::{WriteMode, WriteParams};
+use lance::Dataset;
+use std::sync::Arc;
 
 // Writes sample dataset to the given path
 async fn write_dataset(data_path: &str) {
-
     // Define new schema
     let schema = Arc::new(Schema::new(vec![
         Field::new("key", DataType::UInt32, false),
         Field::new("value", DataType::UInt32, false),
     ]));
 
-    // Create new record batch
+    // Create new record batches
     let batch = RecordBatch::try_new(
         schema.clone(),
         vec![
             Arc::new(UInt32Array::from(vec![1, 2, 3, 4, 5, 6])),
             Arc::new(UInt32Array::from(vec![6, 7, 8, 9, 10, 11])),
-            ],
+        ],
     )
     .unwrap();
-    
+
     let batches = RecordBatchIterator::new([Ok(batch)], schema.clone());
 
     // Define write parameters (e.g. overwrite dataset)
     let write_params = WriteParams {
         mode: WriteMode::Overwrite,
-        use_experimental_writer: true, // use lance v2 writer
-        .. Default::default()
+        ..Default::default()
     };
 
-    Dataset::write(batches, data_path, Some(write_params)).await.unwrap();
-
+    Dataset::write(batches, data_path, Some(write_params))
+        .await
+        .unwrap();
 }
 
 // Reads dataset from the given path and prints batch size, schema for all record batches. Also extracts and prints a slice from the first batch
 async fn read_dataset(data_path: &str) {
-
     let dataset = Dataset::open(data_path).await.unwrap();
     let scanner = dataset.scan();
 
-    let batches: Vec<RecordBatch> = scanner
-        .try_into_stream()
-        .await
-        .unwrap()
-        .map(|b| b.unwrap())
-        .collect::<Vec<RecordBatch>>()
-        .await;
+    let mut batch_stream = scanner.try_into_stream().await.unwrap().map(|b| b.unwrap());
 
-    // Iterate over record batches
-    for (batch_index, batch) in batches.iter().enumerate() {
-        println!("RecordBatch #{}:", batch_index);
-        println!("Batch size: {}, {}", batch.num_rows(), batch.num_columns());  // print size of batch
-        println!("Schema: {:?}", batch.schema());         // print schema of recordbatch
+    while let Some(batch) = batch_stream.next().await {
+        println!("Batch size: {}, {}", batch.num_rows(), batch.num_columns()); // print size of batch
+        println!("Schema: {:?}", batch.schema()); // print schema of recordbatch
     }
-
-    // extract a slice (rows 3-4) from the first batch
-    let target_batch = &batches[0];
-    let slice_batch = target_batch.slice(3, 1);    
-    println!("Sliced batch: {:?}", slice_batch)
 }
 
 #[tokio::main]
