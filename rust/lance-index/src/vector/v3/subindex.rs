29d23c9f5152590ed35f0e6701640749aa7df877
rmeng <rob.xu.meng@gmail.com>
2024-05-22 23:39:21 -0400

    Update (base update)
    [ghstack-poisoned]

--- rust/lance-index/src/vector/v3/subindex.rs
+++ rust/lance-index/src/vector/v3/subindex.rs
@@ -2,7 +2,6 @@
 // SPDX-FileCopyrightText: Copyright The Lance Authors
 
 use arrow_array::{RecordBatch, StructArray};
-use arrow_schema::SchemaRef;
 use lance_core::Result;
 use num_traits::Num;
 use roaring::RoaringBitmap;
@@ -10,10 +9,10 @@ use roaring::RoaringBitmap;
 use super::storage::VectorStore;
 
 /// A sub index for IVF index
-pub trait IvfSubIndex<T: Num, Store: VectorStore>: Send + Sync + Sized {
+pub trait IvfSubIndex: Send + Sync + Sized {
     type QueryParams: Default;
 
-    fn index_name(&self) -> &str;
+    fn name(&self) -> &str;
 
     /// Search the sub index for nearest neighbors.
     /// # Arguments:
@@ -21,42 +20,21 @@ pub trait IvfSubIndex<T: Num, Store: VectorStore>: Send + Sync + Sized {
     /// * `k` - The number of nearest neighbors to return
     /// * `params` - The query parameters
     /// * `pre_filter_bitmap` - The pre filter bitmap indicating which vectors to skip
-    fn search(
+    fn search<T: Num>(
         &self,
         query: &[T],
         k: usize,
         params: Self::QueryParams,
-        storage: &Store,
+        storage: &impl VectorStore,
         pre_filter_bitmap: Option<RoaringBitmap>,
     ) -> Result<RecordBatch>;
-
-    // check if the builder supports the metadata schema requested
-    fn supports_metadata(&self, _schema: SchemaRef) -> bool {
-        false
-    }
 
     /// Load the sub index from a struct array with a single row
     fn load(data: StructArray) -> Result<Self>;
 
     /// Given a vector storage, containing all the data for the IVF partition, build the sub index.
-    fn index(&self, storage: &Store) -> Result<()>;
+    fn index_vectors(&self, storage: &impl VectorStore) -> Result<()>;
 
-    /// Turn the sub index into a struct array
+    /// Encode the sub index into a struct array
     fn to_array(&self) -> Result<StructArray>;
-
-    /// Given a vector storage, containing all the data for the IVF partition, build the sub index.
-    /// The returned value is a struct array with a SINGLE ROW and always have the same schema
-    ///
-    /// It is recommended to not implement this method and use the default implementation
-    /// The implementation should implement `index` and `to_array` method instead, as they make the index
-    /// appendable
-    ///
-    /// NOTE: we use a single record batch to avoid the need of async operations to read the data
-    ///
-    /// The roundtrip looks like
-    /// IvfSubIndexBuilder.index(data).to_array()
-    fn build(&self, storage: &Store) -> Result<StructArray> {
-        self.index(storage)?;
-        self.to_array()
-    }
 }
