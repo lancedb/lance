e198c51bc0189b1fa58882817fabadf8b964f250
Lei Xu <lei@lancedb.com>
2024-06-05 15:50:22 -0700

    add missing license

--- rust/lance/src/index/prefilter.rs
+++ rust/lance/src/index/prefilter.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright The Lance Authors
+
 //! Secondary Index pre-filter
 //!
 //! Based on the query, we might have information about which fragment ids and
