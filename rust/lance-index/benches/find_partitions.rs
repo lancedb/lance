c5a6dca34dfebd184f0d3ab9defef5a17dfb1618
Lei Xu <lei@eto.ai>
2024-06-05 08:57:08 -0700

    revert find_partitions

--- rust/lance-index/benches/find_partitions.rs
+++ rust/lance-index/benches/find_partitions.rs
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright The Lance Authors
 
-mod sq;
-
 use arrow_array::Float32Array;
 use arrow_array::{types::Float32Type, FixedSizeListArray};
 use lance_arrow::FixedSizeListArrayExt;
