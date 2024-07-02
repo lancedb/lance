48c25620a7deb3e6ee0139797f698e79c9ad2772
Weston Pace <weston.pace@gmail.com>
2024-07-02 10:59:27 -0700

    Add tests for vector data type

--- python/python/tests/test_indices.py
+++ python/python/tests/test_indices.py
@@ -6,8 +6,8 @@ import pyarrow as pa
 from lance.indices import IndicesBuilder, IvfModel
 
 
-def gen_dataset(tmpdir):
-    vectors = np.random.randn(10000, 128).astype(np.float32)
+def gen_dataset(tmpdir, datatype=np.float32):
+    vectors = np.random.randn(10000, 128).astype(datatype)
     vectors.shape = -1
     vectors = pa.FixedSizeListArray.from_arrays(vectors, 128)
     table = pa.Table.from_arrays([vectors], names=["vectors"])
@@ -28,6 +28,22 @@ def test_ivf_centroids(tmpdir):
     reloaded = IvfModel.load(str(tmpdir / "ivf"))
     assert reloaded.distance_type == "l2"
     assert ivf.centroids == reloaded.centroids
+
+
+def test_ivf_centroids_column_type(tmpdir):
+    def check(column_type, typename):
+        ds = gen_dataset(tmpdir / typename, column_type)
+        ivf = IndicesBuilder(ds).train_ivf(
+            "vectors", sample_rate=16, accelerator="cuda"
+        )
+        assert len(ivf.centroids) == 100
+        ivf.save(str(tmpdir / f"ivf_{typename}"))
+        reloaded = IvfModel.load(str(tmpdir / f"ivf_{typename}"))
+        assert ivf.centroids == reloaded.centroids
+
+    check(np.float16, "f16")
+    check(np.float32, "f32")
+    check(np.float64, "f64")
 
 
 def test_ivf_centroids_distance_type(tmpdir):
