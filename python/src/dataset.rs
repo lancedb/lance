cc0efacc9c12846a1c56e53b34d8b7d1d1343189
Weston Pace <weston.pace@gmail.com>
2024-07-20 05:50:15 -0700

    Update dataset.rs

--- python/src/dataset.rs
+++ python/src/dataset.rs
@@ -622,7 +622,6 @@ impl Dataset {
         self_: PyRef<'_, Self>,
         row_indices: Vec<u64>,
         columns: Option<Vec<String>>,
-        columns_with_transform: Option<Vec<(String, String)>,
     ) -> PyResult<PyObject> {
         let projection = if let Some(columns) = columns {
             self_.ds.schema().project(&columns)
