733c90a65e1131ebfbb1927dee791fac244385cc
Will Jones <willjones127@gmail.com>
2024-07-25 11:23:49 -0700

    feat: support deletion with stable row id and scalar indices (#2609)
    `MaterializeIndex` only produces `_rowid`, so to produce `_rowaddr` we
    need to use a new node to add that column in.
    
    Stacked on #2608

--- rust/lance/src/dataset.rs
+++ rust/lance/src/dataset.rs
@@ -2610,7 +2610,10 @@ mod tests {
 
     #[rstest]
     #[tokio::test]
-    async fn test_delete(#[values(false, true)] use_legacy_format: bool) {
+    async fn test_delete(
+        #[values(false, true)] use_legacy_format: bool,
+        #[values(false, true)] with_scalar_index: bool,
+    ) {
         use std::collections::HashSet;
 
         fn sequence_data(range: Range<u32>) -> RecordBatch {
@@ -2641,6 +2644,19 @@ mod tests {
         let mut dataset = TestDatasetGenerator::new(batches, use_legacy_format)
             .make_hostile(test_uri)
             .await;
+
+        if with_scalar_index {
+            dataset
+                .create_index(
+                    &["i"],
+                    IndexType::Scalar,
+                    Some("scalar_index".to_string()),
+                    &ScalarIndexParams::default(),
+                    false,
+                )
+                .await
+                .unwrap();
+        }
 
         // Delete nothing
         dataset.delete("i < 0").await.unwrap();
