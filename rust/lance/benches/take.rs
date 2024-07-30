937c61b85a949014bd02f0c53dedf25fe057616a
BubbleCal <bubble-cal@outlook.com>
2024-07-30 10:28:27 +0800

    fix
    Signed-off-by: BubbleCal <bubble-cal@outlook.com>

--- rust/lance/benches/take.rs
+++ rust/lance/benches/take.rs
@@ -17,6 +17,7 @@ use object_store::ObjectStore;
 use pprof::criterion::{Output, PProfProfiler};
 use rand::Rng;
 use std::sync::Arc;
+use std::time::Duration;
 use url::Url;
 
 use lance::dataset::{Dataset, WriteMode, WriteParams};
