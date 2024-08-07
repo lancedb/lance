5eab0b1278437771ecaa795d26d0d667e5523d26
Lei Xu <lei@lancedb.com>
2024-08-06 20:37:21 -0700

    chore: remove MatrixView (#2700)
    We introduced MatrixView to make it easy to calculate SVD for OPQ.
    However we dont use OPQ anymore, MatrixView adds extra layer of
    abstraction that is not necessary.

--- rust/lance-index/src/vector/pq/utils.rs
+++ rust/lance-index/src/vector/pq/utils.rs
@@ -1,49 +1,45 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright The Lance Authors
 
-use std::sync::Arc;
-
-use lance_arrow::ArrowFloatType;
+use arrow_array::{cast::AsArray, types::ArrowPrimitiveType, Array, FixedSizeListArray};
 use lance_core::{Error, Result};
-use lance_linalg::MatrixView;
 use snafu::{location, Location};
 
 /// Divide a 2D vector in [`T::Array`] to `m` sub-vectors.
 ///
 /// For example, for a `[1024x1M]` matrix, when `n = 8`, this function divides
 /// the matrix into  `[128x1M; 8]` vector of matrix.
-pub(super) fn divide_to_subvectors<T: ArrowFloatType>(
-    data: &MatrixView<T>,
+pub(super) fn divide_to_subvectors<T: ArrowPrimitiveType>(
+    fsl: &FixedSizeListArray,
     m: usize,
-) -> Result<Vec<Arc<T::ArrayType>>> {
-    assert!(!data.num_rows() > 0);
-    if data.num_columns() % m != 0 {
+) -> Result<Vec<Vec<T::Native>>> {
+    let dim = fsl.value_length() as usize;
+    if dim % m != 0 {
         return Err(Error::invalid_input(
             format!(
                 "num_sub_vectors must divide vector dimension {}, but got {}",
-                data.num_columns(),
-                m
+                dim, m
             ),
             location!(),
         ));
-    }
+    };
 
-    let sub_vector_length = data.num_columns() / m;
-    let capacity = data.num_rows() * sub_vector_length;
-    let mut subarrays = vec![];
+    let sub_vector_length = dim / m;
+    let capacity = fsl.len() * sub_vector_length;
+    let mut subarrays = vec![Vec::with_capacity(capacity); m];
 
     // TODO: very intensive memory copy involved!!! But this is on the write path.
     // Optimize for memory copy later.
-    for i in 0..m {
-        let mut builder = Vec::with_capacity(capacity);
-        for j in 0..data.num_rows() {
-            let row = data.row_ref(j).unwrap();
-            let start = i * sub_vector_length;
-            builder.extend_from_slice(&row[start..start + sub_vector_length]);
-        }
-        let values = T::ArrayType::from(builder);
-        subarrays.push(Arc::new(values));
-    }
+    fsl.values()
+        .as_primitive::<T>()
+        .values()
+        .chunks(dim)
+        .for_each(|vec| {
+            for i in 0..m {
+                subarrays[i]
+                    .extend_from_slice(&vec[i * sub_vector_length..(i + 1) * sub_vector_length]);
+            }
+        });
     Ok(subarrays)
 }
 
@@ -78,22 +74,23 @@ pub fn get_sub_vector_centroids<T>(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use arrow_array::{types::Float32Type, Float32Array};
+    use arrow_array::{types::Float32Type, FixedSizeListArray, Float32Array};
+    use lance_arrow::FixedSizeListArrayExt;
 
     #[test]
     fn test_divide_to_subvectors() {
         let values = Float32Array::from_iter((0..320).map(|v| v as f32));
         // A [10, 32] array.
-        let mat = MatrixView::new(values.into(), 32);
+        let mat = FixedSizeListArray::try_new_from_values(values, 32).unwrap();
         let sub_vectors = divide_to_subvectors::<Float32Type>(&mat, 4).unwrap();
         assert_eq!(sub_vectors.len(), 4);
         assert_eq!(sub_vectors[0].len(), 10 * 8);
 
         assert_eq!(
-            sub_vectors[0].as_ref(),
-            &Float32Array::from_iter_values(
-                (0..10).flat_map(|i| (0..8).map(move |c| 32.0 * i as f32 + c as f32))
-            )
+            sub_vectors[0],
+            (0..10)
+                .flat_map(|i| (0..8).map(move |c| 32.0 * i as f32 + c as f32))
+                .collect::<Vec<_>>()
         );
     }
 }
