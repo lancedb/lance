b576c0cd97ce16a0a959e0604bfd58dfa59dec5e
Weston Pace <weston.pace@gmail.com>
2024-08-16 13:18:35 -0700

    docs: document newly added compaction options in optimize.py (#2746)
    The options were being passed through correctly but the documentation
    was incorrect. This PR also adds a test confirming that
    `Compaction.plan` respects the new options.

--- python/python/tests/test_optimize.py
+++ python/python/tests/test_optimize.py
@@ -45,7 +45,6 @@ def test_optimize_max_bytes(tmp_path: Path):
     dataset = lance.write_dataset(
         data, base_dir, max_rows_per_file=2 * 1024, data_storage_version="stable"
     )
-    assert dataset.version == 1
     assert len(dataset.get_fragments()) == 2
 
     # max_bytes_per_file is too small and we get tiny files
@@ -63,8 +62,6 @@ def test_optimize_max_bytes(tmp_path: Path):
     assert metrics.files_removed == 2
     assert metrics.files_added == 4
 
-    assert dataset.version == 3
-
     num_frags = len(dataset.get_fragments())
     assert num_frags == 4
 
@@ -76,7 +73,29 @@ def test_optimize_max_bytes(tmp_path: Path):
         mode="overwrite",
     )
 
-    # max_bytes_per_file is still too small but the batch size
+    # Same test but use Compaction.plan
+    plan = Compaction.plan(
+        dataset,
+        options=dict(
+            target_rows_per_fragment=100 * 1024, max_bytes_per_file=1000, batch_size=128
+        ),
+    )
+    results = [task.execute(dataset) for task in plan.tasks]
+    metrics = Compaction.commit(dataset, results)
+    assert metrics.fragments_removed == 2
+    assert metrics.fragments_added == 4
+    assert metrics.files_removed == 2
+    assert metrics.files_added == 4
+
+    dataset = lance.write_dataset(
+        data,
+        base_dir,
+        max_rows_per_file=2 * 1024,
+        data_storage_version="stable",
+        mode="overwrite",
+    )
+
+    # In this test max_bytes_per_file is still too small but the batch size
     # is so large we read the entire input in a single batch
     metrics = dataset.optimize.compact_files(
         target_rows_per_fragment=100 * 1024,
@@ -89,8 +108,6 @@ def test_optimize_max_bytes(tmp_path: Path):
     assert metrics.fragments_added == 2
     assert metrics.files_removed == 2
     assert metrics.files_added == 2
-
-    assert dataset.version == 6
 
     num_frags = len(dataset.get_fragments())
     assert num_frags == 2
