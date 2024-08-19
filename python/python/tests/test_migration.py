849d48a9bf390fb201a6eb25b840b05e443692b8
Weston Pace <weston.pace@gmail.com>
2024-08-19 12:12:22 -0700

    Move historical datasets into test_data folder

--- python/python/tests/test_migration.py
+++ python/python/tests/test_migration.py
@@ -8,10 +8,12 @@ import lance
 import pyarrow as pa
 
 
-def prep_dataset(tmp_path: Path, name: str):
-    dataset_dir = Path(__file__).parent / "historical_datasets" / name
-    shutil.copytree(dataset_dir, tmp_path / name)
-    return lance.dataset(tmp_path / name)
+def prep_dataset(tmp_path: Path, version, name: str):
+    dataset_dir = (
+        Path(__file__).parent.parent.parent.parent / "test_data" / version / name
+    )
+    shutil.copytree(dataset_dir, tmp_path / version / name)
+    return lance.dataset(tmp_path / version / name)
 
 
 def test_add_data_storage_version(tmp_path: Path):
@@ -25,13 +27,13 @@ def test_add_data_storage_version(tmp_path: Path):
     tab = pa.table({"x": range(1024)})
 
     def check_dataset(dataset_name: str, expected_version: str):
-        ds = prep_dataset(tmp_path, dataset_name)
+        ds = prep_dataset(tmp_path, "v0.15.0", dataset_name)
         assert ds.data_storage_version == expected_version
 
         lance.write_dataset(tab, ds.uri, mode="append")
         assert ds.data_storage_version == expected_version
 
-    check_dataset("0_15_0_v1_no_files.lance", "0.1")
-    check_dataset("0_15_0_v2_no_files.lance", "2.0")
-    check_dataset("0_15_0_v1_with_files.lance", "0.1")
-    check_dataset("0_15_0_v2_with_files.lance", "2.0")
+    check_dataset("v1_no_files.lance", "0.1")
+    check_dataset("v2_no_files.lance", "2.0")
+    check_dataset("v1_with_files.lance", "0.1")
+    check_dataset("v2_with_files.lance", "2.0")
