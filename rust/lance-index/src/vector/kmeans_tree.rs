7e647d8f5be687a4e8066175818bd82420c87364
jacketsj <jacketsj@gmail.com>
2024-08-29 20:03:26 -0700

    Seemingly complete a kmeans tree build implementation

--- rust/lance-index/src/vector/kmeans_tree.rs
+++ rust/lance-index/src/vector/kmeans_tree.rs
@@ -5,14 +5,15 @@
 //!
 //! Support ``cosine`` distances only for now
 
-use arrow_array::{Array, ArrayRef, FixedSizeListArray, RecordBatch}; //, Float32Array};
-                                                                     // use core::ops::Range;
 use crate::vector::VECTOR_RESULT_SCHEMA;
+use arrow_array::{Array, ArrayRef, FixedSizeListArray, Float32Array, RecordBatch};
+use core::cmp::min;
 use lance_core::{Error, Result};
 use snafu::{location, Location};
+use std::sync::Arc;
 use {
     lance_linalg::distance::DistanceType,
-    // lance_linalg::kmeans::{compute_partitions_arrow_array, KMeans},
+    lance_linalg::kmeans::{compute_partitions_arrow_array, KMeans},
 };
 
 #[derive(Debug, Clone)]
@@ -37,7 +38,8 @@ pub struct KMeansTreeParams {
 #[derive(Debug, Clone)]
 pub struct KMeansTree {
     /// TODO: Temporary for development & testing: All the data stored in memory
-    pub data: FixedSizeListArray,
+    pub data: Vec<f32>,
+    pub dimension: usize,
 
     /// Parameters for indexing/inserting
     pub params: KMeansTreeParams,
@@ -56,7 +58,7 @@ pub struct ClusteringNode {
     /// During initial build, this does not include spilled nodes
     pub children: Vec<usize>,
     /// For each child, also record the centroid
-    pub centroids: Vec<Vec<f32>>,
+    pub centroids: FixedSizeListArray,
 }
 
 /// A leaf node in the k-means tree.
@@ -66,6 +68,22 @@ pub struct LeafCluster {
     pub offset: usize,
     /// How many data points are assigned to this node (incl spills)
     pub size: usize,
+}
+
+fn chunk_to_fixed_size_list_array(
+    float_array: Arc<Float32Array>,
+    dimension: i32,
+) -> FixedSizeListArray {
+    // Define the field for the list elements
+    let field = Arc::new(arrow_schema::Field::new(
+        "item",
+        arrow_schema::DataType::Float32,
+        true,
+    ));
+
+    // Create the FixedSizeListArray
+    FixedSizeListArray::try_new(field, dimension, float_array as ArrayRef, None)
+        .expect("Unable to create FixedSizeListArray")
 }
 
 impl KMeansTree {
@@ -95,17 +113,79 @@ impl KMeansTree {
         self.clusterings_per_layer.push(
             old_leaf_clusters
                 .into_iter()
-                .map(|_leaf: LeafCluster| {
-                    // TODO
-                    // - call kmeans
-                    // - get centroids
-                    // - shuffle data and get offsets
-                    // - add new leaf nodes to self.leaf_clusters
-                    //
-                    // stub (to be replaced)
+                .map(|leaf: LeafCluster| {
+                    let data_slice = chunk_to_fixed_size_list_array(
+                        Arc::new(Float32Array::from(
+                            self.data[leaf.offset..leaf.offset + leaf.size].to_vec(),
+                        )),
+                        self.dimension as i32,
+                    );
+                    // let data_slice = self.data.slice(leaf.offset, leaf.offset + leaf.size);
+                    let k = min(leaf.size, self.params.k); // don't make empty clusters
+                    let kmeans = KMeans::new(&data_slice, k, self.params.max_iters).unwrap();
+
+                    let centroids = chunk_to_fixed_size_list_array(
+                        Arc::new(
+                            kmeans
+                                .centroids
+                                .as_any()
+                                .downcast_ref::<Float32Array>()
+                                .unwrap()
+                                .clone(),
+                        ),
+                        self.dimension as i32,
+                    );
+                    let partitions = compute_partitions_arrow_array(
+                        &centroids,
+                        &data_slice,
+                        self.params.distance_type,
+                    )
+                    .unwrap();
+
+                    let mut children = Vec::with_capacity(k);
+
+                    let mut partitioned_indices: Vec<Vec<usize>> = vec![Vec::new(); k];
+
+                    for (i, partition) in partitions.iter().enumerate() {
+                        if let Some(cluster_idx) = partition {
+                            partitioned_indices[*cluster_idx as usize].push(i);
+                        }
+                    }
+
+                    let mut new_data = Vec::with_capacity(data_slice.len());
+                    let mut current_offset = leaf.offset;
+                    for (_cluster_idx, indices) in partitioned_indices.iter().enumerate() {
+                        if !indices.is_empty() {
+                            let partition_size = indices.len();
+
+                            // Copy the corresponding data points into the new_data vector
+                            for &index in indices {
+                                new_data.push(data_slice.value(index).clone());
+                            }
+
+                            self.leaf_clusters.push(LeafCluster {
+                                offset: current_offset,
+                                size: partition_size,
+                            });
+                            children.push(self.leaf_clusters.len() - 1);
+                            current_offset += partition_size;
+                        }
+                    }
+                    for (i, value) in new_data.iter().enumerate() {
+                        self.data[(i + leaf.offset) * self.dimension
+                            ..(i + leaf.offset + 1) * self.dimension]
+                            .copy_from_slice(
+                                value
+                                    .as_any()
+                                    .downcast_ref::<Float32Array>()
+                                    .unwrap()
+                                    .values(),
+                            );
+                    }
+
                     ClusteringNode {
-                        children: Vec::new(),
-                        centroids: Vec::new(),
+                        children,
+                        centroids,
                     }
                 })
                 .collect(),
@@ -132,7 +212,14 @@ impl KMeansTree {
 
         // Create the tree with one root node
         let mut tree = KMeansTree {
-            data: data.clone(),
+            data: data
+                .values()
+                .as_any()
+                .downcast_ref::<Float32Array>()
+                .unwrap()
+                .values()
+                .to_vec(),
+            dimension: data.value_length() as usize,
             params,
             clusterings_per_layer: vec![],
             leaf_clusters: vec![LeafCluster {
