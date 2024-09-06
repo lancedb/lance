15c243a569be776c4a329fb4efdca242b5941283
jacketsj <jacketsj@gmail.com>
2024-09-06 15:03:37 -0700

    Move import check to top of kmeans.py

--- python/python/lance/cuvs/__init__.py
+++ python/python/lance/cuvs/__init__.py
@@ -1,18 +1,2 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright The Lance Authors
-
-import importlib.util
-
-if importlib.util.find_spec("cuvs") is None:
-    raise ImportError(
-        "cuvs is not installed. Please install cuvs"
-        + " to use lance.cuvs module (optional dependency name: "
-        + '"cuvs-py39"/"cuvs-py310"/"cuvs-py311")',
-    )
-
-if importlib.util.find_spec("pylibraft") is None:
-    raise ImportError(
-        "pylibraft is not installed. Please install pylibraft"
-        + " to use lance.cuvs module (optional dependency name: "
-        + '"cuvs-py39"/"cuvs-py310"/"cuvs-py311")',
-    )
--- python/python/lance/cuvs/kmeans.py
+++ python/python/lance/cuvs/kmeans.py
@@ -1,3 +1,19 @@
+import importlib.util
+
+if importlib.util.find_spec("cuvs") is None:
+    raise ImportError(
+        "cuvs is not installed. Please install cuvs"
+        + " to use lance.cuvs module (optional dependency name: "
+        + '"cuvs-py39"/"cuvs-py310"/"cuvs-py311")',
+    )
+
+if importlib.util.find_spec("pylibraft") is None:
+    raise ImportError(
+        "pylibraft is not installed. Please install pylibraft"
+        + " to use lance.cuvs module (optional dependency name: "
+        + '"cuvs-py39"/"cuvs-py310"/"cuvs-py311")',
+    )
+
 import logging
 import time
 from typing import Literal, Optional, Tuple, Union
