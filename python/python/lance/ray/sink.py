3dda1df33a414a1444ca2267f28201e4b82442d6
huangzhaowei <huangzhaowei.416@bytedance.com>
2024-09-26 14:21:58 +0800

    add test case

--- python/python/lance/ray/sink.py
+++ python/python/lance/ray/sink.py
@@ -372,6 +372,21 @@ class LanceCommitter(_BaseLanceDatasink):
         """Passthrough the fragments to commit phase"""
         v = []
         for block in blocks:
+            # Empty sink did not have "fragment" and "schema" in block
+            if isinstance(block, pa.Table) and block.num_columns == 0:
+                continue
+
+            from ..dependencies import _PANDAS_AVAILABLE
+            from ..dependencies import pandas as pd
+
+            # check block as pandas
+            if (
+                _PANDAS_AVAILABLE
+                and isinstance(block, pd.DataFrame)
+                and len(block.columns) == 0
+            ):
+                continue
+
             for fragment, schema in zip(
                 block["fragment"].to_pylist(), block["schema"].to_pylist()
             ):
