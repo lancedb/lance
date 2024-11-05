b7d7057ac3d4e5da2bf8f8280916e0e10910818f
broccoliSpicy <broccoli.spicy@outlook.com>
2024-11-05 12:15:04 -0500

    lint

--- rust/lance-encoding/src/statistics.rs
+++ rust/lance-encoding/src/statistics.rs
@@ -1197,7 +1197,7 @@ mod tests {
     fn test_max_length_variable_width_datablock() {
         let string_array = StringArray::from(vec![Some("hello"), Some("world")]);
         let block = DataBlock::from_array(string_array.clone());
-      
+
         let expected_max_length =
             Arc::new(UInt64Array::from(vec![string_array.value_length(0) as u64])) as ArrayRef;
         let actual_max_length = block.get_stat(Stat::MaxLength);
