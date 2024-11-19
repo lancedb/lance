566082fcfa6d2b56fed8ac19a2aa607caa4df9e5
Will Jones <willjones127@gmail.com>
2024-11-19 11:46:29 -0800

    feat!: allow passing down existing dataset for write (#3119)
    BREAKING CHANGE: return value in Rust of `write_fragments()` has changed
    to `Result<Transaction>`.
    
    * Closes #3058
    * Consolidates different implementations of writing and committing into
    two builders: `InsertBuilder` and `CommitBuilder`.
    * Allow passing number of commit retries into `CommitBuilder`.
    * Adds Python `LanceDataset.insert()` method to modify existing
    datasets.
      * Will be used to fix https://github.com/lancedb/lancedb/issues/1655
    * Closes #1891
    
    ---------
    
    Co-authored-by: Weston Pace <weston.pace@gmail.com>

--- python/src/fragment.rs
+++ python/src/fragment.rs
@@ -21,11 +21,14 @@ use arrow_array::RecordBatchReader;
 use arrow_schema::Schema as ArrowSchema;
 use futures::TryFutureExt;
 use lance::dataset::fragment::FileFragment as LanceFragment;
-use lance::dataset::NewColumnTransform;
+use lance::dataset::transaction::Operation;
+use lance::dataset::{InsertBuilder, NewColumnTransform, WriteDestination};
+use lance::Error;
 use lance_table::format::{DataFile as LanceDataFile, Fragment as LanceFragmentMetadata};
 use lance_table::io::deletion::deletion_file_path;
 use pyo3::prelude::*;
 use pyo3::{exceptions::*, pyclass::CompareOp, types::PyDict};
+use snafu::{location, Location};
 
 use crate::dataset::{get_write_params, transforms_from_python};
 use crate::error::PythonErrorExt;
@@ -454,9 +457,9 @@ impl FragmentMetadata {
 }
 
 #[pyfunction(name = "_write_fragments")]
-#[pyo3(signature = (dataset_uri, reader, **kwargs))]
+#[pyo3(signature = (dest, reader, **kwargs))]
 pub fn write_fragments(
-    dataset_uri: &str,
+    dest: &Bound<PyAny>,
     reader: &Bound<PyAny>,
     kwargs: Option<&PyDict>,
 ) -> PyResult<Vec<FragmentMetadata>> {
@@ -467,17 +470,37 @@ pub fn write_fragments(
         .transpose()?
         .unwrap_or_default();
 
+    let dest = if dest.is_instance_of::<Dataset>() {
+        let dataset: Dataset = dest.extract()?;
+        WriteDestination::Dataset(dataset.ds.clone())
+    } else {
+        WriteDestination::Uri(dest.extract()?)
+    };
+
     let written = RT
-        .block_on(Some(reader.py()), async {
-            lance::dataset::write_fragments(dataset_uri, batches, params).await
-        })?
+        .block_on(
+            Some(reader.py()),
+            InsertBuilder::new(dest)
+                .with_params(&params)
+                .execute_uncommitted_stream(batches),
+        )?
         .map_err(|err| PyIOError::new_err(err.to_string()))?;
 
     assert!(
-        written.blob.is_none(),
+        written.blobs_op.is_none(),
         "Blob writing is not yet supported by the python _write_fragments API"
     );
-    let fragments = written.default.0;
+
+    let get_fragments = |operation| match operation {
+        Operation::Overwrite { fragments, .. } => Ok(fragments),
+        Operation::Append { fragments, .. } => Ok(fragments),
+        _ => Err(Error::Internal {
+            message: "Unexpected operation".into(),
+            location: location!(),
+        }),
+    };
+    let fragments =
+        get_fragments(written.operation).map_err(|err| PyRuntimeError::new_err(err.to_string()))?;
 
     fragments
         .into_iter()
