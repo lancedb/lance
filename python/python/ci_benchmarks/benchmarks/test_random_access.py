f7741e3244f9a14ade98490082a7b3eb4cd3580d
Weston Pace <weston.pace@gmail.com>
2024-11-22 10:58:00 -0800

    Copy over the benchmarks from the benchmarks repo

---
+++ python/python/ci_benchmarks/benchmarks/test_random_access.py
@@ -0,0 +1,21 @@
+import random
+
+import lance
+import pytest
+from ci_benchmarks.datasets import get_dataset_uri
+
+DATASETS = ["tpch"]
+
+
+@pytest.mark.parametrize("dataset", DATASETS)
+def test_random_access(benchmark, dataset):
+    NUM_INDICES = 10
+    dataset_uri = get_dataset_uri(dataset)
+
+    ds = lance.dataset(dataset_uri)
+    random_indices = [random.randint(0, ds.count_rows()) for _ in range(NUM_INDICES)]
+
+    def bench(random_indices):
+        ds.take(random_indices)
+
+    benchmark.pedantic(bench, args=(random_indices,), rounds=5)
---
+++ python/python/ci_benchmarks/benchmarks/test_search.py
@@ -0,0 +1,33 @@
+import lance
+import pytest
+from ci_benchmarks.datasets import get_dataset_uri
+
+COLUMN_LABELS = ["bools", "normals"]
+COLUMNS = [["bools"], ["normals"]]
+FILTERS = [None, "bools IS TRUE"]
+
+
+@pytest.mark.parametrize("columns", COLUMNS, ids=COLUMN_LABELS)
+@pytest.mark.parametrize("filt", FILTERS)
+def test_eda_search(benchmark, columns, filt):
+    dataset_uri = get_dataset_uri("image_eda")
+
+    batch_size = 32 if columns == ["image_data"] else None
+    limit = None if filter is None else 100000
+    frag_readahead = (
+        4
+        if (columns == ["image_data"] or columns == ["strings"]) and filter is None
+        else None
+    )
+
+    def bench():
+        ds = lance.dataset(dataset_uri)
+        ds.to_table(
+            columns=columns,
+            filter=filt,
+            batch_size=batch_size,
+            fragment_readahead=frag_readahead,
+            limit=limit,
+        )
+
+    benchmark.pedantic(bench, rounds=1, iterations=1)
