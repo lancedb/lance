a3eb9871b7fa792418ea2e2691472e947ba9ded7
huangzhaowei <carlmartinmax@gmail.com>
2024-11-28 21:44:29 +0800

    fix format

--- rust/lance/src/dataset/fragment/write.rs
+++ rust/lance/src/dataset/fragment/write.rs
@@ -456,7 +456,10 @@ mod tests {
         // Uses provided schema. Field ids are correct in fragment metadata.
         let data = test_data();
         let tmp_dir = tempfile::tempdir().unwrap();
-        let writer_params = WriteParams { max_rows_per_file: 1, ..Default::default() };
+        let writer_params = WriteParams {
+            max_rows_per_file: 1,
+            ..Default::default()
+        };
         let fragments = FragmentCreateBuilder::new(tmp_dir.path().to_str().unwrap())
             .write_params(&writer_params)
             .write_fragments(data)
