805438f85a09f0428ff4afeaac564fff32ba331a
Weston Pace <weston.pace@gmail.com>
2024-12-20 13:10:41 -0800

    fix: when taking struct fields they should be merged into the output in the correct order (#3277)
    In various situations we need to fetch some fields from a struct and
    then later add more fields for the struct. For example, maybe we have a
    `struct<big_string: string, filter: i32>`. We might query with a filter
    on `filter` and then use late materialization to take `big_string`. When
    we do this we were previously creating `struct<filter: i32, big_string:
    string>` which would cause issues since that isn't the correct data
    type.
    
    In creating this fix I added a new `Projection` concept and I would like
    to slowly replace a lot of the places where we use schemas as
    projections to use `Projection` instead. Not necessarily for performance
    but more for convenience.

--- rust/lance/src/dataset/write/merge_insert.rs
+++ rust/lance/src/dataset/write/merge_insert.rs
@@ -54,7 +54,7 @@ use futures::{
     Stream, StreamExt, TryStreamExt,
 };
 use lance_core::{
-    datatypes::SchemaCompareOptions,
+    datatypes::{OnMissing, OnTypeMismatch, SchemaCompareOptions},
     error::{box_error, InvalidInputSnafu},
     utils::{
         futures::Capacity,
@@ -454,12 +454,19 @@ impl MergeInsertJob {
         }
 
         // 4 - Take the mapped row ids
-        let mut target = Arc::new(TakeExec::try_new(
-            self.dataset.clone(),
-            index_mapper,
-            Arc::new(self.dataset.schema().project_by_schema(schema.as_ref())?),
-            get_num_compute_intensive_cpus(),
-        )?) as Arc<dyn ExecutionPlan>;
+        let projection = self
+            .dataset
+            .empty_projection()
+            .union_arrow_schema(schema.as_ref(), OnMissing::Error)?;
+        let mut target = Arc::new(
+            TakeExec::try_new(
+                self.dataset.clone(),
+                index_mapper,
+                projection,
+                get_num_compute_intensive_cpus(),
+            )?
+            .unwrap(),
+        ) as Arc<dyn ExecutionPlan>;
 
         // 5 - Take puts the row id and row addr at the beginning.  A full scan (used when there is
         //     no scalar index) puts the row id and addr at the end.  We need to match these up so
@@ -632,7 +639,11 @@ impl MergeInsertJob {
             ) -> Result<usize> {
                 // batches still have _rowaddr
                 let write_schema = batches[0].schema().as_ref().without_column(ROW_ADDR);
-                let write_schema = dataset.local_schema().project_by_schema(&write_schema)?;
+                let write_schema = dataset.local_schema().project_by_schema(
+                    &write_schema,
+                    OnMissing::Error,
+                    OnTypeMismatch::Error,
+                )?;
 
                 let updated_rows: usize = batches.iter().map(|batch| batch.num_rows()).sum();
                 if Some(updated_rows) == metadata.physical_rows {
@@ -804,7 +815,11 @@ impl MergeInsertJob {
                 let reader = RecordBatchIterator::new(batches, write_schema.clone());
                 let stream = reader_to_stream(Box::new(reader));
 
-                let write_schema = dataset.schema().project_by_schema(write_schema.as_ref())?;
+                let write_schema = dataset.schema().project_by_schema(
+                    write_schema.as_ref(),
+                    OnMissing::Error,
+                    OnTypeMismatch::Error,
+                )?;
 
                 let fragments = write_fragments_internal(
                     Some(dataset.as_ref()),
