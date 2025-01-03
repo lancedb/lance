45fde4c0bc15482d15d243e813034fa79f09d4c7
vinoyang <yanghua1127@gmail.com>
2025-01-04 07:42:12 +0800

    ci(java/scala): auto check and insert unified license header (#3296)

--- java/core/src/test/java/com/lancedb/lance/TestVectorDataset.java
+++ java/core/src/test/java/com/lancedb/lance/TestVectorDataset.java
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.lancedb.lance;
 
 import com.lancedb.lance.index.DistanceType;
