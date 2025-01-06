a097f610a5ecff3e39682b17ad421ef0c4e31dc9
yanghua <yanghua1127@gmail.com>
2025-01-06 14:07:07 +0800

    temp commit

--- rust/lance/src/catalog.rs
+++ rust/lance/src/catalog.rs
@@ -2,9 +2,5 @@
 // SPDX-FileCopyrightText: Copyright The Lance Authors
 
 pub(crate) mod catalog_trait;
-pub(crate) mod database;
-pub(crate) mod dataset_identifier;
 
 pub use catalog_trait::Catalog;
-pub use database::Database;
-pub use dataset_identifier::DatasetIdentifier;
--- rust/lance/src/catalog/catalog_trait.rs
+++ rust/lance/src/catalog/catalog_trait.rs
@@ -1,95 +1,131 @@
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: Copyright The Lance Authors
+// Copyright 2024 Lance Developers.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-use crate::catalog::database::Database;
-use crate::catalog::dataset_identifier::DatasetIdentifier;
-use crate::dataset::Dataset;
-use std::collections::{HashMap, HashSet};
+use std::sync::Arc;
 
-pub trait Catalog {
-    /// Initialize the catalog.
-    fn initialize(&self, name: &str, properties: &HashMap<&str, &str>) -> Result<(), String>;
+use lance_io::object_store::{ObjectStore, ObjectStoreParams, WrappingObjectStore};
+use lance_table::io::commit::CommitHandler;
 
-    /// List all datasets under a specified database.
-    fn list_datasets(&self, database: &Database) -> Vec<DatasetIdentifier>;
+use crate::{Result};
 
-    /// Create a new dataset in the catalog.
-    fn create_dataset(
+/// Contains all the information that Lance needs to access a table
+pub struct TableReference {
+    /// Base URI of the table
+    pub uri: String,
+    /// Object store wrapper used to access the table
+    pub store_wrapper: Option<Arc<dyn WrappingObjectStore>>,
+    /// Commit handler used to handle new commits to the table
+    pub commit_handler: Arc<dyn CommitHandler>,
+    /// Parameters used to read / write to the object store
+    pub store_params: Option<ObjectStoreParams>,
+}
+
+/// Trait to be implemented by any catalog that Lance can use
+#[async_trait::async_trait]
+pub trait Catalog: std::fmt::Debug + Send + Sync {
+    /// Create a new table in the catalog
+    ///
+    /// Returns a table reference that can be used to read/write to the table
+    async fn create_table(&self, name: &str) -> Result<TableReference>;
+    /// Drop a table from the catalog
+    async fn drop_table(&self, name: &str) -> Result<()>;
+    /// Get a reference to a table in the catalog
+    ///
+    /// Returns a table reference that can be used to read/write to the table
+    async fn get_table(&self, name: &str) -> Result<TableReference>;
+    /// List all tables in the catalog
+    ///
+    /// The `start_after` parameter is an optional table name that, if provided, will
+    /// start the listing after the named table. If the named table is not found, the
+    /// listing will start after the table that would be named if it existed.
+    ///
+    /// The `limit` parameter is an optional limit on the number of tables returned.
+    async fn list_tables(
         &self,
-        identifier: &DatasetIdentifier,
-        location: &str,
-    ) -> Result<Dataset, String>;
+        start_after: Option<String>,
+        limit: Option<u32>,
+    ) -> Result<Vec<String>>;
+}
 
-    /// Check if a dataset exists in the catalog.
-    fn dataset_exists(&self, identifier: &DatasetIdentifier) -> bool;
+/// A simplified catalog that puts all tables in a single base directory
+///
+/// The object store's CAS primitives are used for commit handling.
+///
+/// This object store is simplistic but has zero dependencies (beyond an object store
+/// of some kind)
+#[derive(Debug)]
+pub struct ListingCatalog {
+    base_path: object_store::path::Path,
+    object_store: ObjectStore,
+}
 
-    /// Drop a dataset from the catalog.
-    fn drop_dataset(&self, identifier: &DatasetIdentifier) -> Result<(), String>;
+const LANCE_EXTENSION: &str = "lance";
 
-    /// Drop a dataset from the catalog and purge the metadata.
-    fn drop_dataset_with_purge(
-        &self,
-        identifier: &DatasetIdentifier,
-        purge: &bool,
-    ) -> Result<(), String>;
+impl ListingCatalog {
+    pub fn new(base_path: object_store::path::Path, object_store: ObjectStore) -> Self {
+        Self {
+            base_path,
+            object_store,
+        }
+    }
+}
 
-    /// Rename a dataset in the catalog.
-    fn rename_dataset(
-        &self,
-        from: &DatasetIdentifier,
-        to: &DatasetIdentifier,
-    ) -> Result<(), String>;
-
-    /// Load a dataset from the catalog.
-    fn load_dataset(&self, name: &DatasetIdentifier) -> Result<Dataset, String>;
-
-    /// Invalidate cached table metadata from current catalog.
-    fn invalidate_dataset(&self, identifier: &DatasetIdentifier) -> Result<(), String>;
-
-    /// Register a dataset in the catalog.
-    fn register_dataset(&self, identifier: &DatasetIdentifier) -> Result<Dataset, String>;
-
-    /// Create a database in the catalog.
-    fn create_database(
-        &self,
-        database: &Database,
-        metadata: HashMap<String, String>,
-    ) -> Result<(), String>;
-
-    /// List top-level databases from the catalog.
-    fn list_databases(&self) -> Vec<Database> {
-        self.list_child_databases(&Database::empty())
-            .unwrap_or_default()
+#[async_trait::async_trait]
+impl Catalog for ListingCatalog {
+    async fn create_table(&self, name: &str) -> Result<TableReference> {
+        todo!()
     }
 
-    /// List child databases from the database.
-    fn list_child_databases(&self, database: &Database) -> Result<Vec<Database>, String>;
+    async fn drop_table(&self, name: &str) -> Result<()> {
+        todo!()
+    }
 
-    /// Load metadata properties for a database.
-    fn load_database_metadata(
+    async fn get_table(&self, name: &str) -> Result<TableReference> {
+        todo!()
+    }
+
+    async fn list_tables(
         &self,
-        database: &Database,
-    ) -> Result<HashMap<String, String>, String>;
-
-    /// Drop a database.
-    fn drop_database(&self, database: &Database) -> Result<bool, String>;
-
-    /// Set a collection of properties on a database in the catalog.
-    fn set_properties(
-        &self,
-        database: &Database,
-        properties: HashMap<String, String>,
-    ) -> Result<bool, String>;
-
-    /// Remove a set of property keys from a database in the catalog.
-    fn remove_properties(
-        &self,
-        database: &Database,
-        properties: HashSet<String>,
-    ) -> Result<bool, String>;
-
-    /// Checks whether the database exists.
-    fn database_exists(&self, database: &Database) -> bool {
-        self.load_database_metadata(database).is_ok()
+        start_after: Option<String>,
+        limit: Option<u32>,
+    ) -> Result<Vec<String>> {
+        let mut f = self
+            .object_store
+            .read_dir(self.base_path.clone())
+            .await?
+            .iter()
+            .map(std::path::Path::new)
+            .filter(|path| {
+                let is_lance = path
+                    .extension()
+                    .and_then(|e| e.to_str())
+                    .map(|e| e == LANCE_EXTENSION);
+                is_lance.unwrap_or(false)
+            })
+            .filter_map(|p| p.file_stem().and_then(|s| s.to_str().map(String::from)))
+            .collect::<Vec<String>>();
+        f.sort();
+        if let Some(start_after) = start_after {
+            let index = f
+                .iter()
+                .position(|name| name.as_str() > start_after.as_str())
+                .unwrap_or(f.len());
+            f.drain(0..index);
+        }
+        if let Some(limit) = limit {
+            f.truncate(limit as usize);
+        }
+        Ok(f)
     }
 }
