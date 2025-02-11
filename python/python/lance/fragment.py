d0472243aaf5854d5da66539ee9776f5ac103d03
Weston Pace <weston.pace@gmail.com>
2025-02-11 11:30:32 -0800

    Fix python lint

--- python/python/lance/fragment.py
+++ python/python/lance/fragment.py
@@ -685,8 +685,7 @@ if TYPE_CHECKING:
         use_legacy_format: Optional[bool] = None,
         storage_options: Optional[Dict[str, str]] = None,
         enable_move_stable_row_ids: bool = False,
-    ) -> Transaction:
-        ...
+    ) -> Transaction: ...
 
     @overload
     def write_fragments(
@@ -704,8 +703,7 @@ if TYPE_CHECKING:
         use_legacy_format: Optional[bool] = None,
         storage_options: Optional[Dict[str, str]] = None,
         enable_move_stable_row_ids: bool = False,
-    ) -> List[FragmentMetadata]:
-        ...
+    ) -> List[FragmentMetadata]: ...
 
 
 def write_fragments(
