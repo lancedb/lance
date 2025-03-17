6014681f2a54b3e0775609fe7e4858de4dc66607
huangzhaowei <huangzhaowei.416@bytedance.com>
2025-03-17 19:56:39 +0800

    fix format

--- python/python/tests/test_ordering.py
+++ python/python/tests/test_ordering.py
@@ -29,20 +29,8 @@ def test_one_column_order_by(tmp_path: Path):
     ordering = ColumnOrdering.asc_nulls_first("int_col")
     true_value = pa.table(
         {
-            "int_col": [
-                None,
-                1,
-                2,
-                3,
-                4,
-            ],
-            "str_col": [
-                "b",
-                None,
-                "a",
-                "d",
-                "c",
-            ],
+            "int_col": [None, 1, 2, 3, 4],
+            "str_col": ["b", None, "a", "d", "c"],
         }
     )
     assert dataset.scanner(orderings=[ordering]).to_table() == true_value
@@ -51,20 +39,8 @@ def test_one_column_order_by(tmp_path: Path):
     ordering = ColumnOrdering.desc_nulls_last("int_col")
     true_value = pa.table(
         {
-            "int_col": [
-                4,
-                3,
-                2,
-                1,
-                None,
-            ],
-            "str_col": [
-                "c",
-                "d",
-                "a",
-                None,
-                "b",
-            ],
+            "int_col": [4, 3, 2, 1, None],
+            "str_col": ["c", "d", "a", None, "b"],
         }
     )
     assert dataset.scanner(orderings=[ordering]).to_table() == true_value
@@ -73,20 +49,8 @@ def test_one_column_order_by(tmp_path: Path):
     ordering = ColumnOrdering.desc_nulls_first("int_col")
     true_value = pa.table(
         {
-            "int_col": [
-                None,
-                4,
-                3,
-                2,
-                1,
-            ],
-            "str_col": [
-                "b",
-                "c",
-                "d",
-                "a",
-                None,
-            ],
+            "int_col": [None, 4, 3, 2, 1],
+            "str_col": ["b", "c", "d", "a", None],
         }
     )
     assert dataset.scanner(orderings=[ordering]).to_table() == true_value
@@ -95,16 +59,24 @@ def test_one_column_order_by(tmp_path: Path):
 def test_two_column_order_by(tmp_path: Path):
     base_dir = tmp_path / "dataset"
     data = pa.table(
-        {"int_col": [2, 1, 3, None, 4], "str_col": ["a", None, "d", "b", "c"]}
+        {
+            "int_col": [2, 1, 3, 3, None, 4, 3],
+            "str_col": ["a", None, "d", None, "b", "c", "e"],
+        }
     )
     dataset = lance.write_dataset(data, base_dir)
 
-    ## asc null_last
-    ordering = ColumnOrdering.asc_nulls_last("int_col")
+    ## int column asc null_last and str col desc null
+    ordering1 = ColumnOrdering.asc_nulls_last("int_col")
+    ordering2 = ColumnOrdering.desc_nulls_first("str_col")
+
     true_value = pa.table(
-        {"int_col": [1, 2, 3, 4, None], "str_col": [None, "a", "d", "c", "b"]}
+        {
+            "int_col": [1, 2, 3, 3, 3, 4, None],
+            "str_col": [None, "a", None, "e", "d", "c", "b"],
+        }
     )
-    assert dataset.scanner(orderings=[ordering]).to_table() == true_value
+    assert dataset.scanner(orderings=[ordering1, ordering2]).to_table() == true_value
 
 
 def test_all_order_by_support_functions(tmp_path: Path):
