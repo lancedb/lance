351c4afd70183ed8717f123aae6b5e0a69619168
Will Jones <willjones127@gmail.com>
2025-04-17 15:34:54 -0700

    finish spill

--- rust/lance-datafusion/src/spill.rs
+++ rust/lance-datafusion/src/spill.rs
@@ -2,7 +2,6 @@
 // SPDX-FileCopyrightText: Copyright The Lance Authors
 
 use std::{
-    io::Error,
     path::PathBuf,
     sync::{Arc, Mutex},
 };
@@ -10,8 +9,11 @@ use std::{
 use arrow::ipc::{reader::StreamReader, writer::StreamWriter};
 use arrow_array::RecordBatch;
 use arrow_schema::{ArrowError, Schema};
-use datafusion::execution::SendableRecordBatchStream;
+use datafusion::{
+    execution::SendableRecordBatchStream, physical_plan::stream::RecordBatchStreamAdapter,
+};
 use datafusion_common::DataFusionError;
+use futures::TryStreamExt;
 
 /// A spill of Arrow data to a temporary file. The file is an Arrow IPC stream
 /// file.
@@ -50,7 +52,7 @@ enum SpillState {
     },
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct WriteStatus {
     finished: bool,
     batches_written: usize,
@@ -58,21 +60,16 @@ struct WriteStatus {
 
 impl From<&SpillState> for WriteStatus {
     fn from(state: &SpillState) -> Self {
-        match state {
-            SpillState::Uninitialized => WriteStatus {
-                finished: false,
-                batches_written: 0,
-            },
+        let (finished, batches_written) = match state {
+            SpillState::Uninitialized => (false, 0),
             SpillState::Initialized {
                 batches_written, ..
-            } => WriteStatus {
-                finished: false,
-                batches_written: *batches_written,
-            },
-            SpillState::Finished { batches_written } => WriteStatus {
-                finished: true,
-                batches_written: *batches_written,
-            },
+            } => (false, *batches_written),
+            SpillState::Finished { batches_written } => (true, *batches_written),
+        };
+        WriteStatus {
+            finished,
+            batches_written,
         }
     }
 }
@@ -83,10 +80,11 @@ impl Spill {
     /// create the Arrow IPC stream file.
     pub fn new(tmp_dir: tempfile::TempDir, schema: Arc<Schema>) -> Self {
         let path = tmp_dir.path().join("spill.arrow");
-        let (status_sender, status_receiver) = tokio::sync::watch::channel(WriteStatus {
+        let initial_status = WriteStatus {
             finished: false,
             batches_written: 0,
-        });
+        };
+        let (status_sender, status_receiver) = tokio::sync::watch::channel(initial_status);
         Self {
             tmp_dir,
             schema,
@@ -100,33 +98,41 @@ impl Spill {
     /// Write a batch to the spill. The batch is immediately flushed to the
     /// IPC stream file.
     pub async fn write(&mut self, batch: RecordBatch) -> Result<(), DataFusionError> {
-        match &mut self.state {
+        if let SpillState::Finished { .. } = self.state {
+            return Err(DataFusionError::Execution(
+                "Spill has already been finished".to_string(),
+            ));
+        }
+
+        let (writer, batches_written) = match &mut self.state {
             SpillState::Uninitialized => {
                 let writer =
                     AsyncStreamWriter::open(self.path.clone(), self.schema.clone()).await?;
-                writer.write(batch).await?;
                 self.state = SpillState::Initialized {
                     writer,
-                    batches_written: 1,
+                    batches_written: 0,
                 };
-                self.status_sender
-                    .send_replace(WriteStatus::from(&self.state));
+                if let SpillState::Initialized {
+                    writer,
+                    batches_written,
+                } = &mut self.state
+                {
+                    (writer, batches_written)
+                } else {
+                    unreachable!()
+                }
             }
             SpillState::Initialized {
                 writer,
                 batches_written,
-            } => {
-                writer.write(batch).await?;
-                *batches_written += 1;
-                self.status_sender
-                    .send_replace(WriteStatus::from(&self.state));
-            }
-            SpillState::Finished { .. } => {
-                return Err(DataFusionError::Execution(
-                    "Spill has already been finished".to_string(),
-                ));
-            }
-        }
+            } => (writer, batches_written),
+            _ => unreachable!(),
+        };
+
+        writer.write(batch).await?;
+        *batches_written += 1;
+        self.status_sender
+            .send_replace(WriteStatus::from(&self.state));
 
         Ok(())
     }
@@ -164,21 +170,80 @@ impl Spill {
         Ok(())
     }
 
+    /// Returns a stream of batches from the spill. The stream will emit
+    /// batches as they are written to the spill. If the spill has already
+    /// been finished, the stream will emit all batches in the spill.
+    ///
+    /// The stream will not complete until [`Self::finish()`] is called.
+    ///
+    /// If the spill has been dropped, an error will be returned.
     pub fn read(&self) -> Result<SendableRecordBatchStream, DataFusionError> {
-        todo!()
-        // Take a copy of the write path, status reciever.
-        // In thee stream, watch the status reciever to show > 0 batches written
-        // before opening the file.
-        // After that, open the file and create a stream reader.
-        // Wait until the status reciever says a batch has been written, before
-        // reading the next batch.
-        // When the status reciever says the spill has been finished, read the
-        // remaining batches in the file and then finish the stream.
+        let mut rx = self.status_receiver.clone();
+        let batches_read = 0;
+        let path = self.path.clone();
+
+        let stream = futures::stream::once(async move {
+            if !rx.borrow().finished && rx.borrow().batches_written == 0 {
+                // Wait for data to be written
+                rx.wait_for(|status| status.finished || status.batches_written > 0)
+                    .await
+                    .map_err(|_| {
+                        DataFusionError::Execution(
+                            "Spill has been dropped before reader has finish.".into(),
+                        )
+                    })?;
+            }
+
+            let reader = AsyncStreamReader::open(path).await?;
+
+            Ok::<_, DataFusionError>(futures::stream::try_unfold(
+                (rx, batches_read, reader),
+                move |(mut rx, mut batches_read, reader)| async move {
+                    if !rx.borrow().finished && batches_read >= rx.borrow().batches_written {
+                        // Wait for more data to be available
+                        println!("waiting for more data");
+                        rx.wait_for(|status| {
+                            status.finished || status.batches_written > batches_read
+                        })
+                        .await
+                        .map_err(|_| {
+                            DataFusionError::Execution(
+                                "Spill has been dropped before reader has finish.".into(),
+                            )
+                        })?;
+                    } else if rx.borrow().finished && batches_read >= rx.borrow().batches_written {
+                        return Ok(None);
+                    }
+
+                    let batch = reader.read().await?.ok_or_else(|| {
+                        DataFusionError::Execution(
+                            "Got fewer than expected batches from spill".into(),
+                        )
+                    })?;
+
+                    batches_read += 1;
+
+                    Ok(Some((batch, (rx, batches_read, reader))))
+                },
+            ))
+        })
+        .try_flatten();
+
+        Ok(Box::pin(RecordBatchStreamAdapter::new(
+            self.schema.clone(),
+            stream,
+        )))
     }
 
     pub async fn shutdown(self) -> Result<(), DataFusionError> {
-        self.tmp_dir.close()?;
-        Ok(())
+        let res =
+            tokio::task::spawn_blocking(move || self.tmp_dir.close().map_err(ArrowError::from))
+                .await;
+        match res {
+            Ok(Ok(())) => Ok(()),
+            Ok(Err(e)) => Err(DataFusionError::Execution(e.to_string())),
+            Err(e) => Err(DataFusionError::Execution(e.to_string())),
+        }
     }
 }
 
@@ -204,7 +269,8 @@ impl AsyncStreamWriter {
         let writer = self.writer.clone();
         tokio::task::spawn_blocking(move || {
             let mut writer = writer.lock().unwrap();
-            writer.write(&batch)
+            writer.write(&batch)?;
+            writer.flush()
         })
         .await
         .unwrap()
@@ -251,24 +317,87 @@ impl AsyncStreamReader {
 
 #[cfg(test)]
 mod tests {
+    use arrow_array::Int32Array;
+    use arrow_schema::{DataType, Field};
+    use futures::{poll, StreamExt, TryStreamExt};
+
     use super::*;
 
     #[tokio::test]
     async fn test_spill() {
+        let schema = Arc::new(Schema::new(vec![Field::new("a", DataType::Int32, false)]));
+        let batches = [
+            RecordBatch::try_new(
+                schema.clone(),
+                vec![Arc::new(Int32Array::from(vec![1, 2, 3]))],
+            )
+            .unwrap(),
+            RecordBatch::try_new(
+                schema.clone(),
+                vec![Arc::new(Int32Array::from(vec![4, 5, 6]))],
+            )
+            .unwrap(),
+        ];
+
         // Create a stream
+        let tmp_dir = tempfile::tempdir().unwrap();
+        let mut spill = Spill::new(tmp_dir, schema.clone());
 
-        // Open a reader before writing. Assert we can't get any data.
+        // We can open a reader prior to writing any data. No batches will be ready.
+        let mut stream_before = spill.read().unwrap();
+        let mut stream_before_next = stream_before.next();
+        let poll_res = poll!(&mut stream_before_next);
+        assert!(poll_res.is_pending());
 
-        // Write some data. Assert it shows up, but can't advanced past.
+        // If we write a batch, the existing reader can now receive it.
+        spill.write(batches[0].clone()).await.unwrap();
+        let stream_before_batch1 = stream_before_next
+            .await
+            .expect("Expected a batch")
+            .expect("Expected no error");
+        assert_eq!(&stream_before_batch1, &batches[0]);
+        let mut stream_before_next = stream_before.next();
+        let poll_res = poll!(&mut stream_before_next);
+        assert!(poll_res.is_pending());
 
-        // Open a reader now. Assert we can get the data.
+        // We can also open a ready while the spill is being written to. We can
+        // retrieve batches written so far immediately.
+        let mut stream_during = spill.read().unwrap();
+        let stream_during_batch1 = stream_during
+            .next()
+            .await
+            .expect("Expected a batch")
+            .expect("Expected no error");
+        assert_eq!(&stream_during_batch1, &batches[0]);
+        let mut stream_during_next = stream_during.next();
+        let poll_res = poll!(&mut stream_during_next);
+        assert!(poll_res.is_pending());
 
-        // Finish the spill. Assert we can get all the data from two existing readers.
+        // Once we finish the spill, readers can get remaining batches and will
+        // reach the end of the stream.
+        spill.write(batches[1].clone()).await.unwrap();
+        spill.finish().await.unwrap();
 
-        // Create a new reader. Assert we can get all the data from it.
+        let stream_before_batch2 = stream_before_next
+            .await
+            .expect("Expected a batch")
+            .expect("Expected no error");
+        assert_eq!(&stream_before_batch2, &batches[1]);
+        assert!(stream_before.next().await.is_none());
 
-        // Close the spill. Assert new readers can't get any data.
+        let stream_during_batch2 = stream_during_next
+            .await
+            .expect("Expected a batch")
+            .expect("Expected no error");
+        assert_eq!(&stream_during_batch2, &batches[1]);
+        assert!(stream_during.next().await.is_none());
 
-        // Assert the file is actually deleted.
+        // Can also start a reader after finishing.
+        let stream_after = spill.read().unwrap();
+        let stream_after_batches = stream_after.try_collect::<Vec<_>>().await.unwrap();
+        assert_eq!(&stream_after_batches, &batches);
+
+        // Once we close the spill, the file is deleted.
+        spill.shutdown().await.unwrap();
     }
 }
