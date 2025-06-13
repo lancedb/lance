b434ce9fd8ebab776db037294ab02644b9adbd86
Weston Pace <weston.pace@gmail.com>
2025-06-12 20:43:28 -0700

    fix: don't panic on DF table provider if test has filter on indexed column (#3954)
    We need to ensure all of our exec nodes do not spawn tasks in the
    execute fn until https://github.com/apache/datafusion/issues/16312 is
    addressed. The `AddRowAddrExec` was violating this and this PR fixes
    that.

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -385,6 +385,8 @@ impl FFILanceTableProvider {
         let py = dataset.py();
         let dataset = dataset.getattr("_ds")?.extract::<Py<Dataset>>()?;
         let dataset_ref = &dataset.bind(py).borrow().ds;
+        // TODO: https://github.com/lancedb/lance/issues/3966 remove this workaround
+        let _ = RT.block_on(Some(py), dataset_ref.load_indices())?;
         Ok(Self {
             dataset: dataset_ref.clone(),
             with_row_id,
