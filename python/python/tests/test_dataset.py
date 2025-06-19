15c00f8948b6ad817a976e6da6c7312516de92db
yanghua <yanghua1127@gmail.com>
2025-06-19 20:13:17 +0800

    feat: expose get_config api

--- python/python/tests/test_dataset.py
+++ python/python/tests/test_dataset.py
@@ -1142,6 +1142,15 @@ def test_config_update_auto_cleanup(tmp_path):
     assert len(dataset.versions()) == 2
 
 
+def test_get_configs(tmp_path):
+    table = pa.Table.from_pydict({"a": range(100), "b": range(100)})
+    base_dir = tmp_path / "test"
+    ds = lance.write_dataset(table, base_dir, mode="create")
+    ds.update_config({"test_key": "test_value"})
+    config_value = ds.get_configs(["test_key"])
+    assert config_value["test_key"] == "test_value"
+
+
 def test_auto_cleanup_invalid(tmp_path):
     table = pa.Table.from_pydict({"a": range(100), "b": range(100)})
     base_dir = tmp_path / "test"
