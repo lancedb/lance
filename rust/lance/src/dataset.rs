842c19c0ac36a8d131102fb2d40f4cb91ed7958e
yanghua <yanghua1127@gmail.com>
2025-06-25 21:54:34 +0800

    feat: support sql api for dataset

--- rust/lance/src/dataset.rs
+++ rust/lance/src/dataset.rs
@@ -6619,7 +6619,12 @@ mod tests {
 
         // verify _rowid and _rowaddr
         let results = ds
-            .sql("SELECT x, y, _rowid, _rowaddr FROM foo where y > 100", "foo", true, true)
+            .sql(
+                "SELECT x, y, _rowid, _rowaddr FROM foo where y > 100",
+                "foo",
+                true,
+                true,
+            )
             .await
             .unwrap();
         assert_eq!(results.len(), 1);
