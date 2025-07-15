ef4cbf160a95da1f269a1a27db86c24989aad296
yanghua <yanghua1127@gmail.com>
2025-07-15 09:07:51 +0800

    refactor code

--- python/python/tests/test_tracing.py
+++ python/python/tests/test_tracing.py
@@ -75,15 +75,15 @@ capture_trace_events(callback)
 lance.write_dataset(pa.table({"x": range(100)}), "memory://test")
 assert len(events) == 4
 
-assert events[0].target == "lance::file_audit"
-assert events[0].args["mode"] == "create"
-assert events[0].args["type"] == "data"
+assert events[0].target == "lance::dataset_events"
+assert events[0].args["event"] == "writing"
+assert events[0].args["path"] == "test"
+assert events[0].args["mode"] == "Create"
 assert events[0].args["timestamp"] is not None
 
-assert events[1].target == "lance::dataset_events"
-assert events[1].args["event"] == "writing"
-assert events[1].args["path"] == "test"
-assert events[1].args["mode"] == "Create"
+assert events[1].target == "lance::file_audit"
+assert events[1].args["mode"] == "create"
+assert events[1].args["type"] == "data"
 assert events[1].args["timestamp"] is not None
 
 assert events[2].target == "lance::file_audit"
