fdae6ed9c35d3e432c4fafdb1dc715b4314c1976
Hubert Yuan <hubert@exa.ai>
2025-08-05 14:21:15 -0700

    test strict overwrite success paths

--- python/python/tests/test_dataset.py
+++ python/python/tests/test_dataset.py
@@ -1231,11 +1231,11 @@ def test_create_from_commit(tmp_path: Path):
 def test_strict_overwrite(tmp_path: Path):
     table = pa.Table.from_pydict({"a": range(100), "b": range(100)})
     base_dir = tmp_path / "test"
-    dataset_v1 = lance.write_dataset(table, base_dir)
 
     fragment = lance.fragment.LanceFragment.create(base_dir, table)
     operation = lance.LanceOperation.Overwrite(table.schema, [fragment])
-    lance.LanceDataset.commit(base_dir, operation, read_version=dataset_v1.version)
+    dataset_v1 = lance.LanceDataset.commit(base_dir, operation, max_retries=0)
+    lance.LanceDataset.commit(base_dir, operation, read_version=dataset_v1.version, max_retries=0)
     with pytest.raises(
         OSError, match=f"Commit conflict for version {dataset_v1.version + 1}"
     ):
