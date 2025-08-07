2b6bb99952b4a8079b7dab22c4cc882352e94511
Will Jones <willjones127@gmail.com>
2025-08-07 15:29:00 -0700

    fix license header

--- rust/lance/src/dataset/metadata.rs
+++ rust/lance/src/dataset/metadata.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright The Lance Authors
+
 use std::collections::HashMap;
 
 use crate::dataset::transaction::{Operation, UpdateMap, UpdateMapEntry};
