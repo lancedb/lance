d50db59cddf0d66753d88e9d0f1ebc3fab382336
yanghua <yanghua1127@gmail.com>
2025-08-14 07:40:46 +0800

    test draft

--- rust/lance/src/dataset/write/merge_insert.rs
+++ rust/lance/src/dataset/write/merge_insert.rs
@@ -2002,7 +2002,7 @@ mod tests {
     use std::collections::HashMap;
     use tempfile::tempdir;
     use tokio::sync::{Barrier, Notify};
-
+    use crate::dataset::scanner::ColumnOrdering;
     use super::*;
 
     // Used to validate that futures returned are Send.
@@ -2495,6 +2495,441 @@ mod tests {
             assert_eq!(row_ids_before, row_ids_after);
         } else {
             assert_ne!(row_ids_before, row_ids_after);
+        }
+    }
+
+    #[rstest::rstest]
+    #[tokio::test]
+    async fn test_upsert_only_with_stable_row_id(
+        #[values(LanceFileVersion::Legacy, LanceFileVersion::V2_0)] version: LanceFileVersion,
+        #[values(true, false)] enable_move_stable_row_ids: bool,
+    ) {
+        let schema = Arc::new(Schema::new(vec![
+            Field::new("key", DataType::UInt32, false),
+            Field::new("value", DataType::UInt32, false),
+            Field::new("filterme", DataType::Utf8, false),
+        ]));
+
+        let batch = RecordBatch::try_new(
+            schema.clone(),
+            vec![
+                Arc::new(UInt32Array::from(vec![1, 2, 3, 4, 5, 6])),
+                Arc::new(UInt32Array::from(vec![1, 1, 1, 1, 1, 1])),
+                Arc::new(StringArray::from(vec!["A", "B", "A", "A", "B", "A"])),
+            ],
+        )
+            .unwrap();
+
+        let test_dir = tempdir().unwrap();
+        let test_uri = test_dir.path().to_str().unwrap();
+
+        let write_params = WriteParams {
+            max_rows_per_file: 10,
+            enable_move_stable_row_ids,
+            data_storage_version: Some(version),
+            ..Default::default()
+        };
+
+        let batches = RecordBatchIterator::new([Ok(batch)], schema.clone());
+        let ds = Arc::new(
+            Dataset::write(batches, test_uri, Some(write_params))
+                .await
+                .unwrap(),
+        );
+
+        let new_batch = RecordBatch::try_new(
+            schema.clone(),
+            vec![
+                Arc::new(UInt32Array::from(vec![4, 5, 6, 7, 8, 9])),
+                Arc::new(UInt32Array::from(vec![2, 2, 2, 2, 2, 2])),
+                Arc::new(StringArray::from(vec!["A", "B", "C", "A", "B", "C"])),
+            ],
+        )
+            .unwrap();
+
+        let keys = vec!["key".to_string()];
+
+        let updating_batch = ds
+            .scan()
+            .filter("key IN (4,5,6)")
+            .unwrap()
+            .with_row_id()
+            .try_into_batch()
+            .await
+            .unwrap();
+        let row_ids_before = updating_batch
+            .column_by_name(ROW_ID)
+            .unwrap()
+            .as_any()
+            .downcast_ref::<UInt64Array>()
+            .unwrap();
+
+        // upsert, no delete
+        let job = MergeInsertBuilder::try_new(ds.clone(), keys.clone())
+            .unwrap()
+            .when_matched(WhenMatched::UpdateAll)
+            .try_build()
+            .unwrap();
+        check(
+            new_batch.clone(),
+            job,
+            &[1, 2, 3],
+            &[4, 5, 6, 7, 8, 9],
+            &[3, 3, 0],
+        )
+            .await;
+
+        let ds = Dataset::open(test_uri).await.unwrap();
+        let updated_batch = ds
+            .scan()
+            .filter("key IN (4,5,6)")
+            .unwrap()
+            .with_row_id()
+            .try_into_batch()
+            .await
+            .unwrap();
+        let row_ids_after = updated_batch
+            .column_by_name(ROW_ID)
+            .unwrap()
+            .as_any()
+            .downcast_ref::<UInt64Array>()
+            .unwrap();
+
+        if enable_move_stable_row_ids {
+            assert_eq!(row_ids_before, row_ids_after);
+        } else {
+            assert_ne!(row_ids_before, row_ids_after);
+        }
+    }
+
+    #[rstest::rstest]
+    #[tokio::test]
+    async fn test_conditional_update_with_stable_row_id(
+        #[values(LanceFileVersion::Legacy, LanceFileVersion::V2_0)] version: LanceFileVersion,
+        #[values(true, false)] enable_move_stable_row_ids: bool,
+    ) {
+        let schema = Arc::new(Schema::new(vec![
+            Field::new("key", DataType::UInt32, false),
+            Field::new("value", DataType::UInt32, false),
+            Field::new("filterme", DataType::Utf8, false),
+        ]));
+
+        let batch = RecordBatch::try_new(
+            schema.clone(),
+            vec![
+                Arc::new(UInt32Array::from(vec![1, 2, 3, 4, 5, 6])),
+                Arc::new(UInt32Array::from(vec![1, 1, 1, 1, 1, 1])),
+                Arc::new(StringArray::from(vec!["A", "B", "A", "A", "B", "A"])),
+            ],
+        )
+            .unwrap();
+
+        let test_dir = tempdir().unwrap();
+        let test_uri = test_dir.path().to_str().unwrap();
+
+        let write_params = WriteParams {
+            max_rows_per_file: 10,
+            enable_move_stable_row_ids,
+            data_storage_version: Some(version),
+            ..Default::default()
+        };
+
+        let batches = RecordBatchIterator::new([Ok(batch)], schema.clone());
+        let ds = Arc::new(
+            Dataset::write(batches, test_uri, Some(write_params))
+                .await
+                .unwrap(),
+        );
+
+        let new_batch = RecordBatch::try_new(
+            schema.clone(),
+            vec![
+                Arc::new(UInt32Array::from(vec![4, 5, 6, 7, 8, 9])),
+                Arc::new(UInt32Array::from(vec![2, 2, 2, 2, 2, 2])),
+                Arc::new(StringArray::from(vec!["A", "B", "C", "A", "B", "C"])),
+            ],
+        )
+            .unwrap();
+
+        let keys = vec!["key".to_string()];
+
+        let updating_batch = ds
+            .scan()
+            .filter("key = 6")
+            .unwrap()
+            .with_row_id()
+            .try_into_batch()
+            .await
+            .unwrap();
+        let row_ids_before = updating_batch
+            .column_by_name(ROW_ID)
+            .unwrap()
+            .as_any()
+            .downcast_ref::<UInt64Array>()
+            .unwrap();
+
+        // conditional update, no insert and delete
+        let job = MergeInsertBuilder::try_new(ds.clone(), keys.clone())
+            .unwrap()
+            .when_matched(
+                WhenMatched::update_if(&ds, "source.filterme != target.filterme").unwrap(),
+            )
+            .try_build()
+            .unwrap();
+        check(
+            new_batch.clone(),
+            job,
+            &[1, 2, 3, 4, 5],
+            &[6, 7, 8, 9],
+            &[3, 1, 0],
+        )
+            .await;
+
+        let ds = Dataset::open(test_uri).await.unwrap();
+        let updated_batch = ds
+            .scan()
+            .filter("key = 6")
+            .unwrap()
+            .with_row_id()
+            .try_into_batch()
+            .await
+            .unwrap();
+        let row_ids_after = updated_batch
+            .column_by_name(ROW_ID)
+            .unwrap()
+            .as_any()
+            .downcast_ref::<UInt64Array>()
+            .unwrap();
+
+        if enable_move_stable_row_ids {
+            assert_eq!(row_ids_before, row_ids_after);
+        } else {
+            assert_ne!(row_ids_before, row_ids_after);
+        }
+    }
+
+    #[rstest::rstest]
+    #[tokio::test]
+    async fn test_update_only_with_stable_row_id(
+        #[values(LanceFileVersion::Legacy, LanceFileVersion::V2_0)] version: LanceFileVersion,
+        #[values(true, false)] enable_move_stable_row_ids: bool,
+    ) {
+        let schema = Arc::new(Schema::new(vec![
+            Field::new("key", DataType::UInt32, false),
+            Field::new("value", DataType::UInt32, false),
+            Field::new("filterme", DataType::Utf8, false),
+        ]));
+
+        let batch = RecordBatch::try_new(
+            schema.clone(),
+            vec![
+                Arc::new(UInt32Array::from(vec![1, 2, 3, 4, 5, 6])),
+                Arc::new(UInt32Array::from(vec![1, 1, 1, 1, 1, 1])),
+                Arc::new(StringArray::from(vec!["A", "B", "A", "A", "B", "A"])),
+            ],
+        )
+            .unwrap();
+
+        let test_dir = tempdir().unwrap();
+        let test_uri = test_dir.path().to_str().unwrap();
+
+        let write_params = WriteParams {
+            max_rows_per_file: 10,
+            enable_move_stable_row_ids,
+            data_storage_version: Some(version),
+            ..Default::default()
+        };
+
+        let batches = RecordBatchIterator::new([Ok(batch)], schema.clone());
+        let ds = Arc::new(
+            Dataset::write(batches, test_uri, Some(write_params))
+                .await
+                .unwrap(),
+        );
+
+        let new_batch = RecordBatch::try_new(
+            schema.clone(),
+            vec![
+                Arc::new(UInt32Array::from(vec![4, 5, 6, 7, 8, 9])),
+                Arc::new(UInt32Array::from(vec![2, 2, 2, 2, 2, 2])),
+                Arc::new(StringArray::from(vec!["A", "B", "C", "A", "B", "C"])),
+            ],
+        )
+            .unwrap();
+
+        let keys = vec!["key".to_string()];
+
+        let updating_batch = ds
+            .scan()
+            .filter("key IN (4,5,6)")
+            .unwrap()
+            .with_row_id()
+            .try_into_batch()
+            .await
+            .unwrap();
+        let row_ids_before = updating_batch
+            .column_by_name(ROW_ID)
+            .unwrap()
+            .as_any()
+            .downcast_ref::<UInt64Array>()
+            .unwrap();
+
+        // only update, no insert and delete
+        let job = MergeInsertBuilder::try_new(ds.clone(), keys.clone())
+            .unwrap()
+            .when_matched(WhenMatched::UpdateAll)
+            .when_not_matched(WhenNotMatched::DoNothing)
+            .try_build()
+            .unwrap();
+        check(new_batch.clone(), job, &[1, 2, 3], &[4, 5, 6], &[0, 3, 0]).await;
+
+        let ds = Dataset::open(test_uri).await.unwrap();
+        let updated_batch = ds
+            .scan()
+            .filter("key IN (4,5,6)")
+            .unwrap()
+            .with_row_id()
+            .try_into_batch()
+            .await
+            .unwrap();
+        let row_ids_after = updated_batch
+            .column_by_name(ROW_ID)
+            .unwrap()
+            .as_any()
+            .downcast_ref::<UInt64Array>()
+            .unwrap();
+
+        if enable_move_stable_row_ids {
+            assert_eq!(row_ids_before, row_ids_after);
+        } else {
+            assert_ne!(row_ids_before, row_ids_after);
+        }
+    }
+
+    #[rstest::rstest]
+    #[tokio::test]
+    async fn test_upsert_with_conditional_delete_and_stable_row_id(
+        #[values(LanceFileVersion::Legacy, LanceFileVersion::V2_0)] version: LanceFileVersion,
+        #[values(true, false)] enable_move_stable_row_ids: bool,
+    ) {
+        let schema = Arc::new(Schema::new(vec![
+            Field::new("key", DataType::UInt32, false),
+            Field::new("value", DataType::UInt32, false),
+            Field::new("filterme", DataType::Utf8, false),
+        ]));
+
+        let batch = RecordBatch::try_new(
+            schema.clone(),
+            vec![
+                Arc::new(UInt32Array::from(vec![1, 2, 3, 4, 5, 6])),
+                Arc::new(UInt32Array::from(vec![1, 1, 1, 1, 1, 1])),
+                Arc::new(StringArray::from(vec!["A", "B", "A", "A", "B", "A"])),
+            ],
+        )
+            .unwrap();
+
+        let test_dir = tempdir().unwrap();
+        let test_uri = test_dir.path().to_str().unwrap();
+
+        let write_params = WriteParams {
+            max_rows_per_file: 10,
+            enable_move_stable_row_ids,
+            data_storage_version: Some(version),
+            ..Default::default()
+        };
+
+        let batches = RecordBatchIterator::new([Ok(batch)], schema.clone());
+        let ds = Arc::new(
+            Dataset::write(batches, test_uri, Some(write_params))
+                .await
+                .unwrap(),
+        );
+
+        let new_batch = RecordBatch::try_new(
+            schema.clone(),
+            vec![
+                Arc::new(UInt32Array::from(vec![4, 5, 6, 7, 8, 9])),
+                Arc::new(UInt32Array::from(vec![2, 2, 2, 2, 2, 2])),
+                Arc::new(StringArray::from(vec!["A", "B", "C", "A", "B", "C"])),
+            ],
+        )
+            .unwrap();
+
+        let keys = vec!["key".to_string()];
+
+        let updating_batch = ds
+            .scan()
+            .filter("key IN (1,4,5,6)")
+            .unwrap()
+            .with_row_id()
+            .order_by(Some(vec![
+                ColumnOrdering::asc_nulls_first("key".to_string()),
+            ]))
+            .unwrap()
+            .try_into_batch()
+            .await
+            .unwrap();
+        let row_ids_before = updating_batch
+            .column_by_name(ROW_ID)
+            .unwrap()
+            .as_any()
+            .downcast_ref::<UInt64Array>()
+            .unwrap();
+
+        // upsert, conditional delete the records key > 1 in not matched by source
+        let condition = Expr::gt(
+            Expr::Column(Column::new_unqualified("key")),
+            Expr::Literal(ScalarValue::UInt32(Some(1)), None),
+        );
+        let job = MergeInsertBuilder::try_new(ds.clone(), keys.clone())
+            .unwrap()
+            .when_matched(WhenMatched::UpdateAll)
+            .when_not_matched_by_source(WhenNotMatchedBySource::DeleteIf(condition))
+            .try_build()
+            .unwrap();
+        check(
+            new_batch.clone(),
+            job,
+            &[1],
+            &[4, 5, 6, 7, 8, 9],
+            &[3, 3, 2],
+        )
+            .await;
+
+        let ds = Dataset::open(test_uri).await.unwrap();
+        let updated_batch = ds
+            .scan()
+            .filter("key IN (1,4,5,6)")
+            .unwrap()
+            .with_row_id()
+            .order_by(Some(vec![
+                ColumnOrdering::asc_nulls_first("key".to_string()),
+            ]))
+            .unwrap()
+            .try_into_batch()
+            .await
+            .unwrap();
+        let row_ids_after = updated_batch
+            .column_by_name(ROW_ID)
+            .unwrap()
+            .as_any()
+            .downcast_ref::<UInt64Array>()
+            .unwrap();
+
+        if enable_move_stable_row_ids {
+            // key=1应该保持原row id，key=4,5,6更新后也应该保持原row id
+            assert_eq!(row_ids_before, row_ids_after);
+        } else {
+            // 非stable模式下，被更新的行的row ids会发生变化
+            // key=1可能保持不变，key=4,5,6会变化
+            let key_1_before = row_ids_before.value(0);
+            let key_1_after = row_ids_after.value(0);
+            assert_eq!(key_1_before, key_1_after); // key=1未更新，row id应保持不变
+
+            // key=4,5,6的row ids在非stable模式下应该变化
+            for i in 1..4 {
+                assert_ne!(row_ids_before.value(i), row_ids_after.value(i));
+            }
         }
     }
 
