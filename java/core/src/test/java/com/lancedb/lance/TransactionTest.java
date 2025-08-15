4d9fef74d27df6be4e64a6bd9854a6aac1a46d98
majin1102 <majin1102@gmail.com>
2025-08-15 21:56:36 +0800

    mvn spotless

--- java/core/src/test/java/com/lancedb/lance/TransactionTest.java
+++ java/core/src/test/java/com/lancedb/lance/TransactionTest.java
@@ -1,13 +1,28 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.lancedb.lance;
 
 import com.lancedb.lance.operation.Append;
+
+import org.apache.arrow.memory.RootAllocator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import org.apache.arrow.memory.RootAllocator;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -18,7 +33,8 @@ public class TransactionTest {
   public void testTransaction(@TempDir Path tempDir) {
     String datasetPath = tempDir.resolve("testTransaction").toString();
     try (RootAllocator allocator = new RootAllocator(Long.MAX_VALUE)) {
-      TestUtils.SimpleTestDataset testDataset = new TestUtils.SimpleTestDataset(allocator, datasetPath);
+      TestUtils.SimpleTestDataset testDataset =
+          new TestUtils.SimpleTestDataset(allocator, datasetPath);
       try (Dataset dataset = testDataset.createEmptyDataset()) {
         FragmentMetadata fragmentMeta = testDataset.createNewFragment(20);
 
@@ -28,7 +44,8 @@ public class TransactionTest {
         Transaction appendTxn =
             dataset
                 .newTransactionBuilder()
-                .operation(Append.builder().fragments(Collections.singletonList(fragmentMeta)).build())
+                .operation(
+                    Append.builder().fragments(Collections.singletonList(fragmentMeta)).build())
                 .transactionProperties(properties)
                 .build();
         try (Dataset committedDataset = appendTxn.commit()) {
