74eefeca505bf9dc906612b1fe1bc9755fec49d4
Colin Patrick McCabe <colin@cmccabe.xyz>
2025-08-22 17:19:38 -0700

    refactor: remove special case in can_cast_types (#4549)
    Remove the special case for FixedSizeList in can_cast_types, now that
    Apache Arrow issue 5360 has been completed upstream.

--- rust/lance-arrow/src/cast.rs
+++ rust/lance-arrow/src/cast.rs
@@ -7,20 +7,6 @@ use arrow_array::{Array, ArrayRef, FixedSizeListArray};
 use arrow_cast::CastOptions;
 use arrow_schema::{ArrowError, DataType};
 
-/// Customized [`arrow_cast::can_cast_types`] that handles cases not supported upstream yet.
-pub fn can_cast_types(from_type: &DataType, to_type: &DataType) -> bool {
-    use DataType::*;
-    match (from_type, to_type) {
-        // TODO: remove this once Arrow supports this upstream.
-        // https://github.com/apache/arrow-rs/pull/5360
-        (FixedSizeList(from_field, size_from), FixedSizeList(to_field, size_to)) => {
-            size_from == size_to && can_cast_types(from_field.data_type(), to_field.data_type())
-        }
-        // TODO: support bfloat16 cast?
-        _ => arrow_cast::can_cast_types(from_type, to_type),
-    }
-}
-
 /// Customized [`arrow_cast::cast_with_options`] that handles cases not supported upstream yet.
 pub fn cast_with_options(
     array: &dyn Array,
