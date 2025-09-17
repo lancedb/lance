f74564fa0d43e4704ecfadbc80acaafbf3bddd4a
Jack Ye <yezhaoqin@gmail.com>
2025-09-17 09:12:09 -0700

    java clippy

--- java/lance-jni/src/sql.rs
+++ java/lance-jni/src/sql.rs
@@ -57,8 +57,9 @@ fn inner_into_batch_records(
         with_row_addr,
     )?;
 
-    let stream = RT.block_on(async move { 
-        builder.build().await?.into_stream().await 
+    let stream = RT.block_on(async move {
+        let query = builder.build().await?;
+        query.into_stream().await
     })?;
 
     let ffi_stream =
