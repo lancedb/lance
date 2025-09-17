2ff264d9b0f94096f83015686403ba7d07d0109b
Will Jones <willjones127@gmail.com>
2025-09-17 12:46:43 -0700

    get a working ANN test

--- rust/lance/tests/query/vectors.rs
+++ rust/lance/tests/query/vectors.rs
@@ -9,18 +9,28 @@ use lance::Dataset;
 use lance_datagen::{array, gen_batch, ArrayGeneratorExt, Dimension, RowCount};
 use lance_index::IndexType;
 
-#[tokio::test]
-async fn test_query_vector() {
-    todo!()
+fn date_as_i32(date: &str) -> i32 {
+    // Return as i32 days since unix epoch.
+    use chrono::{NaiveDate, TimeZone, Utc};
+
+    let parsed_date =
+        NaiveDate::parse_from_str(date, "%Y-%m-%d").expect("Date should be in YYYY-MM-DD format");
+
+    let unix_epoch = Utc.timestamp_opt(0, 0).unwrap().date_naive();
+
+    (parsed_date - unix_epoch).num_days() as i32
 }
 
 #[tokio::test]
 async fn test_query_prefilter_date() {
     let batch = gen_batch()
         .col("id", array::step::<Int32Type>())
-        .col("value", array::step::<Date32Type>().with_random_nulls(0.1))
+        .col(
+            "value",
+            array::step_custom::<Date32Type>(date_as_i32("2020-01-01"), 1).with_random_nulls(0.1),
+        )
         .col("vec", array::rand_vec::<Float32Type>(Dimension::from(16)))
-        .into_batch_rows(RowCount::from(60))
+        .into_batch_rows(RowCount::from(256))
         .unwrap();
     DatasetTestCases::from_data(batch)
         .with_index_types("value", [None, Some(IndexType::BTree)])
@@ -29,18 +39,23 @@ async fn test_query_prefilter_date() {
             [
                 None,
                 Some(IndexType::IvfPq),
+                Some(IndexType::IvfSq),
                 Some(IndexType::IvfFlat),
                 Some(IndexType::IvfHnswFlat),
+                Some(IndexType::IvfHnswPq),
+                Some(IndexType::IvfHnswSq),
             ],
         )
         .run(|ds: Dataset, original: RecordBatch| async move {
             test_scan(&original, &ds).await;
             test_take(&original, &ds).await;
-            test_ann(&original, &ds, Some("value is not null")).await;
+            test_ann(&original, &ds, "vec", None).await;
+            test_ann(&original, &ds, "vec", Some("value is not null")).await;
             test_ann(
                 &original,
                 &ds,
-                Some("value >= DATE '2020-06-01' AND value <= DATE '2021-06-01'"),
+                "vec",
+                Some("value >= DATE '2020-01-03' AND value <= DATE '2020-01-25'"),
             )
             .await;
         })
