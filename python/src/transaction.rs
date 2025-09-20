414ff02dbfca5256d2a7bd7a0c278b1c727f72f1
yanghua <yanghua1127@gmail.com>
2025-09-20 15:41:46 +0800

    refactor code

--- python/src/transaction.rs
+++ python/src/transaction.rs
@@ -508,6 +508,7 @@ impl FromPyObject<'_> for PyLance<Transaction> {
             .extract::<Option<HashMap<String, String>>>()?
             .filter(|map| !map.is_empty())
             .map(Arc::new);
+        let primary_key_bloom_filter = ob.getattr("primary_key_bloom_filter")?.extract()?;
         Ok(Self(Transaction {
             read_version,
             uuid,
@@ -515,6 +516,7 @@ impl FromPyObject<'_> for PyLance<Transaction> {
             blobs_op,
             tag: None,
             transaction_properties,
+            primary_key_bloom_filter,
         }))
     }
 }
