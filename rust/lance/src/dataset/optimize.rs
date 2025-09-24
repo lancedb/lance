68b3a6903343b5fd2325c25058f165f8afc4043a
majin1102 <majin1102@gmail.com>
2025-09-24 11:13:09 +0800

    revert optimize.rs

--- rust/lance/src/dataset/optimize.rs
+++ rust/lance/src/dataset/optimize.rs
@@ -220,7 +220,7 @@ pub async fn compact_files(
     mut options: CompactionOptions,
     remap_options: Option<Arc<dyn IndexRemapperOptions>>, // These will be deprecated later
 ) -> Result<CompactionMetrics> {
-    info!(target: TRACE_DATASET_EVENTS, event=DATASET_COMPACTING_EVENT, uri = dataset.uri());
+    info!(target: TRACE_DATASET_EVENTS, event=DATASET_COMPACTING_EVENT, uri = &dataset.uri);
     options.validate();
 
     let compaction_plan: CompactionPlan = plan_compaction(dataset, &options).await?;
