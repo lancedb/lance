4d6b58c22d3f9eb788955bf2e6419f529e0cb9b1
nathan.ma <majin1102@gmail.com>
2025-10-17 22:23:52 +0000

    feat(java): support shallow_clone (#4962)
    Close #4854
    
    ---------
    
    Co-authored-by: majin.nathan <majin.nathan@bytedance.com>

--- java/lance-jni/src/blocking_dataset.rs
+++ java/lance-jni/src/blocking_dataset.rs
@@ -24,7 +24,7 @@ use jni::sys::{jbyteArray, jlong};
 use jni::{objects::JObject, JNIEnv};
 use lance::dataset::builder::DatasetBuilder;
 use lance::dataset::optimize::{compact_files, CompactionOptions as RustCompactionOptions};
-use lance::dataset::refs::TagContents;
+use lance::dataset::refs::{Ref, TagContents};
 use lance::dataset::statistics::{DataStatistics, DatasetStatisticsExt};
 use lance::dataset::transaction::{Operation, Transaction};
 use lance::dataset::{
@@ -1006,6 +1006,79 @@ fn inner_restore(env: &mut JNIEnv, java_dataset: JObject) -> Result<()> {
     let mut dataset_guard =
         unsafe { env.get_rust_field::<_, _, BlockingDataset>(java_dataset, NATIVE_DATASET) }?;
     dataset_guard.restore()
+}
+
+#[no_mangle]
+pub extern "system" fn Java_com_lancedb_lance_Dataset_nativeShallowClone<'local>(
+    mut env: JNIEnv<'local>,
+    java_dataset: JObject,
+    target_path: JString,
+    reference: JObject,
+    storage_options: JObject,
+) -> JObject<'local> {
+    ok_or_throw!(
+        env,
+        inner_shallow_clone(
+            &mut env,
+            java_dataset,
+            target_path,
+            reference,
+            storage_options
+        )
+    )
+}
+
+fn inner_shallow_clone<'local>(
+    env: &mut JNIEnv<'local>,
+    java_dataset: JObject,
+    target_path: JString,
+    reference: JObject,
+    storage_options: JObject,
+) -> Result<JObject<'local>> {
+    let target_path_str = target_path.extract(env)?;
+    let storage_options = env.get_optional(&storage_options, |env, map_obj| {
+        let jmap = JMap::from_env(env, map_obj)?;
+        to_rust_map(env, &jmap)
+    })?;
+
+    let reference = {
+        let version_number = env.get_optional_u64_from_method(&reference, "getVersionNumber")?;
+        let tag_name = env.get_optional_string_from_method(&reference, "getTagName")?;
+        let branch_name = env.get_optional_string_from_method(&reference, "getBranchName")?;
+        match (version_number, branch_name, tag_name) {
+            (Some(version_number), branch_name, None) => {
+                Ref::Version(branch_name, Some(version_number))
+            }
+            (None, None, Some(tag_name)) => Ref::Tag(tag_name),
+            _ => {
+                return Err(Error::input_error(
+                    "One of (optional branch, version_number) and tag must be specified"
+                        .to_string(),
+                ))
+            }
+        }
+    };
+
+    let new_ds = {
+        let mut dataset_guard =
+            unsafe { env.get_rust_field::<_, _, BlockingDataset>(java_dataset, NATIVE_DATASET) }?;
+        RT.block_on(
+            dataset_guard.inner.shallow_clone(
+                &target_path_str,
+                reference,
+                storage_options
+                    .map(|options| {
+                        Some(ObjectStoreParams {
+                            storage_options: Some(options),
+                            ..Default::default()
+                        })
+                    })
+                    .unwrap_or(None),
+            ),
+        )?
+    };
+
+    BlockingDataset { inner: new_ds }.into_java(env)
 }
 
 #[no_mangle]
