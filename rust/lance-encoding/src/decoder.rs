66eec9a0b669b80f99f7c89e4a26e5fbb9500c76
camilesing <camilesing@gmail.com>
2025-11-02 15:30:50 +0800

    fix: panic unwrap on None in decoder.rs

--- rust/lance-encoding/src/decoder.rs
+++ rust/lance-encoding/src/decoder.rs
@@ -789,7 +789,18 @@ impl CoreFieldDecoderStrategy {
             let scheduler = self.create_primitive_scheduler(field, column_info, buffers)?;
             return Ok(scheduler);
         } else if data_type.is_binary_like() {
-            let column_info = column_infos.next().unwrap().clone();
+            let Some(column_info_next) = column_infos.next() else {
+                return Err(Error::InvalidInput {
+                    source: format!(
+                        "column_info cannot be none. the field type is {}, name is {}",
+                        field.data_type(),
+                        field.name
+                    )
+                    .into(),
+                    location: location!(),
+                });
+            };
+            let column_info = column_info_next.clone();
             // Column is blob and user is asking for binary data
             if let Some(blob_col) = Self::unwrap_blob(column_info.as_ref()) {
                 let desc_scheduler =
@@ -797,7 +808,7 @@ impl CoreFieldDecoderStrategy {
                 let blob_scheduler = Box::new(BlobFieldScheduler::new(desc_scheduler.into()));
                 return Ok(blob_scheduler);
             }
-            if let Some(page_info) = column_info.page_infos.first() {
+            return if let Some(page_info) = column_info.page_infos.first() {
                 if matches!(
                     page_info.encoding.as_legacy(),
                     pb::ArrayEncoding {
@@ -813,31 +824,43 @@ impl CoreFieldDecoderStrategy {
                             false,
                         )))
                     };
-                    let list_field = Field::try_from(ArrowField::new(
+                    let list_field_result = Field::try_from(ArrowField::new(
                         field.name.clone(),
                         list_type,
                         field.nullable,
-                    ))
-                    .unwrap();
-                    let list_scheduler = self.create_list_scheduler(
+                    ));
+
+                    if list_field_result.is_err() {
+                        return Err(list_field_result.err().unwrap());
+                    }
+
+                    let list_field = list_field_result?;
+                    let list_scheduler_result = self.create_list_scheduler(
                         &list_field,
                         column_infos,
                         buffers,
                         &column_info,
-                    )?;
+                    );
+
+                    if list_scheduler_result.is_err() {
+                        return Err(list_scheduler_result.err().unwrap());
+                    }
+
+                    let list_scheduler = list_scheduler_result?;
+
                     let binary_scheduler = Box::new(BinaryFieldScheduler::new(
                         list_scheduler.into(),
                         field.data_type(),
                     ));
-                    return Ok(binary_scheduler);
+                    Ok(binary_scheduler)
                 } else {
                     let scheduler =
                         self.create_primitive_scheduler(field, &column_info, buffers)?;
-                    return Ok(scheduler);
+                    Ok(scheduler)
                 }
             } else {
-                return self.create_primitive_scheduler(field, &column_info, buffers);
-            }
+                self.create_primitive_scheduler(field, &column_info, buffers)
+            };
         }
         match &data_type {
             DataType::FixedSizeList(inner, _dimension) => {
