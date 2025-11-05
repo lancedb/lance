c27a2ed4b84bfe952395ce705c5c7f9cc6d62410
Will Jones <willjones127@gmail.com>
2025-11-05 15:45:43 -0800

    exclude macos

--- rust/lance/tests/resource_tests.rs
+++ rust/lance/tests/resource_tests.rs
@@ -1,3 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright The Lance Authors
+
+// The memory tests don't work currently on MacOS because they rely on thread
+// local storage in the allocator, which seems to have some issues on MacOS.
+#[cfg(not(target_os = "macos"))]
 mod resource_test;
