0512f821a9c1d459daa2d95e1ac75870e35a969a
yanghua <yanghua1127@gmail.com>
2026-02-10 20:43:41 +0800

    address review comments

--- rust/lance-core/src/utils/mask.rs
+++ rust/lance-core/src/utils/mask.rs
@@ -1008,7 +1008,9 @@ impl RowSetOps for RowIdSet {
         self.inner.contains(row)
     }
     fn union_all(other: &[&Self]) -> Self {
-        let mut result = other.first().map_or(Self::default(), |&first| first.clone());
+        let mut result = other
+            .first()
+            .map_or(Self::default(), |&first| first.clone());
         for set in other {
             result.inner |= &set.inner;
         }
