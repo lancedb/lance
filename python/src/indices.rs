b2819e36b511c83988834835749bf72649748aaa
Jack Ye <yezhaoqin@gmail.com>
2026-05-02 19:27:55 -0700

    fix: update Python bindings and bench for sync object_store

--- python/src/indices.rs
+++ python/src/indices.rs
@@ -526,7 +526,7 @@ async fn do_load_shuffled_vectors(
 
     let mut ds = dataset.ds.as_ref().clone();
     let index_dir = ds.indices_dir().child(index_id.to_string());
-    let object_store = ds.object_store(None).await.infer_error()?;
+    let object_store = ds.object_store(None).infer_error()?;
     let files = list_index_files_with_sizes(object_store.as_ref(), &index_dir)
         .await
         .infer_error()?;
