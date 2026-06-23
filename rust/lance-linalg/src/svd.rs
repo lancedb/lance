4dd41f8959b14b6ec9fa7da57cf17577661cc6e9
Sarah Nasser <sarahnasser@Sarahs-MacBook-Air-8.local>
2026-06-22 23:44:16 -0400

    Checked whether the input matrix has at least one row and at least one column

--- rust/lance-linalg/src/svd.rs
+++ rust/lance-linalg/src/svd.rs
@@ -143,6 +143,8 @@ fn multiply_A_by_vector(a: &[f64], x: &[f64], m: usize, n: usize) -> Vec<f64> {
 }
 
 pub fn svd(a: &[f64], m: usize, n: usize) -> (Vec<f64>, Vec<f64>, Vec<f64>) {
+    assert!(m > 0 && n > 0, "Matrix must have at least 1 row and at least 1 column.");
+
     let ata = compute_ata(a, m, n);
     let (eigenvalues, v) = jacobi_eigen(&ata, n);
     
